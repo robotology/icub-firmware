e66c0f855298d22e1b16af197ff89040e2d18304
Marco Maggiali <marco.maggiali@iit.it>
2011-11-03 06:58:43 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=14266

--- libDsp56f807/api/phase_hall_sens.h
+++ libDsp56f807/api/phase_hall_sens.h
@@ -12,8 +12,14 @@ typedef struct sPwmControlBL_tag
 	UWord16    MaskOut;
 } sPwmControlBL;
 
+#define NORMAL_COMMUTATION_INTERVAL  342   //resolution of the encoder for an electric turn (8192/4)/6=341.3       
+#define SHORT_COMMUTATION_INTERVAL   338    //(2048-(5*342) this step is longer to compensate the 0.3 tick each step 
+#define HALF_COMMUTATION_INTERVAL    171   //resolution of the encoder for an electric turn (8192/4)/6=341.3       
+#define  HALL_ERROR_0K   	0x0  //no errors
 #define  HALL_ERROR_TD0   	0x1  //when there is a mismatch between Hall reading in the hall_interrupt and the 1ms timer
-#define  HALL_ERROR_TABLE 	0x2  //when the reading of the hall in the hall_interrupt is not congruent with the table  
+
+#warning "we should change it with 0x02 but iCubInterface does not read it"
+#define  HALL_ERROR_TABLE 	0x1   //when the reading of the hall in the hall_interrupt is not congruent with the table  
 #define  HALL_ERROR_GLITCH	0x4 //when there is a mismatch between the two hall reading in the hall_interrupt 
 
 
@@ -56,4 +62,9 @@ UInt8 Get_Sens1_Status(void);
 
 UInt8 getHallStatus(UInt16 channel);
 void setHallStatus(UInt16 channel,UInt8 val);
+
+void    PWMAReload_Interrupt();
+void    PWMBReload_Interrupt();
+
+
 #endif 
