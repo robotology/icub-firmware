0b1a36125e54d9d7e1ebc7668f9f9db4eb48c62d
Marco Accame <marco.accame@iit.it>
2012-03-26 14:03:53 +0000

    added sys files for mid/cmsis and mid/cmsisdsp
    svn path=/trunk/iCub/firmware/; revision=16474

---
+++ emBODY/arch-armcmx/sys/mid/api/cmsis_stellaris.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _CMSIS_STELLARIS_H_
+#define _CMSIS_STELLARIS_H_
+
+
+/** @file       cmsis_stellaris.h
+	@brief      This header file implements public interface to the cmsis for the stellaris micros from ti 
+	@author     marco.accame@iit.it
+	@date       07/29/2010
+**/
+
+/* @defgroup cmsis_stell 
+    
+    @todo   maybe to place in here some decription of the api used for tha hal ...
+    
+    @{		
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "lm3s_cmsis.h"
+
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+// empty-section
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+// empty-section
+
+    
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+// empty-section
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+// empty-section
+
+
+
+/* @}            
+    end of group cmsis_stell  
+ **/
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
---
+++ emBODY/arch-armcmx/sys/mid/src/system_LM3S.c
@@ -0,0 +1,51 @@
+//*****************************************************************************
+//
+// system_lm3s.c - System initialization file for CMSIS application on a
+//                 Luminary Micro Stellaris microcontroller.
+//
+// Copyright (c) 2009 Luminary Micro, Inc.  All rights reserved.
+// Software License Agreement
+// 
+// Luminary Micro, Inc. (LMI) is supplying this software for use solely and
+// exclusively on LMI's microcontroller products.
+// 
+// The software is owned by LMI and/or its suppliers, and is protected under
+// applicable copyright laws.  All rights are reserved.  You may not combine
+// this software with "viral" open-source software in order to form a larger
+// program.  Any use in violation of the foregoing restrictions may subject
+// the user to criminal sanctions under applicable laws, as well as to civil
+// liability for the breach of the terms and conditions of this license.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+// OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+// LMI SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+// CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 48 of the Stellaris CMSIS Package.
+//
+//*****************************************************************************
+
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#error --> the content of this file has been removed because "You may not combine this software with "viral" open-source software in order to form a larger program."
+
+
+
+
