4ac2a961524a8c2da11a2f764e67c70517c561a8
Marco Accame <marco.accame@iit.it>
2012-03-29 16:13:13 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=16619

---
+++ emBODY/arch-armcmx/body/midware/osiit-package.disinstall.bat
@@ -0,0 +1,34 @@
+
+    
+@echo off
+
+cls
+echo == Initiating system instance variables...
+echo. -- Setting the variables...
+
+:: Here you need to make some changes to suit your system.
+set SOURCE=./osiit-package
+set SVN=C:\Program Files\TortoiseSVN\bin
+set URL=https://svn.cognitivehumanoids.eu/sdl/embedded/arm/embody/body/abslayer/ipal-package/ipal/ipal-extra
+
+:: Unless you want to modify the script, this is enough.
+
+echo. -- deleting ...
+
+rmdir /S "%SOURCE%"
+DEL /F ..\..\sys\mid\lib\osiit*.lib
+DEL /F ..\..\sys\mid\api\osiit*
+DEL /F ..\..\sys\mid\cfg\osiit*
+DEL /F ..\..\sys\mid\inc\osiit*
+DEL /F ..\..\sys\mid\inc\rtl_*
+
+echo. ++ Done.
+    
+    
+    
+    
+
+
+
+
+
