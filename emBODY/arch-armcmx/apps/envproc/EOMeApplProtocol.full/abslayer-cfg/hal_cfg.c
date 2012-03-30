d303f3796df9471b2a6621b97c60f60f163cc1d9
Marco Accame <marco.accame@iit.it>
2012-03-30 14:38:20 +0000

    added copyright
    svn path=/trunk/iCub/firmware/; revision=16663

--- emBODY/arch-armcmx/apps/envproc/EOMeApplProtocol.full/abslayer-cfg/hal_cfg.c
+++ emBODY/arch-armcmx/apps/envproc/EOMeApplProtocol.full/abslayer-cfg/hal_cfg.c
@@ -1,8 +1,25 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
 
 /* @file       hal_cfg.c
-	@brief      This file keeps internal implementation of the osal.
+	@brief      This file keeps internal implementation of the hal.
 	@author     marco.accame@iit.it
-    @date       11/27/2009
+    @date       09/09/2011
 **/
 
 
@@ -25,7 +42,7 @@
 static void s_hal_cfg_on_fatalerror(hal_fatalerror_t errorcode, const char * errormsg);
 
 
-extern const hal_cfg_t s_hal_cfg = 
+extern const hal_cfg_t hal_cfg = 
 {   
     .cpu_family             = (hal_cpufamily_t) HAL_CPUFAM,
     .cpu_type               = (hal_cputype_t) HAL_CPUTYPE,
@@ -62,7 +79,7 @@ extern const hal_cfg_t s_hal_cfg =
 };
 
 
-extern const hal_cfg_t *hal_cfgMINE = &s_hal_cfg;
+extern const hal_cfg_t *hal_cfgMINE = &hal_cfg;
 
 
 
@@ -75,10 +92,6 @@ static void s_hal_cfg_on_fatalerror(hal_fatalerror_t errorcode, const char * err
         sprintf(str, "fatal error #%d: %s\n", errorcode, errormsg);
         hal_trace_puts(str);
     }
-
-    hal_led_init(hal_led7, NULL);
-
-    hal_led_off(hal_led7);
 
     for(;;);
 
