f36f8c3ac7e5d8f9f46dbb6a5689512baab995dd
Marco Accame <marco.accame@iit.it>
2012-04-02 12:17:11 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=16754

---
+++ emBODY/arch-armcmx/sys/env/bin/eproc--.externals.install.bat
@@ -0,0 +1,45 @@
+
+    
+@echo off
+
+cls
+echo == Initiating system instance variables...
+echo. -- Setting the variables...
+
+:: Here you need to make some changes to suit your system.
+set SOURCE=./externals
+set SVN=C:\Program Files\TortoiseSVN\bin
+set URL=https://svn.cognitivehumanoids.eu/sdl/embedded/addons2sourceforge/arm-sys-env-bin
+
+:: Unless you want to modify the script, this is enough.
+
+echo. %SOURCE%
+echo. %SVN%
+echo. %URL%
+echo. ++ Done setting variables.
+echo.
+echo == Updating source from SVN
+echo. -- Running update...
+"%SVN%\TortoiseProc.exe" /command:checkout /path:"%SOURCE%" /url:"%URL%" /closeonend:1
+echo. ++ Done.
+
+
+echo. -- setting it RO...
+
+attrib +R "%SOURCE%"\*.*
+
+copy "%SOURCE%"\*.* .
+
+echo. -- Cleaning up...
+set SOURCE=
+set SVN=
+echo. ++ Done.
+    
+    
+    
+    
+
+
+
+
+
