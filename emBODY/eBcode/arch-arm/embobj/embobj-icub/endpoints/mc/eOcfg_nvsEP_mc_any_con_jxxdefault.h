23eb01751c8ec1ec876bce364507cdd400c2ef16
Marco Accame <marco.accame@iit.it>
2012-04-16 10:44:04 +0000

    inserted back these two files
    svn path=/trunk/iCub/firmware/emBODY/arch-armcmx/body/embobj/embobj/; revision=16969

---
+++ emBODY/eBcode/arch-arm/embobj/embobj-icub/endpoints/mc/eOcfg_nvsEP_mc_any_con_jxxdefault.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOCFG_NVSEP_MC_ANY_CON_JXXDEFAULT_H_
+#define _EOCFG_NVSEP_MC_ANY_CON_JXXDEFAULT_H_
+
+
+
+
+/** @file       eOcfg_cfg_nvsEP_mc_any_con_jxxdefault.h
+	@brief      This header file gives the ...
+	@author     marco.accame@iit.it
+	@date       09/06/2011
+**/
+
+/** @defgroup eo_456uilsdede2345er Configuation of the ......
+    Tcecece 
+    
+    @{		
+ **/
+
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+#include "EoMotionControl.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+// empty-section 
+        
+
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+    
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+
+extern const eOmc_joint_t eo_cfg_nvsEP_mc_any_con_jxxdefault_defaultvalue;
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+
+
+
+/** @}            
+    end of group eo_456uilsdede2345er  
+ **/
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
+
