493a8320edeb6fbd47eb9a37a5b181fbc2f9b431
Marco Accame <marco.accame@iit.it>
2012-04-17 14:59:22 +0000

    some tentative changes for strain and mais
    svn path=/trunk/iCub/firmware/emBODY/arch-armcmx/body/embobj/embobj/; revision=17002

--- emBODY/eBcode/arch-arm/embobj/embobj-icub/EoSensors.h
+++ emBODY/eBcode/arch-arm/embobj/embobj-icub/EoSensors.h
@@ -40,6 +40,7 @@
 // - external dependencies --------------------------------------------------------------------------------------------
 
 #include "EoCommon.h"
+#include "EOarray.h"
 #include "EoUtilities.h"
 #include "EoMeasures.h"
 
@@ -55,7 +56,6 @@
 
 /** @typedef    typedef struct eOsnsr_forcetorque_t
     @brief      eOsnsr_forcetorque_t contains measurements of force on xyz and of torque on 3 components 
-    @warning    This struct must be of fixed and size and multiple of 4.
  **/
 typedef struct                  // size is: 4+2*3+2*3+0 = 16 
 {
@@ -63,6 +63,46 @@ typedef struct                  // size is: 4+2*3+2*3+0 = 16
     eOmeas_force_t              force[3];           /**< the force split in x, y, and z components */
     eOmeas_torque_t             torque[3];          /**< the torque split in three components*/
 } eOsnsr_forcetorque_t;         EO_VERIFYsizeof(eOsnsr_forcetorque_t, 16);
+
+
+/** @typedef    typedef struct eOsnsr_upto15analog08vals_t
+    @brief      eOsnsr_upto15analog08vals_t contains measurements of generic analog sensors with 8 bit resolution, as the default behaviour of mais 
+ **/
+typedef struct                  // size is: 1+1*15+0 = 16 
+{
+    uint8_t                     number;             /**< the number of values */ 
+    uint8_t                     value[15];          /**< the values */
+} eOsnsr_upto15analog08vals_t;  EO_VERIFYsizeof(eOsnsr_upto15analog08vals_t, 16);
+// note: a more generic type would be an EOarray. however, 4 bytes are required for teh head, thus only 12 bytes are available to fit in  
+
+/** @typedef    typedef struct eOsnsr_arrayofupto12bytes_t
+    @brief      eOsnsr_arrayofupto12bytes_t contains measurements of generic analog sensors with 8/16/32 bit resolution and is able to contain the 12 bytes of the strain
+ **/
+typedef struct                  // size is: 4+12 = 16 
+{
+    eOarray_head_t              head;               /**< the head */ 
+    uint8_t                     data[12];           /**< the data: upto 12 U8, or 6 U16, or 3 U32 */
+} eOsnsr_arrayofupto12bytes_t;  EO_VERIFYsizeof(eOsnsr_arrayofupto12bytes_t, 16);
+
+
+/** @typedef    typedef struct eOsnsr_arrayofupto20bytes_t
+    @brief      eOsnsr_arrayofupto20bytes_t contains measurements of generic analog sensors with 8/16/32 bit resolution and is able to contain the 15 bytes of the mais
+ **/
+typedef struct                  // size is: 4+20 = 24 
+{
+    eOarray_head_t              head;               /**< the head */ 
+    uint8_t                     data[20];           /**< the data: upto 20 U8, or 10 U16, or 5 U32 */
+} eOsnsr_arrayofupto20bytes_t;  EO_VERIFYsizeof(eOsnsr_arrayofupto20bytes_t, 24);
+
+
+/** @typedef    typedef struct eOsnsr_arrayofupto36bytes_t
+    @brief      eOsnsr_arrayofupto20bytes_t contains measurements of generic analog sensors with 8/16/32 bit resolution and is able to contain the 15 halfwords of mais
+ **/
+typedef struct                  // size is: 4+36+0 = 40 
+{
+    eOarray_head_t              head;               /**< the head */ 
+    uint8_t                     data[36];           /**< the data: upto 20 U8, or 10 U16, or 5 U32 */
+} eOsnsr_arrayofupto36bytes_t;  EO_VERIFYsizeof(eOsnsr_arrayofupto36bytes_t, 40);
 
 
 /** @typedef    typedef struct eOsnsr_aea_t
@@ -74,6 +114,30 @@ typedef struct                  // size is: 4+0 = 4
     uint32_t                    value;            // used with ... see hal_encoder.h for bitfield formatting  
 } eOsnsr_aea_t;                 EO_VERIFYsizeof(eOsnsr_aea_t, 4);
 
+
+
+typedef struct                      // size is: 1+1+1+1+4+16+0 = 24
+{
+    eOenum08_t                      txmode;
+    uint8_t                         datarateinms;
+    eObool_t                        signalonce_fullscale;
+    uint8_t                         filler01[1];
+    uint8_t                         filler04[4];    
+    eOsnsr_arrayofupto12bytes_t     values;
+} eOsnsr_strain_t;
+
+
+typedef struct                      // size is: 1+1+1+1+4+24+0 = 32
+{
+    eOenum08_t                      txmode;
+    uint8_t                         datarateinms;
+    eOenum08_t                      notused_resolutionmode;
+    uint8_t                         filler01[1];
+    uint8_t                         filler04[4];    
+    eOsnsr_arrayofupto20bytes_t     values; 
+} eOsnsr_mais_t;
+
+// the endpoint needs: 4 strain + 2 mais -> 4*24+2*32 = 160 bytes. accettabile.
 
 
 /** @typedef    typedef uint8_t  eOsnsr_sensorId_t
