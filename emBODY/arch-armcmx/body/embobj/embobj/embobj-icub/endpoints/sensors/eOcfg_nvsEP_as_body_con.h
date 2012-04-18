b096a5e3941d4a04ec38b0b4100f013109976c21
Marco Accame <marco.accame@iit.it>
2012-04-18 14:44:12 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=17036

--- emBODY/arch-armcmx/body/embobj/embobj/embobj-icub/endpoints/sensors/eOcfg_nvsEP_as_body_con.h
+++ emBODY/arch-armcmx/body/embobj/embobj/embobj-icub/endpoints/sensors/eOcfg_nvsEP_as_body_con.h
@@ -18,13 +18,13 @@
 
 // - include guard ----------------------------------------------------------------------------------------------------
 
-#ifndef _EOCFG_NVSEP_BODYSENSORS_CON_H_
-#define _EOCFG_NVSEP_BODYSENSORS_CON_H_
+#ifndef _EOCFG_NVSEP_AS_WHOLEBODY_CON_H_
+#define _EOCFG_NVSEP_AS_WHOLEBODY_CON_H_
 
 
 
 
-/** @file       eOcfg_nvsEP_bodysensors_con.h
+/** @file       eOcfg_nvsEP_as_wholebody_con.h
 	@brief      This header file gives the constant configuration for the NVs in the endpoint bodysensors
 	@author     marco.accame@iit.it
 	@date       04/05/2012
@@ -46,111 +46,134 @@
 
 #include "EoSensors.h"
 
-#include "eOcfg_nvsEP_mc_any_con_maisxx.h"
-#include "eOcfg_nvsEP_mc_any_con_strainxx.h"
+#include "eOcfg_nvsEP_as_any_con_maisxx.h"
+#include "eOcfg_nvsEP_as_any_con_strainxx.h"
 
  
 
 // - public #define  --------------------------------------------------------------------------------------------------
-
-#define EOK_cfg_nvsEP_bodysensors_NUMofSTRAINS                                      (4)
-#define EOK_cfg_nvsEP_bodysensors_NUMofMAISES                                       (2)
-
-#define EOK_cfg_nvsEP_bodysensors_STRAIN_leftarm                                    (0)
-#define EOK_cfg_nvsEP_bodysensors_STRAIN_rigtharm                                   (1)
-#define EOK_cfg_nvsEP_bodysensors_STRAIN_leftleg                                    (3)
-#define EOK_cfg_nvsEP_bodysensors_STRAIN_rigthleg                                   (4)
-
-#define EOK_cfg_nvsEP_bodysensors_MAIS_lefthand                                     (0)
-#define EOK_cfg_nvsEP_bodysensors_MAIS_rigthhand                                    (1)
-
-// the 16bit value to use to identify the endpoint
-#define EOK_cfg_nvsEP_bodysensors_EP                                                 (0x0020)
-
-// the total number of nv in the endpoint
-#define EOK_cfg_nvsEP_bodysensors_NUMofVARS                                          ((EOK_cfg_nvsEP_mc_any_con_strainxx_snvindex_TOTALnumber*EOK_cfg_nvsEP_bodysensors_NUMofSTRAINS)+(EOK_cfg_nvsEP_mc_any_con_maisxx_mnvindex_TOTALnumber*EOK_cfg_nvsEP_bodysensors_NUMofMAISES))
-
-
-// mettiamo: le due mais, e le 4 strain (con nomi relativi alle bodypart: lleg, larm, rleg, rarm).
-
-// poi: mettiamo una cfg per strain ed una per mais. ed anche i valori segnalati
-// in strain-cfg: datarate, txmode, signal_fullscale_now. 
-// in mais-cfg: datarate, txmode.
-// per mais-values: uso il tipo eOsnsr_arrayofupto20bytes_t
-// per strain-values: uso il tipo eOsnsr_arrayofupto12bytes_t dove conosco l'ordine che e': 3 force e poi 3 torque.
-
-
-
-// the 16bit used to identify the network variable inside the endpoint. they are function of the joint or motor number (jth or mth)
-#define EOK_cfg_nvsEP_bodysensors_NVID_strain__txmode(jth)                                  EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig(jth)
-#define EOK_cfg_nvsEP_bodysensors_NVID_strain__datarate(jth)                     EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__pidposition(jth)                       
-#define EOK_cfg_nvsEP_bodysensors_NVID_strain__signaloncefullscale(jth)                     EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__pidvelocity(jth)                      
-#define EOK_cfg_nvsEP_bodysensors_NVID_strain__values(jth)                       EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__pidtorque(jth)   
-    
-#define EOK_cfg_nvsEP_bodysensors_NVID_mais__txmode(jth)              EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__minpositionofjoint(jth)       
-#define EOK_cfg_nvsEP_bodysensors_NVID_mais__datarate(jth)              EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__maxpositionofjoint(jth)       
-#define EOK_cfg_nvsEP_bodysensors_NVID_mais__resolutionmode(jth)         EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__velocitysetpointtimeout(jth)    
-#define EOK_cfg_nvsEP_bodysensors_NVID_mais__values(jth)       EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_jxx_jconfig__upto02descrforchameleon02(jth)   
-
-
+// empty-section
 
 
 
 // - declaration of public user-defined types ------------------------------------------------------------------------- 
 
 
-/** @typedef    typedef enum eo_cfg_nvsEP_mc_leftlef_con_jointNumber_t
-    @brief      It contains an index for all the joints the left leg. It is a subset of those in eo_cfg_nvsEP_mc_any_con_bodypart_jointNumber_t
+/** @typedef    typedef enum eo_cfg_nvsEP_as_wholebody_con_strainNumber_t
+    @brief      It contains an index for all the strain in the body.
  **/
 typedef enum
 {
     strainLeftArm_00 = 0,   
     strainRigthArm_01, strainLeftLeg_02, strainRigthLeg_03
-} eo_cfg_nvsEP_bodysensor_con_jointNumber_t;
+} eo_cfg_nvsEP_as_wholebody_con_strainNumber_t;
 
-enum { jointLeftLeg_TOTALnumber = 6}; 
+enum { strain_TOTALnumber = 4}; 
 
-/** @typedef    typedef eo_cfg_nvsEP_mc_any_con_bodypart_jointNVindex_t eo_cfg_nvsEP_mc_leftleg_con_jointNVindex_t
-    @brief      It contains an index for all the network variables in a joint of the left leg. use the same type as in bodypart
+/** @typedef    typedef struct eo_cfg_nvsEP_as_wholebody_con_strainNVindex_t
+    @brief      It contains an index for all the network variables in a strain.
  **/
-typedef eo_cfg_nvsEP_mc_any_con_bodypart_jointNVindex_t eo_cfg_nvsEP_mc_leftleg_con_jointNVindex_t;
+typedef enum
+{
+    strainNVindex__txmode                                   =   0,
+    strainNVindex__datarate                                 =   1,
+    strainNVindex__signaloncefullscale                      =   2,
+    strainNVindex__values                                   =   3
+} eo_cfg_nvsEP_as_wholebody_con_strainNVindex_t;
+
+enum { strainNVindex_TOTALnumber = 4 };
+
+/** @typedef    typedef enum eo_cfg_nvsEP_as_wholebody_con_maisNumber_t
+    @brief      It contains an index for all the mais in the body.
+ **/
+typedef enum
+{
+    maisLeftHand_00 = 0,   
+    maisRigthHand_01
+} eo_cfg_nvsEP_as_wholebody_con_maisNumber_t;
+
+enum { mais_TOTALnumber = 4}; 
+
+/** @typedef    typedef struct eo_cfg_nvsEP_as_wholebody_con_maisNVindex_t
+    @brief      It contains an index for all the network variables in a mais.
+ **/
+typedef enum
+{
+    maisNVindex__txmode                                     =   0,
+    maisNVindex__datarate                                   =   1,
+    maisNVindex__resolutionmode                             =   2,
+    maisNVindex__values                                     =   3
+} eo_cfg_nvsEP_as_wholebody_con_maisNVindex_t;
+
+enum { maisNVindex_TOTALnumber = 4 };
 
 
+/** @typedef    enum varsASwholebody_TOTALnumber;
+    @brief      It contains the total number of network variables managed by the endpoint leftleg
+ **/
+enum {varsASwholebody_TOTALnumber = strain_TOTALnumber*strainNVindex_TOTALnumber + mais_TOTALnumber*maisNVindex_TOTALnumber };
 
-// - very important information: on ARM alignment is done using "#pragma pack(8)", thus ... there are rules to follow
 
+/** @typedef    typedef struct eo_cfg_nvsEP_as_wholebody_t;
+    @brief      contains all the variables in the wholebody of kind analog sensors
+ **/
 typedef struct               
 {
-    //eOsnsr_strain_t             strain_larm; 
-    //eOsnsr_strain_t             strain_rarm; 
-    //eOsnsr_strain_t             strain_lleg; 
-    //eOsnsr_strain_t             strain_rleg; 
-    eOsnsr_strain_t             strains[EOK_cfg_nvsEP_bodysensors_NUMofSTRAINS];
-    //eOsnsr_mais_t               mais_lhand;
-    //eOsnsr_mais_t               mais_rhand;
-    eOsnsr_mais_t               maises[EOK_cfg_nvsEP_bodysensors_NUMofMAISES];
-} eo_cfg_nvsEP_bodysensors_t;   EO_VERIFYsizeof(eo_cfg_nvsEP_bodysensors_t, 160);
+    eOsnsr_strain_t                 strains[strain_TOTALnumber];
+    eOsnsr_mais_t                   maises[mais_TOTALnumber];
+} eo_cfg_nvsEP_as_wholebody_t;      EO_VERIFYsizeof(eo_cfg_nvsEP_as_wholebody_t, 160);
 
     
 // - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
-
-//extern const eo_cfg_nvsEP_bodysensors_t eo_cfg_nvsEP_bodysensors_default;
-
-// EOconstvector where each element is a EOtreenode whose data field is a EOnv_con_t object (id, capacity, valuedef, offset)
-extern const EOconstvector* const eo_cfg_nvsEP_bodysensors_constvector_of_treenodes_EOnv_con;
-
-// if not NULL it contains a mapping from IDs to index inside eo_cfg_nvsEP_bodysensors_constvector_of_treenodes_EOnv_con
-extern const eOuint16_fp_uint16_t eo_cfg_nvsEP_bodysensors_fptr_hashfunction_id2index;
-
-
+// empty-section
 
 // - declaration of extern public functions ---------------------------------------------------------------------------
 
-extern uint16_t eo_cfg_nvsEP_bodysensors_hashfunction_id2index(uint16_t id);
+
+
+/** @fn         extern eOnvEP_t eo_cfg_nvsEP_as_wholebody_EP_Get(void)
+    @brief      This function retrieves the eOnvEP_t of this endpoint
+    @return     the endpoint.
+  */
+extern eOnvEP_t eo_cfg_nvsEP_as_wholebody_EP_Get(void);
+
+
+/** @fn         extern eOnvID_t eo_cfg_nvsEP_as_wholebody_NVID_for_strain_var_Get(eo_cfg_nvsEP_as_wholebody_con_strainNumber_t s, eo_cfg_nvsEP_as_wholebody_con_strainNVindex_t snvindex)
+    @brief      This function retrieves the eOnvID_t of a network variable with index @e snvindex for the strain number @e s
+    @param      s               the strain number 
+    @param      snvinxed        the index of the nv inside the strain
+    @return     the nvid or EOK_uint16dummy in case of failure.
+  */
+extern eOnvID_t eo_cfg_nvsEP_as_wholebody_NVID_for_strain_var_Get(eo_cfg_nvsEP_as_wholebody_con_strainNumber_t s, eo_cfg_nvsEP_as_wholebody_con_strainNVindex_t snvindex);
+
+
+/** @fn         extern eOnvID_t eo_cfg_nvsEP_as_wholebody_NVID_for_mais_var_Get(eo_cfg_nvsEP_as_wholebody_con_maisNumber_t s, eo_cfg_nvsEP_as_wholebody_con_maisNVindex_t mnvindex)
+    @brief      This function retrieves the eOnvID_t of a network variable with index @e snvindex for the strain number @e s
+    @param      m               the mais number 
+    @param      mnvinxed        the index of the nv inside the mais
+    @return     the nvid or EOK_uint16dummy in case of failure.
+  */
+extern eOnvID_t eo_cfg_nvsEP_as_wholebody_NVID_for_mais_var_Get(eo_cfg_nvsEP_as_wholebody_con_maisNumber_t s, eo_cfg_nvsEP_as_wholebody_con_maisNVindex_t mnvindex);
 
 
 
 
+// - declarations used to load the endpoint into a EOnvsCfg object ----------------------------------------------------
+
+// the 16bit value to use to identify the endpoint
+#define EOK_cfg_nvsEP_as_wholebody_EP                                                 (0x0020)
+
+// it is a EOconstvector where each element is a EOtreenode whose data field is a EOnv_con_t object (id, capacity, valuedef, offset)
+extern const EOconstvector* const eo_cfg_nvsEP_as_wholebody_constvector_of_treenodes_EOnv_con;
+
+// if not NULL it contains a mapping from IDs to index inside eo_cfg_nvsEP_as_wholebody_constvector_of_treenodes_EOnv_con
+extern const eOuint16_fp_uint16_t eo_cfg_nvsEP_as_wholebody_fptr_hashfunction_id2index;
+
+// the size of the ram to be used
+#define EOK_cfg_nvsEP_as_wholebody_RAMSIZE                                            (sizeof(eo_cfg_nvsEP_as_wholebody_t));
+
+// the hash function
+extern uint16_t eo_cfg_nvsEP_as_wholebody_hashfunction_id2index(uint16_t id);
 
 /** @}            
     end of group eo_werasfdgr234  
