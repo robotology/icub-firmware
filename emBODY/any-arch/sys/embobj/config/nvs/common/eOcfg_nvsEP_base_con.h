893c768360900384e4a238cf5d15ee4213215478
Marco Accame <marco.accame@iit.it>
2012-04-20 08:14:38 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=17084

--- emBODY/any-arch/sys/embobj/config/nvs/common/eOcfg_nvsEP_base_con.h
+++ emBODY/any-arch/sys/embobj/config/nvs/common/eOcfg_nvsEP_base_con.h
@@ -49,11 +49,6 @@
 
 
 
-#define ep_leftleg      0x1234
-#define id_j0_setpoint  0x6789
-
- 
-
 // - public #define  --------------------------------------------------------------------------------------------------
 
 #define EOK_cfg_nvsEP_base                                          (64*0)
@@ -89,7 +84,7 @@
 #define EOK_cfg_nvsEP_base_NVID__forcerestart                       EO_nv_ID(EOK_cfg_nvsEP_base_NVFUNTYP__forcerestart,         8+EOK_cfg_nvsEP_base)
 #define EOK_cfg_nvsEP_base_NVID__localise                           EO_nv_ID(EOK_cfg_nvsEP_base_NVFUNTYP__localise,             9+EOK_cfg_nvsEP_base)
 #define EOK_cfg_nvsEP_base_NVID__remoteipaddress                    EO_nv_ID(EOK_cfg_nvsEP_base_NVFUNTYP__remoteipaddress,      10+EOK_cfg_nvsEP_base)
-#define EOK_cfg_nvsEP_base_NVID__remoteipport(bodypart, joint)                       EO_nv_ID(EOK_cfg_nvsEP_base_NVFUNTYP__remoteipport,         11+EOK_cfg_nvsEP_base)
+#define EOK_cfg_nvsEP_base_NVID__remoteipport                       EO_nv_ID(EOK_cfg_nvsEP_base_NVFUNTYP__remoteipport,         11+EOK_cfg_nvsEP_base)
 
 
 //#define EOK_cfg_nvsEP_base_NVID__boardinfo                          EO_nv_getID(eo_nv_FUN_con, eo_nv_TYP_pkd, 0+EOK_cfg_nvsEP_base)
