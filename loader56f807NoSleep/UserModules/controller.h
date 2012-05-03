cb0c15ec561176ce6a8794b4025b671d53e75177
Lorenzo Natale <lorenzo.natale@iit.it>
2012-05-03 16:27:31 +0000

    fixed old license statements
    svn path=/trunk/iCub/firmware/; revision=17370

--- loader56f807NoSleep/UserModules/controller.h
+++ loader56f807NoSleep/UserModules/controller.h
@@ -1,30 +1,6 @@
-/////////////////////////////////////////////////////////////////////////
-///                                                                   ///
-///       YARP - Yet Another Robotic Platform (c) 2001-2003           ///
-///                                                                   ///
-///                    #Add our name(s) here#                         ///
-///                                                                   ///
-///     "Licensed under the Academic Free License Version 1.0"        ///
-///                                                                   ///
-/// The complete license description is contained in the              ///
-/// licence.template file included in this distribution in            ///
-/// $YARP_ROOT/conf. Please refer to this file for complete           ///
-/// information about the licensing of YARP                           ///
-///                                                                   ///
-/// DISCLAIMERS: LICENSOR WARRANTS THAT THE COPYRIGHT IN AND TO THE   ///
-/// SOFTWARE IS OWNED BY THE LICENSOR OR THAT THE SOFTWARE IS         ///
-/// DISTRIBUTED BY LICENSOR UNDER A VALID CURRENT LICENSE. EXCEPT AS  ///
-/// EXPRESSLY STATED IN THE IMMEDIATELY PRECEDING SENTENCE, THE       ///
-/// SOFTWARE IS PROVIDED BY THE LICENSOR, CONTRIBUTORS AND COPYRIGHT  ///
-/// OWNERS "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, ///
-/// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   ///
-/// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO      ///
-/// EVENT SHALL THE LICENSOR, CONTRIBUTORS OR COPYRIGHT OWNERS BE     ///
-/// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   ///
-/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN ///
-/// CONNECTION WITH THE SOFTWARE.                                     ///
-///                                                                   ///
-/////////////////////////////////////////////////////////////////////////
+# Copyright: (C) 2007-2010 RobotCub Consortium
+# Authors: Marco Maggiali, Marco Randazzo
+# CopyPolicy: Released under the terms of the GNU GPL v2.0.
 
 ///
 /// $Id: controller.h,v 1.1 2008/08/28 09:36:20 vtikha Exp $
