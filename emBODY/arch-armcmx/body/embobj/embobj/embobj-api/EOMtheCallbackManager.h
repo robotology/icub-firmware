81f50fd65dbc62ed9ae43c418ddd0d85e462645f
Marco Accame <marco.accame@iit.it>
2012-05-09 12:30:32 +0000

    added extern "C" in api .h files
    svn path=/trunk/iCub/firmware/; revision=17559

--- emBODY/arch-armcmx/body/embobj/embobj/embobj-api/EOMtheCallbackManager.h
+++ emBODY/arch-armcmx/body/embobj/embobj/embobj-api/EOMtheCallbackManager.h
@@ -20,6 +20,9 @@
 #ifndef _EOMTHECALLBACKMANAGER_H_
 #define _EOMTHECALLBACKMANAGER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /** @file       EOMtheCallbackManager.h
     @brief      This header file implements public interface to the MEE callback manager singleton.
@@ -122,6 +125,10 @@ extern EOMtask * eom_callbackman_GetTask(EOMtheCallbackManager *p);
 /** @}            
     end of group eom_thecallbackmanager  
  **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
 
 #endif  // include-guard
 
