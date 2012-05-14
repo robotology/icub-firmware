018f944632db37a5d3120ba759cb1a9dac46d81c
Marco Accame <marco.accame@iit.it>
2012-05-14 17:09:46 +0000

    added a controller in mc variables. done changes except in upper arm
    svn path=/trunk/iCub/firmware/; revision=17681

--- emBODY/arch-armcmx/body/embobj/embobj/embobj-icub/icub/api/EoMotionControl.h
+++ emBODY/arch-armcmx/body/embobj/embobj/embobj-icub/icub/api/EoMotionControl.h
@@ -574,36 +574,49 @@ typedef struct                  // size is 24+16+0 = 40
 } eOmc_motor_t;                 EO_VERIFYsizeof(eOmc_motor_t, 40); 
  
 
+// -- the definition of a controller containing a given number of joints and motors  
 
-typedef struct                  // size is 4+0 = 4
+/** @typedef    typedef enum eOmc_stateofcontroller_t
+    @brief      contains the possible states of the controller.
+ **/
+typedef enum
 {
+    eomc_stateofcontroller_configuration              = 0,
+    eomc_stateofcontroller_running                    = 1
+} eOmc_stateofcontroller_t;
+ 
+
+typedef struct                  // size is 4+4+0 = 8
+{
+    eOreltime_t                 durationofctrlloop;         /**< the duration of the control-loop in micro-sec. its default is 1000. so far it cannot be changed. */
     uint8_t                     filler04[4];                /**< for future use */
-} eOmc_common_config_t;         EO_VERIFYsizeof(eOmc_common_config_t, 4); 
+} eOmc_controller_config_t;     EO_VERIFYsizeof(eOmc_controller_config_t, 8); 
 
-
-typedef struct                  // size is 4+0 = 4
-{
-    eOenum08_t                  go2runningstate;            /**< it forces a transition to the specified state. use a value from enum eOmc_runningstate_t */
-    uint8_t                     filler03[3];                /**< for future use */
-} eOmc_common_commands_t;       EO_VERIFYsizeof(eOmc_common_commands_t, 4); 
 
 typedef struct                  // size is 1+1+1+1+2+2+0 = 8
 {
-    eObool_t                    jminitted;                  /**< it is eobool_true only when every joint and motor is initted */
+    eObool_t                    alljomoinitted;             /**< it is eobool_true only when every joint and motor is initted */
     uint8_t                     numofjoints;                /**< the number of joints */   
     uint8_t                     numofmotors;                /**< the number of motors */ 
-    eOenum08_t                  runningstate;               /**< it holds a value from enum eOmc_runningstate_t */               
+    eOenum08_t                  stateofcontroller;          /**< it holds a value from enum eOmc_stateofcontroller_t */               
     eO16flags_t                 flagsinittedjoints;         /**< bit position 0 (1, 2, ..) keeps 1 if the joint 0 (1, 2, ..) is fully initted. */
     eO16flags_t                 flagsinittedmotors;         /**< bit position 0 (1, 2, ..) keeps 1 if the motor 0 (1, 2, ..) is fully initted. */ 
-} eOmc_common_status_t;         EO_VERIFYsizeof(eOmc_common_status_t, 8); 
+} eOmc_controller_status_t;     EO_VERIFYsizeof(eOmc_controller_status_t, 8); 
 
 
-typedef struct                  // size is 4+4+8+0 = 16
+typedef struct                  // size is 1+7+0 = 8
 {
-    eOmc_common_config_t        cconfig;                    /**< common configuration for a set of joints-motors */
-    eOmc_common_commands_t      ccmmnds;                    /**< common commands for a set of joints-motors */
-    eOmc_common_status_t        cstatus;                    /**< common status for a set of joints-motors */
-} eOmc_common_t;                EO_VERIFYsizeof(eOmc_common_t, 16); 
+    eOenum08_t                  go2stateofcontroller;       /**< it forces a transition towards the specified state. use a value from enum eOmc_stateofcontroller_t */
+    uint8_t                     filler07[7];                /**< for future use */
+} eOmc_controller_commands_t;   EO_VERIFYsizeof(eOmc_controller_commands_t, 8); 
+
+
+typedef struct                  // size is 8+8+8+0 = 24
+{
+    eOmc_controller_config_t    cconfig;                    /**< controller configuration */
+    eOmc_controller_status_t    cstatus;                    /**< controller status  */
+    eOmc_controller_commands_t  ccmmnds;                    /**< controller commands  */
+} eOmc_controller_t;            EO_VERIFYsizeof(eOmc_controller_t, 24); 
 
 
  
