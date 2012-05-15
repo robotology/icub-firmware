9b7d242c01186e9f37fb0ac2107ec6ed3b64a574
Marco Accame <marco.accame@iit.it>
2012-05-15 15:24:02 +0000

    changed the enum eOmc_stateofcontroller_t: added a value, renamed the existing ones
    svn path=/trunk/iCub/firmware/emBODY/arch-armcmx/body/embobj/embobj/; revision=17703

--- emBODY/eBcode/arch-arm/embobj/embobj-icub/icub-nvscfg/ep-analogsensors/src/eOcfg_nvsEP_as_onestrain_con.h
+++ emBODY/eBcode/arch-arm/embobj/embobj-icub/icub-nvscfg/ep-analogsensors/src/eOcfg_nvsEP_as_onestrain_con.h
@@ -103,6 +103,7 @@ typedef struct                      // size is 56*1 = 56
     eOsnsr_strain_t                 strains[1];
 } eo_cfg_nvsEP_as_onestrain_t;      EO_VERIFYsizeof(eo_cfg_nvsEP_as_onestrain_t, 56);
 
+#warning --> add an enable in eo_cfg_nvsEP_as_onestrain_t whcih tells the ems to forward any msg to the pc104 (?)
     
 // - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
 
