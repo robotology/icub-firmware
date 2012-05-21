6c021d396b571e86c3dfc7e92ada2737df3ccaac
Alessandro Scalzo <alessandro.scalzo@iit.it>
2012-05-21 08:23:57 +0000

    Added speed meter from low-res encoder measures.
    svn path=/trunk/iCub/firmware/; revision=17820

---
+++ emBODY/arch-armcmx/body/embobj/embobj/embobj-api/EOspeedmeter.h
@@ -0,0 +1,70 @@
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOSPEEDMETER_H_
+#define _EOSPEEDMETER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file       EOspeedmeter.h
+    @brief      This header file implements public interface to axis speed measure from slow encoder.
+    @author     alessandro.scalzo@iit.it
+    @date       19/05/2012
+**/
+
+/** @defgroup eo_speedmeter Object EOspeedmeter
+    Does something.
+     
+    @{        
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+ 
+
+/** @typedef    typedef struct EOspeedmeter_hid EOspeedmeter
+    @brief      EOpid is an opaque struct. It is used to implement data abstraction for the 
+                object so that the user cannot see its private fields and he/she is forced to manipulate the
+                object only with the proper public functions. 
+ **/  
+typedef struct EOspeedmeter_hid EOspeedmeter;
+
+
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+// empty-section
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+ 
+ 
+extern EOspeedmeter* eo_speedmeter_New(int32_t impulse_per_revolution, float period);
+
+extern void eo_speedometer_EncoderValid(EOspeedmeter* o, int32_t encoder);
+extern void eo_speedometer_EncoderError(EOspeedmeter* o);
+extern float eo_speedometer_GetSpeed(EOspeedmeter* o);
+
+
+/** @}            
+    end of group eo_pid  
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
---
+++ emBODY/arch-armcmx/body/embobj/embobj/embobj-src/EOspeedmeter_hid.h
@@ -0,0 +1,65 @@
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOSPEEDMETER_HID_H_
+#define _EOSPEEDMETER_HID_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*  @file       EOspeedmeter_hid.h
+    @brief      This header file implements hidden interface to speed meter from slow encoder.
+    @author     alessandro.scalzo@iit.it
+    @date       19/05/2012
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+
+// - declaration of extern public interface ---------------------------------------------------------------------------
+ 
+#include "EOspeedmeter.h"
+
+
+// - #define used with hidden struct ----------------------------------------------------------------------------------
+
+
+
+// - definition of the hidden struct implementing the object ----------------------------------------------------------
+
+/** @struct     EOspeedmeter_hid
+    @brief      Hidden definition. Implements private data used only internally by the 
+                public or private (static) functions of the object and protected data
+                used also by its derived objects.
+ **/  
+ 
+struct EOspeedmeter_hid 
+{
+    float period;
+    float frequency;
+    int32_t impulse_per_revolution;
+    int32_t impulse_per_revolution_by_2;
+    float time_from_last_reading;
+    int32_t last_reading;
+    float speed;
+    float speed_new;
+    eObool_t first_reading;
+}; 
+
+
+// - declaration of extern hidden functions ---------------------------------------------------------------------------
+
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+ 
+#endif  // include-guard
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
+
