59c3b65c44b07dfc2b6aa3a00ad75ed66f0c6de6
Alberto Cardellino <alberto.cardellino@iit.it>
2012-05-22 08:22:19 +0000

    _AC_
    svn path=/trunk/iCub/firmware/; revision=17840

---
+++ emBODY/arch-host/demo/hostTransceiver2/src/icub-nvscfg/ep-skin/usrcbk/eOcfg_nvsEP_sk_usrcbk_ebx.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Valentina Gaggero
+ * email:   valentina.gaggero@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* @file       eOcfg_nvsEP_sk_usrcbk_ebx.c
+    @brief      This file keeps the user-defined functions used in every ems board ebx for endpoint mc
+    @author     valentina.gaggero@iit.it
+    @date       05/04/2012
+**/
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "stdlib.h" 
+#include "string.h"
+#include "stdio.h"
+
+#include "EoCommon.h"
+#include "EOarray.h"
+#include "EOnv_hid.h"
+
+#include "EoSkin.h"
+#include "eOcfg_nvsEP_sk.h"
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+
+//#include "eOcfg_nvsEP_mngmnt_usr_hid.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+
+//sk-init
+
+
+//sk-update
+extern void eo_cfg_nvsEP_sk_hid_UPDT_sstatus__arrayof10canframe(uint16_t n, const EOnv* nv, const eOabstime_t time, const uint32_t sign)
+{
+	EOarray_of_10canframes *sk_array = (EOarray_of_10canframes *)nv->rem;
+	eOutil_canframe_t *canframe;
+	int i, j;
+
+	printf("--- ARRAY SIZE = %d  ---- \n", sk_array->head.size);
+	for(i=0; i<sk_array->head.size; i++)
+	{
+		canframe = &sk_array->data[i];
+		printf("SID = %0x  ", canframe->id);
+		printf("DATA: ");
+		for(j=0; j<canframe->size;j++)
+		{
+			printf(" %0x", canframe->data[j]);
+		}
+		printf("\n\n");
+
+	}
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
