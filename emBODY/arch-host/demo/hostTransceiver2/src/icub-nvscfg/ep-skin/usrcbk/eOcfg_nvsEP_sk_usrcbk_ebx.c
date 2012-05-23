ac91a221c57068ca418c9a8a9ff0224905dec110
Alberto Cardellino <alberto.cardellino@iit.it>
2012-05-23 14:26:00 +0000

    _AC_
    svn path=/trunk/iCub/firmware/; revision=17894

--- emBODY/arch-host/demo/hostTransceiver2/src/icub-nvscfg/ep-skin/usrcbk/eOcfg_nvsEP_sk_usrcbk_ebx.c
+++ emBODY/arch-host/demo/hostTransceiver2/src/icub-nvscfg/ep-skin/usrcbk/eOcfg_nvsEP_sk_usrcbk_ebx.c
@@ -103,12 +103,12 @@ extern void eo_cfg_nvsEP_sk_hid_UPDT_sstatus__arrayof10canframe(uint16_t n, cons
 	printf("--- ARRAY SIZE = %d  ---- \n", sk_array->head.size);
 	for(i=0; i<sk_array->head.size; i++)
 	{
-		canframe = (eOutil_canframe_t*) &sk_array->data[i];
-		printf("SID = %0x  ", canframe->id);
-		printf("DATA: ");
+		canframe = (eOutil_canframe_t*) &sk_array->data[i*sizeof(eOutil_canframe_t)];
+		printf("SID = 0x%0x  ", canframe->id);
+		printf("DATA: 0x");
 		for(j=0; j<canframe->size;j++)
 		{
-			printf(" %0x", canframe->data[j]);
+			printf("%0x", canframe->data[j]);
 		}
 		printf("\n\n");
 
