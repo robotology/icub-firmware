42c208635ce417cdf28734df0c0f399d648451c3
Valentina Gaggero <valentina.gaggero@iit.it>
2012-06-05 08:55:58 +0000

    synchronized with embobj/embobj svn revision 18200 (added EOspeedcurve obj)
    svn path=/trunk/iCub/firmware/emBODY/any-arch/sys/embobj/; revision=18201

--- emBODY/eBcode/arch-arm/embobj/mc/src/EOspeedcurve_hid.h
+++ emBODY/eBcode/arch-arm/embobj/mc/src/EOspeedcurve_hid.h
@@ -1,16 +1,16 @@
 
 // - include guard ----------------------------------------------------------------------------------------------------
-#ifndef _EOSPEEDMETER_HID_H_
-#define _EOSPEEDMETER_HID_H_
+#ifndef _EOSPEEDCURVE_HID_H_
+#define _EOSPEEDCURVE_HID_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/*  @file       EOspeedmeter_hid.h
-    @brief      This header file implements hidden interface to speed meter from slow encoder.
+/* @file        EOtrajectory_hid.h
+    @brief      This header file implements hidden interface to motor minumum jerk trajectory generation.
     @author     alessandro.scalzo@iit.it
-    @date       19/05/2012
+    @date       23/03/2012
  **/
 
 
@@ -20,33 +20,27 @@ extern "C" {
 
 // - declaration of extern public interface ---------------------------------------------------------------------------
  
-#include "EOspeedmeter.h"
+#include "EOspeedcurve.h"
 
 
 // - #define used with hidden struct ----------------------------------------------------------------------------------
 
-#define DELTA_THR 4
 
 // - definition of the hidden struct implementing the object ----------------------------------------------------------
 
-/** @struct     EOspeedmeter_hid
+/** @struct     EOtrajectory_hid
     @brief      Hidden definition. Implements private data used only internally by the 
                 public or private (static) functions of the object and protected data
                 used also by its derived objects.
  **/  
-
-struct EOspeedmeter_hid 
+ 
+struct EOspeedcurve_hid 
 {
-    int32_t FREQUENCY;
-    int32_t FREQUENCYxTHR;
-    int32_t impulse_per_revolution;
-    int32_t impulse_per_revolution_by_2;
-    int32_t time_from_last_reading;
-    int32_t last_valid_reading;
-    int32_t last_reading;
-    int32_t speed;
-    int32_t delta;
-    eObool_t first_reading;
+    float v;
+    float vf;
+    float acc;
+    float rabbit;
+    eObool_t done;
 }; 
 
 
