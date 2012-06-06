7c0066bf21f098cba9d8082b6702dca27ae38e38
Marco Accame <marco.accame@iit.it>
2012-06-06 08:47:10 +0000

    changed the way a macro is called because gcc does not like it
    svn path=/trunk/iCub/firmware/emBODY/any-arch/sys/embobj/; revision=18252

--- emBODY/eBcode/arch-arm/embobj/icub-nvscfg/ep-motioncontrol/src/eOcfg_nvsEP_mc_lowerarm_con.c
+++ emBODY/eBcode/arch-arm/embobj/icub-nvscfg/ep-motioncontrol/src/eOcfg_nvsEP_mc_lowerarm_con.c
@@ -376,7 +376,7 @@ static const eOmc_controller_t s_eo_cfg_nvsEP_mc_lowerarm_con_defaultvalue =
 
 #define CMACRO_EXTERNALPREFIX_GETID                EOK_cfg_nvsEP_mc_any_con_bodypart_NVID_c00
 
-#define CMACRO_EXTERNALPREFIX_CONST                s_eo_cfg_nvsEP_mc_lowerarm_con_defaultvalue
+#define CMACRO_EXTERNALPREFIX_CONST                s_eo_cfg_nvsEP_mc_lowerarm_con_
 
 #define CMACRO_PSTR    _lowerarm
 #define CMACRO_CSTR    _c00
