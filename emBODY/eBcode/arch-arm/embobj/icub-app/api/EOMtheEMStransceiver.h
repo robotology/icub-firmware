1ac3cd24278af8c2c65865547ffd6c6ecd5629d0
Marco Accame <marco.accame@iit.it>
2012-07-06 09:49:17 +0000

    added icub-app in sys
    svn path=/trunk/iCub/firmware/emBODY/any-arch/sys/embobj/; revision=19174

---
+++ emBODY/eBcode/arch-arm/embobj/icub-app/api/EOMtheEMStransceiver.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOMTHEEMSTRANSCEIVER_H_
+#define _EOMTHEEMSTRANSCEIVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file       EOMtheEMStransceiver.h
+	@brief      This header file implements public interface to the EMS transceiver singleton.
+ 	@author     marco.accame@iit.it
+	@date       05/21/2012
+ **/
+
+/** @defgroup eom_theemstransceiver Singleton EOMtheEMStransceiver 
+    
+    The .....  
+  
+    @{		
+ **/
+
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+#include "EOtransceiver.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+// empty-section
+  
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+/** @typedef    typedef struct EOMtheEMStransceiver_hid EOMtheEMStransceiver
+    @brief      EOMtheEMStransceiver is an opaque struct. It is used to implement data abstraction for the 
+                object so that the user cannot see its private fields so that he/she is forced to manipulate the
+                object only with the proper public functions
+ **/  
+typedef struct EOMtheEMStransceiver_hid EOMtheEMStransceiver;
+
+
+
+/**	@typedef    typedef struct eOemstransceiver_cfg_t 
+ 	@brief      Contains the configuration for the EOMtheEMStransceiver. 
+ **/
+typedef struct
+{
+    const EOconstvector*    vectorof_endpoint_cfg;
+    eOuint16_fp_uint16_t    hashfunction_ep2index;
+    eOipv4addr_t            hostipv4addr;
+    eOipv4port_t            hostipv4port; 
+    eo_transceiver_sizes_t  sizes;  
+} eOemstransceiver_cfg_t;
+
+
+    
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+
+// in here we shall place configuration of rop dimensione etc. now in defines such as EOK_BOARDTRANSCEIVER_capacityofpacket etc.
+extern const eOemstransceiver_cfg_t eom_emstransceiver_DefaultCfg; // = {.hostipv4addr = EO_COMMON_IPV4ADDR(10, 0, 1, 200), .hostipv4port = 12345};
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+
+
+/** @fn         extern EOMtheEMStransceiver * eom_emstransceiver_Initialise(const eOemstransceiver_cfg_t *emstransceivercfg)
+    @brief      Initialise the EOMtheEMStransceiver singleton 
+    @arg        emstransceivercfg       The configuration. NULL is OK.
+    @return     The handle to the EOMtheEMStransceiver
+ **/
+extern EOMtheEMStransceiver * eom_emstransceiver_Initialise(const eOemstransceiver_cfg_t *emstransceivercfg);
+
+
+/** @fn         extern EOMtheEMStransceiver * eom_emstransceiver_GetHandle(void)
+    @brief      Retrieve the EOMtheEMStransceiver singleton 
+    @return     The handle to the EOMtheEMStransceiver
+ **/
+extern EOMtheEMStransceiver * eom_emstransceiver_GetHandle(void);
+
+
+/** @fn         extern EOtransceiver* eom_emstransceiver_GetTransceiver(EOMtheEMStransceiver* p)
+    @brief      Retrieves the handle to the transceiver instantiated in the EOMtheEMStransceiver 
+    @arg        p                       The handle to the EOMtheEMStransceiver
+    @return     The requ    ired EOtransceiver
+ **/
+extern EOtransceiver* eom_emstransceiver_GetTransceiver(EOMtheEMStransceiver* p);
+
+
+extern EOnvsCfg* eom_emstransceiver_GetNVScfg(EOMtheEMStransceiver* p);
+
+
+extern eOresult_t eom_emstransceiver_Parse(EOMtheEMStransceiver* p, EOpacket* rxpkt, uint16_t *numberofrops, eOabstime_t* txtime);
+
+extern eOresult_t eom_emstransceiver_Form(EOMtheEMStransceiver* p, EOpacket** txpkt, uint16_t *numberofrops);
+
+
+/** @}            
+    end of group eom_theemstransceiver  
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
---
+++ emBODY/eBcode/arch-arm/embobj/icub-app/cfg/overridden_configurator.c
@@ -0,0 +1,114 @@
+
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+
+#include "stdlib.h"
+#include "string.h"
+#include "EoCommon.h"
+
+#include "EOMtheEMSappl.h"
+#include "EOMtask.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "EOMtheEMSconfigurator.h"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "EOMtheEMSconfigurator_hid.h" 
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables, but better using _get(), _set() 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+extern void eom_emsconfigurator_hid_userdef_DoJustAfterPacketParsing(EOMtheEMSconfigurator *p)
+{
+    static uint8_t num = 0;
+    // just for debug
+    if(0 == p->numofrxrops)
+    {
+        num++;
+        if(3 == num)
+        {
+            num = 0;
+//            eom_emsappl_ProcessEvent(eom_emsappl_GetHandle(), eo_sm_emsappl_EVgo2run);
+            eom_task_SetEvent(eom_emsconfigurator_GetTask(eom_emsconfigurator_GetHandle()), emsconfigurator_evt_go2runner);
+        }
+    }
+
+} 
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
