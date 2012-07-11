beaeda2b03d459a1c3b678b102479c51b3ed61b4
Valentina Gaggero <valentina.gaggero@iit.it>
2012-07-11 12:10:51 +0000

    added proto test
    svn path=/trunk/iCub/firmware/; revision=19255

---
+++ emBODY/arch-armcmx/apps/tests/app-skeleton-ems-V06/app_extra/proto_test.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Alessandro Scalzo, Valentina Gaggero
+ * email:   alessandro.scalzo@iit.it, valentina.gaggero@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _PROTO_TEST_H_
+#define _PROTO_TEST_H_
+
+// - doxy begin -------------------------------------------------------------------------------------------------------
+// empty-section
+
+// - external dependencies --------------------------------------------------------------------------------------------
+// empty-section
+
+// - public #define  --------------------------------------------------------------------------------------------------
+// empty-section
+ 
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+// empty-section
+    
+// - declaration of extern public variables, ...deprecated: better using use _get/_set instead ------------------------
+// empty-section
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+extern eOresult_t proto_test_parse(uint8_t *payload);
+
+// - doxy end ---------------------------------------------------------------------------------------------------------
+// empty-section
+
+#endif  // include-guard
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
