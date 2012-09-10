3a72600d3f8bbef6b13fc48288e1f4c83909c15c
Marco Accame <marco.accame@iit.it>
2012-09-10 10:19:02 +0000

    added methods to retrieve information about EPs and NVids of network variables inside a board
    svn path=/trunk/iCub/firmware/emBODY/any-arch/sys/embobj/; revision=20104

---
+++ emBODY/eBcode/arch-arm/embobj/icub-nvscfg/board-eps/eOcfg_EPs_board.c
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2012 iCub Facility - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* @file       eOcfg_EPs_board.c
+    @brief      This file keeps ...
+    @author     marco.accame@iit.it
+    @date       09/10/2012
+**/
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "stdlib.h" 
+#include "string.h"
+#include "stdio.h"
+
+#include "EOnvsCfg.h"
+
+#include "EOconstvector_hid.h"
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "eOcfg_EPs_board.h"
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+extern uint16_t eo_cfg_nvsEP_board_EPs_numberof_get(const EOconstvector* constvectorof_nvscfgep)
+{
+    return(eo_constvector_Size(constvectorof_nvscfgep));
+}
+
+extern eOnvscfg_EP_t* eo_cfg_nvsEP_board_EPs_cfg_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex)
+{
+    return((eOnvscfg_EP_t*) eo_constvector_At(constvectorof_nvscfgep, epindex));
+}
+
+extern eOnvEP_t eo_cfg_nvsEP_board_EPs_endpoint_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex)
+{
+    eOnvEP_t endpoint = EOK_uint16dummy;
+    eOnvscfg_EP_t* epcfg = (eOnvscfg_EP_t*) eo_constvector_At(constvectorof_nvscfgep, epindex);
+    if(NULL != epcfg)
+    {
+        endpoint = epcfg->endpoint;
+    }
+    
+    return(endpoint);
+}
+
+extern uint16_t eo_cfg_nvsEP_board_NVs_total_numberof_get(const EOconstvector* constvectorof_nvscfgep)
+{    
+    uint16_t number = 0;
+    uint16_t tmp = 0;
+    eOsizecntnr_t i;
+    eOsizecntnr_t numofeps;
+    eOnvscfg_EP_t *epscfg = NULL;
+    
+    numofeps =  eo_constvector_Size(constvectorof_nvscfgep);
+    for(i=0; i<numofeps; i++)
+    {
+        epscfg = (eOnvscfg_EP_t*) eo_constvector_At(constvectorof_nvscfgep, i);
+        if(NULL != epscfg)
+        {       
+            tmp = eo_constvector_Size(epscfg->constvector_of_treenodes_EOnv_con);
+            number += tmp;
+        }
+    }
+    return(number);
+}
+
+extern uint16_t eo_cfg_nvsEP_board_NVs_endpoint_numberof_get(const EOconstvector* constvectorof_nvscfgep, eOnvEP_t ep)
+{  
+    uint16_t number = 0;
+    eOsizecntnr_t i;
+    eOnvscfg_EP_t *epscfg = NULL;
+       
+    i = eo_cfg_nvsEP_eb1_hashfunction_ep2index(ep);
+    
+    if(EOK_uint16dummy == i)
+    {
+        return(0);
+    }
+    
+    epscfg = (eOnvscfg_EP_t*) eo_constvector_At(constvectorof_nvscfgep, i);
+    
+    if(NULL == epscfg)
+    {
+        return(0);
+    }
+    
+    number = eo_constvector_Size(epscfg->constvector_of_treenodes_EOnv_con);
+
+    return(number);
+}
+
+
+extern eOnvID_t eo_cfg_nvsEP_board_NVs_endpoint_NVID_get(const EOconstvector* constvectorof_nvscfgep, eOnvEP_t ep, uint16_t nvindex)
+{
+    const EOconstvector* constvectorof_nvscfgep = eo_cfg_EPs_vectorof_eb1;
+    
+    eOnvID_t nvid = EOK_uint16dummy;
+    eOsizecntnr_t i;
+    eOsizecntnr_t number;
+    eOnvscfg_EP_t *epscfg = NULL;
+    EOtreenode *treenode = NULL;
+    EOnv_con_t *nvcon;
+       
+    i = eo_cfg_nvsEP_eb1_hashfunction_ep2index(ep);
+    
+    if(EOK_uint16dummy == i)
+    {
+        return(nvid);
+    }
+    
+    epscfg = (eOnvscfg_EP_t*) eo_constvector_At(constvectorof_nvscfgep, i);
+    
+    if(NULL == epscfg)
+    {
+        return(nvid);
+    }
+    
+    number = eo_constvector_Size(epscfg->constvector_of_treenodes_EOnv_con);
+    if(nvindex >= number)
+    {
+        return(nvid);
+    }
+    
+    treenode = (EOtreenode*) eo_constvector_At(epscfg->constvector_of_treenodes_EOnv_con, nvindex);
+
+    if(NULL == treenode)
+    {
+        return(nvid);
+    }
+    
+    nvcon = (EOnv_con_t*) eo_treenode_GetData(treenode);
+    
+    return(nvcon->id);
+}
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
