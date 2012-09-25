9228fcb13ca78a5776eec36c812264f9046f897f
Marco Accame <marco.accame@iit.it>
2012-09-25 07:27:53 +0000

    No commit message
    svn path=/trunk/iCub/firmware/emBODY/arch-armcmx/body/embobj/embobj/; revision=20311

--- emBODY/eBcode/arch-arm/embobj/embobj-icub/icub-nvscfg/cfg-eps-ebx/eOcfg_EPs_board.h
+++ emBODY/eBcode/arch-arm/embobj/embobj-icub/icub-nvscfg/cfg-eps-ebx/eOcfg_EPs_board.h
@@ -47,7 +47,8 @@ extern "C" {
 #include "EOconstvector.h"
 #include "EOnvsCfg.h"
 
-
+#warning "This include (eOcfg_EPs_allboards) will generate chaos on EMS code!"
+#include "eOcfg_EPs_allboards.h"
 
 // - public #define  --------------------------------------------------------------------------------------------------
 
@@ -61,18 +62,38 @@ extern "C" {
 
 // - declaration of extern public functions ---------------------------------------------------------------------------
 
+// Legend:
+// eOnvEP_t ep 		= unique EP identifier with semantic
+// uint16_t epindex = index of ith EP inside that particular board
+// eOnvID_t nv		= unique NV identifier with semantic
+// eOnvEP_t nvindex	= index of ith NV inside that particular EP
+
+// return constvectorof_nvscfgep for a given board
+extern EOconstvector* eo_cfg_nvsEP_board_EPs_nvscfgep_get(uint8_t boardNum);
+
+// return number of EP handled by this board
 extern uint16_t eo_cfg_nvsEP_board_EPs_numberof_get(const EOconstvector* constvectorof_nvscfgep);
 
+// return to the configuration structure of the ith EP
 extern eOnvscfg_EP_t* eo_cfg_nvsEP_board_EPs_cfg_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex);
 
+// return unique eOnvEP_t EP identifier from epindex
 extern eOnvEP_t eo_cfg_nvsEP_board_EPs_endpoint_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex);
 
+// Return epindex from eOnvEP_t ep, this is board-dependent
+extern uint16_t eo_cfg_nvsEP_board_EPs_epindex_get(uint8_t boardNum, eOnvEP_t ep);
+
+// return the number of NVs handled by the whole board (sum of all EPs)
 extern uint16_t eo_cfg_nvsEP_board_NVs_total_numberof_get(const EOconstvector* constvectorof_nvscfgep);
 
-extern uint16_t eo_cfg_nvsEP_board_NVs_endpoint_numberof_get(const EOconstvector* constvectorof_nvscfgep, eOnvEP_t ep);
+// return the number of NVs contained in the EP ep
+extern uint16_t eo_cfg_nvsEP_board_NVs_endpoint_numberof_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex);
 
-extern eOnvID_t eo_cfg_nvsEP_board_NVs_endpoint_NVID_get(const EOconstvector* constvectorof_nvscfgep, eOnvEP_t ep, uint16_t nvindex);
+// return the NVID of the i-th NV of the EP ep
+extern eOnvID_t eo_cfg_nvsEP_board_NVs_endpoint_NVID_get(const EOconstvector* constvectorof_nvscfgep, uint16_t epindex, uint16_t nvindex);
 
+// Return the nvindex from the eOnvID_t
+extern uint16_t eo_cfg_nvsEP_board_NVs_endpoint_Nvindex_get(const eOnvscfg_EP_t* EPcfg, eOnvID_t nvid);
 
 /** @}            
     end of group eo_asdgfgr1111234fr  
