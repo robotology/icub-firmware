32fe3e1cdce5f08830d8610a1c87b11ce641e6f0
Marco Accame <marco.accame@iit.it>
2012-11-20 10:42:20 +0000

    added a mutex for YARP
    svn path=/trunk/iCub/firmware/emBODY/any-arch/sys/embobj/; revision=21477

---
+++ emBODY/eBcode/arch-arm/embobj/core-yee/src/EOYmutex.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2012 iCub Facility - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame, Alberto Cardellino
+ * email:   marco.accame@iit.it, alberto.cardellino@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "stdlib.h"
+#include "EoCommon.h"
+#include "string.h"
+#include "EOtheMemoryPool.h"
+#include "EOtheErrorManager.h"
+#include "EOVmutex_hid.h"
+
+// we use YARP
+ADD_YARP_REF:
+#include ".h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "EOYmutex.h"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "EOYmutex_hid.h" 
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables, but better using _get(), _set() 
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+
+// virtual
+static eOresult_t s_eoy_mutex_take(void *p, eOreltime_t tout);
+// virtual
+static eOresult_t s_eoy_mutex_release(void *p);
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+
+static const char s_eobj_ownname[] = "EOYmutex";
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+extern EOYmutex* eoy_mutex_New(void) 
+{
+    EOYmutex *retptr = NULL;    
+
+    // i get the memory for the yarp mutex object
+    retptr = eo_mempool_GetMemory(eo_mempool_GetHandle(), eo_mempool_align_32bit, sizeof(EOYmutex), 1);
+    
+    // i get the base mutex
+    retptr->mutex = eov_mutex_hid_New();
+
+    // init its vtable
+    eov_mutex_hid_SetVTABLE(retptr->mutex, s_eoy_mutex_take, s_eoy_mutex_release); 
+    
+    // i get a new yarp mutex
+    was:
+    //retptr->yarpmutex = osal_mutex_new();
+    becomes:
+    ADD_YARP_REF
+    retptr->yarpmutex = ..... non piu' osal_mutex_new() ma qualcosa che ritorni un handle al mutex
+
+    // need to check because yarp may return NULL
+    eo_errman_Assert(eo_errman_GetHandle(), (NULL != retptr->yarpmutex), s_eobj_ownname, "osal cannot give a mutex");
+    
+    return(retptr);    
+}
+
+
+extern eOresult_t eoy_mutex_Take(EOYmutex *m, eOreltime_t tout)
+{
+    if(NULL == m)
+    {
+        return(eores_NOK_nullpointer);
+    }
+    
+    return(s_eoy_mutex_take(m, tout));
+}
+
+
+extern eOresult_t eoy_mutex_Release(EOYmutex *m)
+{
+    if(NULL == m)
+    {
+        return(eores_NOK_nullpointer);
+    }
+    
+    return(s_eoy_mutex_release(m));
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+static eOresult_t s_eoy_mutex_take(void *p, eOreltime_t tout) 
+{
+    EOYmutex *m = (EOYmutex *)p;
+    // p it is never NULL because the base function calls checks it before calling this function, then osal will
+    // check m->osalmutex vs NULL
+    was:
+    //return((eOresult_t)osal_mutex_take(m->osalmutex, tout));
+    becomes:
+    ADD_YARP_REF
+}
+
+
+static eOresult_t s_eoy_mutex_release(void *p) 
+{
+    EOYmutex *m = (EOYmutex *)p;
+    // p it is never NULL because the base function calls checks it before calling this function, then osal will
+    // check m->osalmutex vs NULL
+    was:
+    //return((eOresult_t)osal_mutex_release(m->osalmutex));
+    becomes:
+    ADD_YARP_REF
+}
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
