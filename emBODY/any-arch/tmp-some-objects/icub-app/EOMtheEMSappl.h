f1549ec3b08013227e8beb5cc6b2361f0a5219c4
Marco Accame <marco.accame@iit.it>
2012-11-28 16:44:51 +0000

    contains changed to some objects in sys: 1. some cleaning 2. mutex in transceiver, 3. support for sending rops w/ signature
    svn path=/trunk/iCub/firmware/; revision=21667

---
+++ emBODY/any-arch/tmp-some-objects/icub-app/EOMtheEMSappl.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOMTHEEMSAPPL_H_
+#define _EOMTHEEMSAPPL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file       EOMtheEMSappl.h
+	@brief      This header file implements public interface to the EMS application singleton.
+ 	@author     marco.accame@iit.it
+	@date       05/21/2012
+ **/
+
+/** @defgroup eom_theemsappl Singleton EOMtheEMSappl 
+    
+    The .....  
+  
+    @{		
+ **/
+
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+#include "eEcommon.h"
+#include "EOsm.h"
+#include "eOcfg_sm_EMSappl.h"
+
+// - public #define  --------------------------------------------------------------------------------------------------
+// empty-section
+  
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+/** @typedef    typedef struct EOMtheEMSappl_hid EOMtheEMSappl
+    @brief      EOMtheEMSappl is an opaque struct. It is used to implement data abstraction for the 
+                object so that the user cannot see its private fields so that he/she is forced to manipulate the
+                object only with the proper public functions
+ **/  
+typedef struct EOMtheEMSappl_hid EOMtheEMSappl;
+
+
+
+/**	@typedef    typedef struct eOemsappl_cfg_t 
+ 	@brief      Contains the configuration for the EOMtheEMSappl. 
+ **/
+typedef struct
+{
+    const eEmoduleInfo_t*   emsappinfo;
+    eOipv4addr_t            hostipv4addr; 
+} eOemsappl_cfg_t;
+
+
+    
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+
+extern const eOemsappl_cfg_t eom_emsappl_DefaultCfg; // = {.emsappinfo = NULL, .hostipv4addr = EO_COMMON_IPV4ADDR(10, 0, 1, 200), .hostipv4port = 12345};
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+
+
+/** @fn         extern EOMtheEMSappl * eom_emsappl_Initialise(const eOemsappl_cfg_t *emsapplcfg)
+    @brief      Initialise the EOMtheEMSappl singleton 
+    @arg        emsapplcfg       The configuration. NULL is OK.
+    @return     The handle to the EOMtheEMSappl
+ **/
+
+extern EOMtheEMSappl * eom_emsappl_Initialise(const eOemsappl_cfg_t *emsapplcfg);
+
+
+/** @fn         extern EOMtheEMSappl * eom_emsappl_GetHandle(void)
+    @brief      Retrieve the EOMtheEMSappl singleton 
+    @return     The handle to the EOMtheEMSappl
+ **/
+extern EOMtheEMSappl * eom_emsappl_GetHandle(void);
+
+
+//extern EOsm* eom_emsappl_GetStateMachine(EOMtheEMSappl *p) ;
+
+
+extern eOresult_t eom_emsappl_ProcessEvent(EOMtheEMSappl *p, eOsmEventsEMSappl_t ev);
+
+/** @fn         eOresult_t eom_emsappl_ProcessGo2stateRequest(EOMtheEMSappl *p, eOemsappl_applstate_t newstate)
+    @brief      say the application to go to new state as soon as possible. In some case the tarnsition can not be done immediatly,
+                so it is necessary waiting current activity finished.
+                if new state is equal to current state any trnasition is performed, and return ok.
+                if current state is error, this function returns eores_NOK_unsupported, becoause is not possible live error state.
+                if new state is error, this function returns eores_NOK_unsupported, becoause is not possible go to aerror state with command, but only because an interal error happened.
+    @arg        p           EOMtheEMSappl handle
+                newstate    the new state of application
+    @return     eores_OK on success
+                eores_NOK_unsupported if transition is not possible
+                eores_NOK_nullpointer if p is null.
+    @note       the user can overriden eom_emsappl_hid_userdef_ProcessGo2stateRequest, in order to notify to its obj transition toward newstate.
+ **/
+extern eOresult_t eom_emsappl_ProcessGo2stateRequest(EOMtheEMSappl *p, eOsmStatesEMSappl_t newstate);
+
+ 
+extern eOresult_t eom_emsappl_GetCurrentState(EOMtheEMSappl *p, eOsmStatesEMSappl_t *currentstate);
+
+
+
+
+/** @}            
+    end of group eom_theemsappl  
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
---
+++ emBODY/any-arch/tmp-some-objects/prot/EOnvsCfg.h
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EONVSCFG_H_
+#define _EONVSCFG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file       EOnvsCfg.h
+    @brief      This header file implements public interface to the configuration of netvars.
+    @author     marco.accame@iit.it
+    @date       09/06/2011
+**/
+
+/** @defgroup eo_n Objevct EOnvsCfg
+    Offers methods for reading and writing some data on RAM, ROM and storage and for doing some
+    actions upon such operations. It is to be manipulated only via its public methods.
+    It is not created but loaded as a constant object from a configuration mapped in ROM
+      
+    @{        
+ **/
+
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+#include "EOnv.h"
+#include "EOconstvector.h"
+#include "EOtreenode.h"
+#include "EOVmutex.h"
+#include "EOVstorage.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+
+
+
+
+  
+
+// - declaration of public user-defined types -------------------------------------------------------------------------    
+
+
+/** @typedef    typedef const struct EOnvsCfg_hid EOnvsCfg
+    @brief      EOaction is an opaque struct. It is used to implement data abstraction for the datagram 
+                object so that the user cannot see its private fields and he/she is forced to manipulate the
+                object only with the proper public functions. 
+ **/  
+typedef struct EOnvsCfg_hid EOnvsCfg;
+
+
+typedef enum
+{
+    eo_nvscfg_ownership_local       = 0,
+    eo_nvscfg_ownership_remote      = 1
+} eOnvscfgOwnership_t; 
+
+typedef enum
+{
+    eo_nvscfg_devicesownership_none                  = 0,
+    eo_nvscfg_devicesownership_onelocal              = 1,
+    eo_nvscfg_devicesownership_onelocalsomeremote    = 2,
+    eo_nvscfg_devicesownership_someremote            = 3
+} eOnvscfgDevicesOwnership_t;
+
+typedef struct
+{
+    const EOconstvector*    vectorof_endpoint_cfg;
+    eOuint16_fp_uint16_t    hashfunction_ep2index;
+} eOnvscfg_setof_EPs_t;
+
+typedef struct                      // size is 24 bytes
+{
+    eOnvEP_t                        endpoint;
+    uint16_t                        sizeof_endpoint_data;
+    eOuint16_fp_uint16_t            hashfunction_id2index;
+    const EOconstvector* const      constvector_of_treenodes_EOnv_con;
+    const EOconstvector* const      constvector_of_EOnv_usr;
+    eOvoid_fp_uint16_voidp_voidp_t  endpoint_data_init;
+    eOvoid_fp_uint16_voidp_voidp_t  endpoint_data_retrieve;
+} eOnvscfg_EP_t;
+
+typedef enum
+{
+    eo_nvscfg_protection_none               = 0,    /**< we dont protect vs concurrent access at all */
+    eo_nvscfg_protection_one_per_object     = 1,    /**< all the NVs in the object share the same mutex: reduced use of memory but minimum concurrency */
+    eo_nvscfg_protection_one_per_device     = 2,    /**< all the NVs in a device inside the object share the same mutex */
+    eo_nvscfg_protection_one_per_endpoint   = 3,    /**< all the NVs in an endpoint inside each device inside the object share the same mutex */
+    eo_nvscfg_protection_one_per_netvar     = 4     /**< every NV has its own mutex: heavy use of memory but maximum concurrency */
+} eOnvscfg_protection_t;
+
+    
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+
+
+
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+extern EOnvsCfg* eo_nvscfg_New(uint16_t ndevices, EOVstorageDerived* stg, eOnvscfg_protection_t prot, eov_mutex_fn_mutexderived_new mtxnew);
+
+extern eOresult_t eo_nvscfg_PushBackDevice(EOnvsCfg* p, eOnvscfgOwnership_t ownership, eOipv4addr_t ipaddress, eOuint16_fp_uint16_t hashfn_ep2index, uint16_t nendpoints);
+
+
+extern uint16_t eo_nvscfg_GetIndexOfLocalDevice(EOnvsCfg* p);
+
+//extern eOresult_t eo_nvscfg_ondevice_PushBackEndpoint(EOnvsCfg* p, uint16_t ondevindex, eOnvEP_t endpoint, eOuint16_fp_uint16_t hashfn_id2index, const EOconstvector* treeofnvs_con, const EOconstvector* datanvs_usr, uint32_t datanvs_size, eOvoid_fp_uint16_voidp_voidp_t datanvs_init, EOVmutexDerived* mtx);
+
+extern eOresult_t eo_nvscfg_ondevice_PushBackEP(EOnvsCfg* p, uint16_t ondevindex, eOnvscfg_EP_t *cfgofep);
+
+
+extern eOresult_t eo_nvscfg_data_Initialise(EOnvsCfg* p);
+
+// removed. it was useful to retrieve ram of endpoin, but much better using the variable instead
+//extern void * eo_nvscfg_localdev_endpoint_GetRAM(EOnvsCfg* p, eOnvEP_t endpoint);
+
+
+extern eOresult_t eo_nvscfg_GetIndices(EOnvsCfg* p, 
+                                        eOipv4addr_t ip, eOnvEP_t ep, eOnvID_t id, 
+                                        uint16_t *ipindex, uint16_t *epindex, uint16_t *idindex); 
+
+
+extern EOtreenode* eo_nvscfg_GetTreeNode(EOnvsCfg* p, uint16_t ondevindex, uint16_t onendpointindex, uint16_t onidindex);
+
+
+extern EOnv* eo_nvscfg_GetNV(EOnvsCfg* p, uint16_t ondevindex, uint16_t onendpointindex, uint16_t onidindex, EOtreenode* treenode, EOnv* nvtarget);
+
+
+
+/** @}            
+    end of group eo_nv 
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
