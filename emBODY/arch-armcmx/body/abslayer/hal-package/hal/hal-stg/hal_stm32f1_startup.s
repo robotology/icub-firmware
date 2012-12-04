0bb3c5463169e7b5fd6b636babbdf694aa219a5f
Marco Accame <marco.accame@iit.it>
2012-12-04 13:58:23 +0000

    corrected inclusion of SystemInit()
    svn path=/trunk/iCub/firmware/; revision=21741

--- emBODY/arch-armcmx/body/abslayer/hal-package/hal/hal-stg/hal_stm32f1_startup.s
+++ emBODY/arch-armcmx/body/abslayer/hal-package/hal/hal-stg/hal_stm32f1_startup.s
@@ -185,10 +185,10 @@ __Vectors_Size  EQU  __Vectors_End - __Vectors
 ; Reset handler
 Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
-;        IMPORT  SystemInit
+        IMPORT  SystemInit
         IMPORT  __main
-;                 LDR     R0, =SystemInit
-;                 BLX     R0
+                 LDR     R0, =SystemInit
+                 BLX     R0
                  LDR     R0, =__main
                  BX      R0
                  ENDP
