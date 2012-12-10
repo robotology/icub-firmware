4f3e4db4ac78ed6c65f743c2199851403e25f0dd
Alberto Cardellino <alberto.cardellino@iit.it>
2012-12-10 09:41:43 +0000

    Update to virtual EMS demo
    svn path=/trunk/iCub/firmware/; revision=21796

---
+++ emBODY/arch-host/demo/boardTransceiver2/src/eOcfg_nvsEP_mc_overridden.h
@@ -0,0 +1,13 @@
+/*
+ * pc104_mc_ovverride.h
+ *
+ *  Created on: Sep 17, 2012
+ *      Author: icub
+ */
+
+#ifndef EMS_MC_OVVERRIDE_H_
+#define EMS_MC_OVVERRIDE_H_
+
+// nothing to do here!! (for now)
+
+#endif /* PC104_MC_OVVERRIDE_H_ */
