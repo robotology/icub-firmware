5cb3e556b4aea5dcec865be7d57d9793b40fc5e8
Valentina Gaggero <valentina.gaggero@iit.it>
2013-02-25 16:28:20 +0000

    updated with new repo location
    svn path=/trunk/iCub/firmware/; revision=22456

--- emBODY/arch-armcmx/body/abslayer/ipal-package/ipal/ipal-extra.disinstall.bat
+++ emBODY/arch-armcmx/body/abslayer/ipal-package/ipal/ipal-extra.disinstall.bat
@@ -9,7 +9,7 @@ echo. -- Setting the variables...
 :: Here you need to make some changes to suit your system.
 set SOURCE=./ipal-extra
 set SVN=C:\Program Files\TortoiseSVN\bin
-set URL=https://svn.cognitivehumanoids.eu/sdl/embedded/arm/embody/body/abslayer/ipal-package/ipal/ipal-extra
+set URL=https://svn.icub.iit.local/repos/soft/embed/embedded/arm/embody/body/abslayer/ipal-package/ipal/ipal-extra
 
 :: Unless you want to modify the script, this is enough.
 
