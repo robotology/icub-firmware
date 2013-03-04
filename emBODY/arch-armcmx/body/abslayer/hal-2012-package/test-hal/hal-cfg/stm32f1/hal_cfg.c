e50ae251fe953ea3fdc0d914911279e51010d865
Marco Accame <marco.accame@iit.it>
2013-03-04 18:07:48 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=22628

--- emBODY/arch-armcmx/body/abslayer/hal-2012-package/test-hal/hal-cfg/stm32f1/hal_cfg.c
+++ emBODY/arch-armcmx/body/abslayer/hal-2012-package/test-hal/hal-cfg/stm32f1/hal_cfg.c
@@ -41,19 +41,6 @@
 static void s_hal_cfg_on_fatalerror(hal_fatalerror_t errorcode, const char * errormsg);
 
 
-// extern const hal_base_cfg_t hal_base_cfg = 
-// {   
-//     .stacksize              = HAL_BASE_CFG_STACKSIZE,
-//     .heapsize               = HAL_BASE_CFG_HEAPSIZE,
-//     .extfn                  =
-//     {
-//         .usr_on_fatal_error                 = s_hal_cfg_on_fatalerror,
-//         .osal_system_scheduling_suspend     = NULL,
-//         .osal_system_scheduling_restart     = NULL
-//     }   
-// };
-
-
 extern const hal_core_cfg_t hal_core_cfg = 
 {   
     .basecfg    =
@@ -78,9 +65,8 @@ extern const hal_core_cfg_t hal_core_cfg =
 };
 
 
-//extern const hal_base_cfg_t *hal_base_cfgMINE = &hal_base_cfg;
-
 extern const hal_core_cfg_t *hal_coreCFGptr = &hal_core_cfg;
+
 
 void SysTick_Handler(void)
 {
