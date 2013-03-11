833c3a33a0e7976bea5bf76a99d130888c7c8415
Valentina Gaggero <valentina.gaggero@iit.it>
2013-03-11 10:21:02 +0000

    change version 1.2. and build date compiled with any-arch/sys/embobj 22731 ATTENTION: last commit was wrong.
    svn path=/trunk/iCub/firmware/; revision=22758

--- emBODY/arch-armcmx/apps/tests/EOMemsAppl_V01/app_cfg/EOMtheEMSapplCfg_cfg.h
+++ emBODY/arch-armcmx/apps/tests/EOMemsAppl_V01/app_cfg/EOMtheEMSapplCfg_cfg.h
@@ -153,20 +153,20 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          1
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          1
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          2
 //  </h>version
 
 //  <h> build date
 //  <o> year            <2010-2020> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_YEAR         2013
 //  <o> month           <1-12> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        2
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        3
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          25
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          11
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         16
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         11
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          45
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          18
 //  </h>build date
 
 // </h>Info 
