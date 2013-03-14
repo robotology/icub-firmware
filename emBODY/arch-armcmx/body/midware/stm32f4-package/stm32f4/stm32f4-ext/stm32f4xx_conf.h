182f2b74982586ecba69bbd6c745ab2ddf3a8a45
Marco Accame <marco.accame@iit.it>
2013-03-14 14:35:04 +0000

    aligned w/ latest version
    svn path=/trunk/iCub/firmware/; revision=22921

--- emBODY/arch-armcmx/body/midware/stm32f4-package/stm32f4/stm32f4-ext/stm32f4xx_conf.h
+++ emBODY/arch-armcmx/body/midware/stm32f4-package/stm32f4/stm32f4-ext/stm32f4xx_conf.h
@@ -1,23 +1,29 @@
 /**
   ******************************************************************************
-  * @file    USART/USART_TwoBoards/DataExchangeDMA/stm32f4xx_conf.h 
+  * @file    HASH/Context_Swap/stm32f4xx_conf.h  
   * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    30-September-2011
+  * @version V1.0.1
+  * @date    13-April-2012
   * @brief   Library configuration file.
   ******************************************************************************
   * @attention
   *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
   *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
   ******************************************************************************
-  */ 
+  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F4xx_CONF_H
@@ -25,22 +31,22 @@
 
 /* Includes ------------------------------------------------------------------*/
 /* Uncomment the line below to enable peripheral header file inclusion */
-#include "stm32f4xx_adc.h"
-#include "stm32f4xx_can.h"
-#include "stm32f4xx_crc.h"
+#include "stm32f4xx_adc.h" 
+#include "stm32f4xx_can.h" 
+#include "stm32f4xx_crc.h" 
 #include "stm32f4xx_cryp.h"
-#include "stm32f4xx_dac.h"
-#include "stm32f4xx_dbgmcu.h"
-#include "stm32f4xx_dcmi.h"
-#include "stm32f4xx_dma.h"
+#include "stm32f4xx_dac.h" 
+#include "stm32f4xx_dbgmcu.h" 
+#include "stm32f4xx_dcmi.h" 
+#include "stm32f4xx_dma.h" 
 #include "stm32f4xx_exti.h"
-#include "stm32f4xx_flash.h"
+#include "stm32f4xx_flash.h" 
 #include "stm32f4xx_fsmc.h"
-#include "stm32f4xx_hash.h"
+#include "stm32f4xx_hash.h" 
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_i2c.h"
 #include "stm32f4xx_iwdg.h"
-#include "stm32f4xx_pwr.h"
+#include "stm32f4xx_pwr.h" 
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_rng.h"
 #include "stm32f4xx_rtc.h"
@@ -54,13 +60,6 @@
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-
-/* If an external clock source is used, then the value of the following define 
-   should be set to the value of the external clock source, else, if no external 
-   clock is used, keep this define commented */
-/*#define I2S_EXTERNAL_CLOCK_VAL   12288000 */ /* Value of the external clock in Hz */
-
-
 /* Uncomment the line below to expanse the "assert_param" macro in the 
    Standard Peripheral Library drivers code */
 /* #define USE_FULL_ASSERT    1 */
@@ -85,4 +84,4 @@
 
 #endif /* __STM32F4xx_CONF_H */
 
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
