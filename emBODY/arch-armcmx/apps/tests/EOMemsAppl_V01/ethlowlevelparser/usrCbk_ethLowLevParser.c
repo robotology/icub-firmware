1933772d7cf376bb05ae88b03e9ac4573a4f1beb
Valentina Gaggero <valentina.gaggero@iit.it>
2013-03-22 13:06:57 +0000

    modified osal cfg for timers. added user callback for eth low level parser
    svn path=/trunk/iCub/firmware/; revision=23116

---
+++ emBODY/arch-armcmx/apps/tests/EOMemsAppl_V01/ethlowlevelparser/usrCbk_ethLowLevParser.c
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Valentina Gaggero
+ * email:   valentina.gaggero@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* @file       eOtheEthLowLevelParser.c
+    @brief      
+    @author     valentina.gaggero@iit.it
+    @date       03/18/2013
+**/
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "stdlib.h"
+#include "EoCommon.h"
+#include "string.h"
+
+#include "hal.h"
+
+#include "eOtheEthLowLevelParser.h"
+#include "eODeb_eoProtoParser.h"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables, but better using _get(), _set() 
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+extern void EthLowLevParser_callbackInHAL(uint8_t *packet)
+{
+    eOresult_t res;
+    eOethLowLevParser_packetInfo_t pktInfo = {0};
+
+    res = eOTheEthLowLevParser_GetUDPdatagramPayload(eo_ethLowLevParser_GetHandle(), packet, &pktInfo);
+    if(eores_OK != res)
+    {
+        return;
+    }
+    eODeb_eoProtoParser_RopFrameDissect(eODeb_eoProtoParser_GetHandle(), &pktInfo);
+}
+
+extern void my_cbk_onErrorSeqNum(eOethLowLevParser_packetInfo_t *pktInfo_ptr, uint32_t rec_seqNum, uint32_t expected_seqNum)
+{
+    char str[80];
+    sprintf(str, "ETHPARSER: SEQ_NUM: rec=%d expeted=%d", rec_seqNum, expected_seqNum);
+    hal_trace_puts(str);
+
+    hal_led_on(hal_led1); //led verde
+}
+
+extern void my_cbk_onNVfound(eOethLowLevParser_packetInfo_t *pktInfo_ptr, eODeb_eoProtoParser_ropAdditionalInfo_t *ropAddInfo_ptr)
+{
+    char str[80];
+    sprintf(str, "eoDeb_protoParser:NVfound: ep=%d, id=%d", ropAddInfo_ptr->desc.ep, ropAddInfo_ptr->desc.id);
+    hal_trace_puts(str);
+}
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
