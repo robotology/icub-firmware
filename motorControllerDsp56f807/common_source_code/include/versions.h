819df490c9f34f122bac0c99c386ba22ca1ab92f
Marco Randazzo <marco.randazzo@iit.it>
2013-07-31 15:17:52 +0000

    added versions.h
    svn path=/trunk/iCub/firmware/; revision=25379

---
+++ motorControllerDsp56f807/common_source_code/include/versions.h
@@ -0,0 +1,17 @@
+/*
+ * versions.h
+ * macros for easy handling of build versions
+ *
+ */
+ 
+#ifndef __BUILD_VERSIONS__
+#define __BUILD_VERSIONS__
+
+#define BUILD_2BLL     71
+#define BUILD_2BLL_DC  71
+#define BUILD_2BLL_IE  71
+#define BUILD_2BLL_IE  71
+#define BUILD_4DC      71
+
+#endif
+
