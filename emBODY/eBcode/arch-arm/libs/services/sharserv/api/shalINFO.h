65debabe718a9b0c14a90578ad9a5d679664caf7
Marco Accame <marco.accame@iit.it>
2013-11-19 18:14:53 +0000

    added sharserv/api
    svn path=/trunk/iCub/firmware/; revision=27048

---
+++ emBODY/eBcode/arch-arm/libs/services/sharserv/api/shalINFO.h
@@ -0,0 +1,144 @@
+/* 
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _SHALINFO_H_
+#define _SHALINFO_H_
+
+
+/** @file       shalINFO.h
+    @brief      This header file implements public interface to the shalINFO library.
+    @author     marco.accame@iit.it
+    @date       03/11/2011
+**/
+
+/** @defgroup shal_info Library shalINFO
+    The shalINFO library allows ...... 
+ 
+    @todo acemor-facenda: do documentation.
+    
+    @{        
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "stdint.h"
+#include "eEcommon.h"
+#include "eEmemorymap.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+// here is information used for versioning of the library
+// a process uses this info to recognise validity of the permanent or volatile data managed by the library
+// so far, data is recognised valid if the read value has the same SHALxxx_SIGN and SHALxxx_VER_MAJOR values.
+// if not recognised as valid the data is reset to the default values
+
+#define SHALINFO_NAME                   "shalINFO"        
+
+#define SHALINFO_SIGN                   0x30                // must have least significant nibble at 0
+
+#define SHALINFO_VER_MAJOR              0x02                // change of binary form of data
+#define SHALINFO_VER_MINOR              0x00                // change of implementation
+
+#define SHALINFO_BUILDDATE_YEAR         2013
+#define SHALINFO_BUILDDATE_MONTH        9
+#define SHALINFO_BUILDDATE_DAY          11
+#define SHALINFO_BUILDDATE_HOUR         16
+#define SHALINFO_BUILDDATE_MIN          0
+
+
+
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+
+typedef enum
+{
+    shalinfo_ipnet          = 0,
+    shalinfo_can1net        = 1,
+    shalinfo_can2net        = 2,
+    shalinfo_page08         = 3,
+    shalinfo_page32         = 4,
+    shalinfo_page64         = 5,
+    shalinfo_page128        = 6
+} shalinfo_deviceinfo_part_t;
+
+
+typedef struct                  // 256B              
+{
+    eEipnetwork_t               ipnetwork;      //016B 
+    eEcannetwork_t              can1network;    //004B
+    eEcannetwork_t              can2network;    //004B
+    uint8_t                     page08[8];      //008B
+    uint8_t                     page32[32];     //032B
+    uint8_t                     page64[64];     //064B
+    uint8_t                     page128[128];   //128B
+} shalinfo_deviceinfo_t;        EECOMMON_VERIFYsizeof(shalinfo_deviceinfo_t, 256);
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+// - info about the module
+
+extern const eEmoduleInfo_t * shalinfo_moduleinfo_get(void);
+extern const eEentity_t * shalinfo_moduleinfo_entity_get(void);
+extern eEresult_t shalinfo_isvalid(void);
+
+
+// - generic services
+
+extern eEresult_t shalinfo_init(void);
+extern eEresult_t shalinfo_deinit(void);
+extern eEresult_t shalinfo_erase(void);
+
+// - board-info services: it is about the hw printed-circuit-board.
+
+// only the loader calls it with its own board-info.
+extern eEresult_t shalinfo_boardinfo_synchronise(const eEboardInfo_t* boardinfo);
+// other eprocesses or shared libraries just get it.
+extern eEresult_t shalinfo_boardinfo_get(const eEboardInfo_t** boardinfo);
+
+
+// - device-info services: it is about information used by all the e-processes. for instance mac address, ip address etc.
+
+extern eEresult_t shalinfo_deviceinfo_clr(void);
+extern eEresult_t shalinfo_deviceinfo_get(const shalinfo_deviceinfo_t** deviceinfo);
+extern eEresult_t shalinfo_deviceinfo_set(const shalinfo_deviceinfo_t* deviceinfo);
+
+
+extern eEresult_t shalinfo_deviceinfo_part_clr(shalinfo_deviceinfo_part_t part);
+extern eEresult_t shalinfo_deviceinfo_part_get(shalinfo_deviceinfo_part_t part, const void** data);
+extern eEresult_t shalinfo_deviceinfo_part_set(shalinfo_deviceinfo_part_t part, const void* data);
+
+
+ 
+ 
+
+/** @}            
+    end of group shal_info 
+ **/
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
---
+++ emBODY/eBcode/arch-arm/libs/services/sharserv/api/shalPART.h
@@ -0,0 +1,148 @@
+/* 
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _SHALPART_H_
+#define _SHALPART_H_
+
+
+/** @file       shalPART.h
+    @brief      This header file implements public interface to the shalPART shared library.
+    @author     marco.accame@iit.it
+    @date       03/07/2011
+**/
+
+/** @defgroup shal_part  shalPART
+    The shalPART shared library allows ...... 
+ 
+    @todo acemor-facenda: do documentation.
+    
+    @{        
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "stdint.h"
+#include "eEcommon.h"
+#include "eEmemorymap.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+// here is information used for versioning of the library
+// a process uses this info to recognise validity of the permanent or volatile data managed by the library
+// so far, data is recognised valid if the read value has the same SHALxxx_SIGN and SHALxxx_VER_MAJOR values.
+// if not recognised as valid the data is reset to the default values
+
+#define SHALPART_NAME                   "shalPART"          // name ... 
+
+#define SHALPART_SIGN                   0x20                // must have least significant nibble at 0
+
+#define SHALPART_VER_MAJOR              0x02                // change of binary form of data
+#define SHALPART_VER_MINOR              0x00                // change of implementation
+
+#define SHALPART_BUILDDATE_YEAR         2013
+#define SHALPART_BUILDDATE_MONTH        9
+#define SHALPART_BUILDDATE_DAY          11
+#define SHALPART_BUILDDATE_HOUR         17
+#define SHALPART_BUILDDATE_MIN          0
+
+#define SHALPART_DEFAULTSTARTUPTIME     (5*1000*1000)
+
+
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+typedef enum 
+{
+    shalpart_reset_default  = 0, 
+    shalpart_reset_rawvals = 1
+} shalpart_reset_mode_t;
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+
+// - info about the module
+
+extern const eEmoduleInfo_t * shalpart_moduleinfo_get(void);
+extern const eEentity_t * shalpart_moduleinfo_entity_get(void);
+extern eEresult_t shalpart_isvalid(void);
+
+
+// - generic services
+
+extern eEresult_t shalpart_init(void);
+extern eEresult_t shalpart_reset(shalpart_reset_mode_t rm);
+extern eEresult_t shalpart_deinit(void);
+
+// - management of process
+
+extern eEresult_t shalpart_proc_synchronise(eEprocess_t proc, const eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_proc_allavailable_get(const eEprocess_t **table, uint8_t *size);
+
+extern eEresult_t shalpart_proc_add(eEprocess_t proc, eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_proc_rem(eEprocess_t proc);
+extern eEresult_t shalpart_proc_set(eEprocess_t proc, eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_proc_get(eEprocess_t proc, const eEmoduleInfo_t **moduleinfo);
+
+
+// - management of shared libraries
+
+extern eEresult_t shalpart_shal_synchronise(eEsharlib_t shal, const eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_shal_allavailable_get(const eEsharlib_t **table, uint8_t *size);
+
+extern eEresult_t shalpart_shal_add(eEsharlib_t shal, eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_shal_rem(eEsharlib_t shal);
+extern eEresult_t shalpart_shal_set(eEsharlib_t shal, eEmoduleInfo_t *moduleinfo);
+extern eEresult_t shalpart_shal_get(eEsharlib_t shal, const eEmoduleInfo_t **moduleinfo);
+    
+    
+// management of bootstrap info
+
+// retrieve the address of the execution specified process
+extern eEresult_t shalpart_proc_runaddress_get(eEprocess_t proc, uint32_t *addr);
+
+
+// retrieves / sets the process which must run as startup (loaded by the eLoader)
+extern eEresult_t shalpart_proc_startup_get(eEprocess_t *proc);
+extern eEresult_t shalpart_proc_startup_set(eEprocess_t proc);
+ 
+
+// retrieves / sets the time that the system spends in the startup process
+extern eEresult_t shalpart_proc_startuptime_get(uint32_t *startuptime);
+extern eEresult_t shalpart_proc_startuptime_set(uint32_t startuptime);
+
+ 
+// retrieves / sets the process which must eventually run after the startup
+extern eEresult_t shalpart_proc_def2run_get(eEprocess_t *proc);
+extern eEresult_t shalpart_proc_def2run_set(eEprocess_t proc);
+ 
+ 
+
+/** @}            
+    end of group shal_part 
+ **/
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
