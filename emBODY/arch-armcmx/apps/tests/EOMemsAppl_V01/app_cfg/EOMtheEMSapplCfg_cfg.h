b30a2c2e6475eb2c29874a2e36917a381ff40d04
Valentina Gaggero <valentina.gaggero@iit.it>
2013-11-21 17:40:25 +0000

    version 1.31
    svn path=/trunk/iCub/firmware/; revision=27160

--- emBODY/arch-armcmx/apps/tests/EOMemsAppl_V01/app_cfg/EOMtheEMSapplCfg_cfg.h
+++ emBODY/arch-armcmx/apps/tests/EOMemsAppl_V01/app_cfg/EOMtheEMSapplCfg_cfg.h
@@ -68,7 +68,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          1
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          30
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          31
 //  </h>version
 
 //  <h> build date
@@ -77,9 +77,9 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        11
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          5
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          21
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         12
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         11
 //  <o> minute          <0-59> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          0
 //  </h>build date
