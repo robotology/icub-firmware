87697ac3b578551d13abf07d908c9a73b333bf2e
Marco Accame <marco.accame@iit.it>
2013-11-21 12:53:54 +0000

    copied embobj/core/src into new tree
    svn path=/trunk/iCub/firmware/; revision=27105

---
+++ emBODY/eBcode/embobj/core/core/EOconstarray_hid.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOCONSTARRAY_HID_H_
+#define _EOCONSTARRAY_HID_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* @file        EOconstarray_hid.h
+    @brief      This header file implements hidden interface to a array object.
+    @author     marco.accame@iit.it
+    @date       08/03/2011
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+
+// - declaration of extern public interface ---------------------------------------------------------------------------
+ 
+#include "EOconstarray.h"
+
+
+// - #define used with hidden struct ----------------------------------------------------------------------------------
+
+
+// - definition of the hidden struct implementing the object ----------------------------------------------------------
+
+/** @struct     EOconstarray_hid
+    @brief      Hidden definition. Implements private data used only internally by the 
+                public or private (static) functions of the object and protected data
+                used also by its derived objects.
+ **/  
+ 
+struct EOconstarray_hid 
+{
+    eOsizeitem_t    sizeofitem;
+    eOsizecntnr_t   size;
+    const uint8_t*  data;
+}; 
+
+
+// - declaration of extern hidden functions ---------------------------------------------------------------------------
+// empty-section
+
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+ 
+#endif  // include-guard
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
+
