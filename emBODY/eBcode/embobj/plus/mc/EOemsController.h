42502a7d437a66573862988bd260f7daf793a8fa
Marco Accame <marco.accame@iit.it>
2013-11-26 09:00:54 +0000

    copied from original tree
    svn path=/trunk/iCub/firmware/; revision=27309

---
+++ emBODY/eBcode/embobj/plus/mc/EOemsController.h
@@ -0,0 +1,128 @@
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOEMSCONTROLLER_H_
+#define _EOEMSCONTROLLER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** @file       EOemsController.h
+    @brief      This header file implements public interface to joint-motor decoupling matrix.
+    @author     alessandro.scalzo@iit.it
+    @date       18/04/2012
+**/
+
+/** @defgroup eo_emsController Object EOemsController
+    Does something.
+     
+    @{        
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+#include "EOdecoupler.h"
+#include "EOaxisController.h"
+#include "EoMotionControl.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+
+/* @typedef    typedef struct EOtrajectory_hid EOtrajectory
+    @brief      EOtrajectory is an opaque struct. It is used to implement data abstraction for the 
+                object so that the user cannot see its private fields and he/she is forced to manipulate the
+                object only with the proper public functions. 
+ **/  
+typedef struct EOemsController_hid EOemsController;
+
+
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+// empty-section
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+ 
+ 
+/* @fn         extern EOtrajectory* eo_trajectory_New(void)
+    @brief      Creates a new trajectory object 
+    @return     The pointer to the required object.
+ **/
+extern EOemsController* eo_emsController_Init(emsBoardType_t board_type);
+
+
+/* @fn         extern void eo_trajectory_Set(EOtrajectory *o, float p0, float pf, float v0, float speed)
+    @brief      Sets minimum jerk trajectory new start and end points, initial and average speed. When the time is 
+                expired, the function always returns the trajectory end point. 
+    @param      o      The pointer to the trajectory object.
+    @param      p0     The start position of the trajectory.         
+    @param      pf     The end position of the trajectory.
+    @param      v0     The inital velocity.
+    @param      speed  The average speed.
+ **/
+
+// synchronous
+extern void eo_emsController_ReadTorque(uint8_t joint, eOmeas_torque_t trq_measure);
+extern void eo_emsController_ReadEncoders(int32_t *pos);
+#ifdef USE_2FOC_FAST_ENCODER
+extern void eo_emsController_ReadSpeed(uint8_t axis, int32_t speed);
+#endif
+extern void eo_emsController_PWM(int16_t* pwm);
+extern void eo_emsController_SetEncSign(uint16_t jxx, int32_t enc_sign);
+
+extern void eo_emsController_SetOutput(uint8_t joint, int16_t out);
+extern void eo_emsController_SetPosRef(uint8_t joint, int32_t pos, int32_t avg_vel);
+extern void eo_emsController_SetVelRef(uint8_t joint, int32_t vel, int32_t avg_acc);
+extern void eo_emsController_SetPosRaw(uint8_t joint, int32_t pos);
+extern void eo_emsController_SetTrqRef(uint8_t joint, int32_t trq);
+
+//extern void eo_emsController_GetPosRef(int32_t* pos, int32_t* avg_vel);
+extern eObool_t eo_emsController_GetLimitedCurrentMask(uint8_t* mask);
+
+// asynchronous
+extern void eo_emsController_CheckCalibrations(void);
+extern void eo_emsController_StartCalibration(uint8_t joint, int32_t pos, int32_t vel, int32_t offset);
+extern void eo_emsController_SetControlMode(uint8_t joint, eOmc_controlmode_command_t mode, eObool_t twoFOC_off);
+extern eOmc_controlmode_t eo_emsController_GetControlMode(uint8_t joint);
+extern void eo_emsController_ResetPosPid(uint8_t joint);
+extern void eo_emsController_ResetTrqPid(uint8_t joint);
+extern void eo_emsController_Stop(uint8_t joint);
+
+extern void eo_emsController_GetActivePidStatus(uint8_t joint, eOmc_joint_status_ofpid_t* pidStatus);
+extern void eo_emsController_GetJointStatus(uint8_t joint, eOmc_joint_status_basic_t* jointStatus);
+extern eObool_t eo_emsController_GetMotionDone(uint8_t joint);
+
+// configuration
+extern void eo_emsController_SetPosPid(uint8_t joint, float K, float Kd, float Ki, float Imax, int32_t Ymax, int32_t Yoff);
+extern void eo_emsController_SetTrqPid(uint8_t joint, float K, float Kd, float Ki, float Imax, int32_t Ymax, int32_t Yoff);
+
+extern void eo_emsController_SetImpedance(uint8_t joint, int32_t stiffness, int32_t damping, int32_t offset);
+extern void eo_emsController_GetImpedance(uint8_t joint, int32_t *stiffness, int32_t *damping, int32_t *offset);
+
+extern void eo_emsController_SetLimits(uint8_t joint, int32_t pos_min, int32_t pos_max, int32_t vel_max);
+extern void eo_emsController_SetPosMin(uint8_t joint, int32_t pos_min);
+extern void eo_emsController_SetPosMax(uint8_t joint, int32_t pos_max);
+extern void eo_emsController_SetVelMax(uint8_t joint, int32_t vel_max);
+
+extern void eo_emsController_SetVelTimeout(uint8_t joint, int32_t vel_timeout);
+
+extern void eo_emsController_ReadMotorstatus(uint8_t motor, uint8_t motorerror, uint8_t canerror, eOmc_controlmode_t controlmode);
+
+/** @}            
+    end of group eo_emsController  
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
---
+++ emBODY/eBcode/embobj/plus/mc/EOspeedmeter.h
@@ -0,0 +1,84 @@
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _EOSPEEDMETER_H_
+#define _EOSPEEDMETER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file       EOspeedmeter.h
+    @brief      This header file implements public interface to axis speed measure from slow encoder.
+    @author     alessandro.scalzo@iit.it
+    @date       19/05/2012
+**/
+
+/** @defgroup eo_speedmeter Object EOspeedmeter
+    Does something.
+     
+    @{        
+ **/
+
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "EoCommon.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+#define ENC_INVALID 0x7FFFFFFF
+
+//#define USE_2FOC_FAST_ENCODER
+
+#define SPEED_2FOC_TO_EMS(speed) (5*((int32_t)(speed)))
+
+// - declaration of public user-defined types ------------------------------------------------------------------------- 
+
+ 
+
+/** @typedef    typedef struct EOspeedmeter_hid EOspeedmeter
+    @brief      EOpid is an opaque struct. It is used to implement data abstraction for the 
+                object so that the user cannot see its private fields and he/she is forced to manipulate the
+                object only with the proper public functions. 
+ **/  
+typedef struct EOspeedmeter_hid EOspeedmeter;
+
+
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+// empty-section
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+ 
+ 
+extern EOspeedmeter* eo_speedmeter_New(void);
+extern void eo_speedometer_Calibrate(EOspeedmeter* o, int32_t offset);
+extern eObool_t eo_speedometer_IsOk(EOspeedmeter* o);
+//extern void eo_speedometer_Reset(EOspeedmeter* o);
+extern eObool_t eo_speedometer_IsHardFault(EOspeedmeter* o);
+//extern eObool_t eo_speedometer_IsStarted(EOspeedmeter* o);
+extern void eo_speedometer_SetEncSign(EOspeedmeter* o, int32_t enc_sign);
+
+extern void eo_speedometer_SlowEncoderRead(EOspeedmeter* o, int32_t position);
+
+#ifdef USE_2FOC_FAST_ENCODER
+extern void eo_speedometer_FastEncoderRead(EOspeedmeter* o, int32_t velocity);
+#endif
+
+extern int32_t eo_speedometer_GetDistance(EOspeedmeter* o);
+extern int32_t eo_speedometer_GetVelocity(EOspeedmeter* o);
+
+/** @}            
+    end of group eo_pid  
+ **/
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
