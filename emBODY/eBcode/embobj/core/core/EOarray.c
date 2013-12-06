6f51adb4e810d68cee3f0d82097f90f329178479
Marco Accame <marco.accame@iit.it>
2013-12-06 13:18:58 +0000

    changed internal representation so that capacity is just a uint8_t, changed api of New(), added a resize() and a assign()
    svn path=/trunk/iCub/firmware/; revision=27587

--- emBODY/eBcode/embobj/core/core/EOarray.c
+++ emBODY/eBcode/embobj/core/core/EOarray.c
@@ -79,10 +79,14 @@
 
 
  
-extern EOarray* eo_array_New(uint16_t capacity, uint8_t itemsize, void *memory)
+extern EOarray* eo_array_New(uint8_t capacity, uint8_t itemsize, void *memory)
 {
-    EOarray *retptr = NULL;    
-
+    EOarray *retptr = NULL;   
+    uint8_t capacity08 = (uint8_t)capacity;     
+    uint8_t itemsize08 = (uint8_t)itemsize;
+    
+//    eo_errman_Assert(eo_errman_GetHandle(), (capacity08 == capacity), s_eobj_ownname, ":eo_array_New() has capacity > uint8_t");
+//    eo_errman_Assert(eo_errman_GetHandle(), (itemsize08 == itemsize), s_eobj_ownname, ":eo_array_New() has itemsize > uint8_t");
     
     if(NULL != memory)
     {   // i use external memory
@@ -90,11 +94,11 @@ extern EOarray* eo_array_New(uint16_t capacity, uint8_t itemsize, void *memory)
     }
     else
     {   // i get the memory for the object
-        retptr = eo_mempool_GetMemory(eo_mempool_GetHandle(), eo_mempool_align_32bit, sizeof(eOarray_head_t) + capacity*itemsize, 1);
+        retptr = eo_mempool_GetMemory(eo_mempool_GetHandle(), eo_mempool_align_32bit, sizeof(eOarray_head_t) + capacity08*itemsize08, 1);
     }
 
-    retptr->head.capacity       = capacity;
-    retptr->head.itemsize       = itemsize;
+    retptr->head.capacity       = capacity08;
+    retptr->head.itemsize       = itemsize08;
     retptr->head.size           = 0;
     
     eo_array_Reset(retptr);
@@ -110,19 +114,62 @@ extern eOresult_t eo_array_Reset(EOarray *p)
     }
 
     p->head.size = 0;
-    memset(p->data, 0, p->head.capacity*p->head.itemsize);
+    memset(p->data, 0, (uint16_t)p->head.capacity*p->head.itemsize);
 
     return(eores_OK);
 }
 
-extern uint16_t eo_array_Capacity(EOarray *p)
+extern void eo_array_Resize(EOarray *p, uint8_t size)
+{
+    uint8_t *start = NULL;
+    uint16_t first;
+    uint16_t last;
+    
+    if(NULL == p)
+    {
+        return;     // invalid array
+    }
+    
+    if(size > p->head.size)
+    {
+        return;     // nothing to do
+    }
+    
+    if(size > p->head.capacity)
+    {
+        return;     // nothing to do
+    }
+
+    if(size < p->head.size)
+    {
+        first   = size;
+        last    = p->head.size;
+    }
+    else
+    {
+        first   = p->head.size;
+        last    = size;
+    }
+    
+    // new size
+    p->head.size = size;
+    
+    
+    // clear the removed elements or the added ones     
+    start = (uint8_t*) (p->data);
+    memset(&start[(uint32_t)(first*p->head.itemsize)], 0, (last-first)*p->head.itemsize);
+
+    return;
+}
+
+extern uint8_t eo_array_Capacity(EOarray *p)
 {
     if(NULL == p)
     {
         return(0);
     }
 
-    return( p->head.capacity);
+    return((uint8_t)p->head.capacity);
 }
 
 extern uint8_t eo_array_ItemSize(EOarray *p)
@@ -159,7 +206,7 @@ extern uint16_t eo_array_UsedBytes(EOarray *p)
         return(0);
     }
 
-    return(sizeof(eOarray_head_t) + p->head.size*p->head.itemsize);
+    return(sizeof(eOarray_head_t) + (uint16_t)p->head.size*p->head.itemsize);
 }
 
 extern eOresult_t eo_array_PushBack(EOarray *p, const void *item)
@@ -174,7 +221,7 @@ extern eOresult_t eo_array_PushBack(EOarray *p, const void *item)
         return(eores_NOK_generic);
     }
     
-    memcpy(&(p->data[p->head.size*p->head.itemsize]), item, p->head.itemsize);
+    memcpy(&(p->data[(uint16_t)p->head.size*p->head.itemsize]), item, p->head.itemsize);
     p->head.size++;
 
     return(eores_OK);
@@ -190,7 +237,7 @@ extern void * eo_array_At(EOarray *p, uint8_t pos)
         return(NULL);
     }
 
-    return(&(p->data[pos*p->head.itemsize]));
+    return(&(p->data[(uint16_t)pos*p->head.itemsize]));
 }
 
 extern eOresult_t eo_array_PopBack(EOarray *p)
@@ -207,6 +254,75 @@ extern eOresult_t eo_array_PopBack(EOarray *p)
     p->head.size--;
 
     return(eores_OK);
+}
+
+
+extern void eo_array_AssignOne(EOarray *p, uint8_t pos, const void *item) 
+{
+    uint8_t *start = NULL;
+        
+    if((NULL == p) || (NULL == item)) 
+    {    
+        // invalid data
+        return;    
+    }
+    
+    if(pos >= p->head.capacity) 
+    { 
+        // beyond the capacity of the array
+        return;
+    }
+ 
+    
+    if(pos >= p->head.size)
+    {
+        eo_array_Resize(p, pos+1); 
+    }
+    
+    // now fill the pos-th position w/ object item
+        
+    start = (uint8_t*) (p->data);
+    // cast to uint16_t to tell the reader that index of array start[] can be bigger
+    start = &start[(uint16_t)pos * p->head.size]; 
+    
+    memcpy(start, item, p->head.itemsize);
+    
+    return; 
+}
+
+extern void eo_array_Assign(EOarray *p, uint8_t pos, const void *items, uint8_t nitems)
+{
+    uint8_t *start = NULL;
+        
+    if((NULL == p) || (NULL == items) || (0 == nitems)) 
+    {    
+        // invalid data
+        return;    
+    }
+    
+    if((pos+nitems-1) >= p->head.capacity) 
+    { 
+        // beyond the capacity of the array
+        return;
+    }
+ 
+    
+    if((pos+nitems-1) >= p->head.size)
+    {
+        eo_array_Resize(p, pos+nitems); 
+    }
+    
+    // now fill from the pos-th position until the (pos+nitems-1)-th position w/ object items
+        
+    start = (uint8_t*) (p->data);
+    // cast to uint16_t to tell the reader that index of array start[] can be bigger
+    start = &start[(uint16_t)pos * p->head.size]; 
+    
+    memcpy(start, items, (uint16_t)nitems*p->head.itemsize);
+    
+    return;     
+    
+    
 }
 
 // --------------------------------------------------------------------------------------------------------------------
--- emBODY/eBcode/embobj/core/core/EOarray.h
+++ emBODY/eBcode/embobj/core/core/EOarray.h
@@ -52,12 +52,12 @@ extern "C" {
 // - declaration of public user-defined types ------------------------------------------------------------------------- 
 
 
-
 typedef struct
 {
-    uint16_t        capacity;
-    uint8_t         itemsize;
-    uint8_t         size;
+    uint8_t         capacity;       /**< it is the maximum possible value of size in array */
+    uint8_t         itemsize;       /**< it is the size of a single item in bytes */
+    uint8_t         size;           /**< it keeps the number of items in the array */
+    uint8_t         dummy;
 } eOarray_head_t;   EO_VERIFYsizeof(eOarray_head_t, 4);
 
 typedef struct
@@ -73,6 +73,7 @@ typedef struct
                 object only with the proper public functions. 
  **/  
 //typedef struct EOarray_hid EOarray;
+
 typedef EOarray_of EOarray;
     
 // - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
@@ -82,39 +83,70 @@ typedef EOarray_of EOarray;
 // - declaration of extern public functions ---------------------------------------------------------------------------
  
  
-/** @fn         extern EOarray* eo_array_New(uint16_t capacity, uint8_t sizeofitem, void *memory)
+/** @fn         extern EOarray* eo_array_New(uint8_t capacity, uint8_t sizeofitem, void *memory)
     @brief      Creates a new EOarray object. If the argument @e memory is not NULL, then it is used for storage
                 inside the object. The function resets
                 the data. If we pass a non NULL @e memory then we consume @a capacity bytes + 4 bytes of memory.
     @param      capacity        The capacity of the array.
-    @param      itemsize        If not zero, then the array contains fixed-sized items, otherwise just bytes
+    @param      itemsize        If not zero, then the array contains fixed-sized items, otherwise error. 
     @param      memory          If not NULL the memory to use for the object. It is required a size equal to:
                                 4 + @e capacity * @e itemsize.
     @return     The pointer to the required object.
     @warning    The function resets the memory pointed by @e memory for the size it requires which is:
                 4+ @e capacity * @e itemsize and puts  @e capacity and @e itemsize at its beginning
  **/
-extern EOarray* eo_array_New(uint16_t capacity, uint8_t itemsize, void *memory);
-
+extern EOarray* eo_array_New(uint8_t capacity, uint8_t itemsize, void *memory);
 
 
 /** @fn         extern eOresult_t eo_array_Reset(EOarray *t)
-    @brief      Resets the array
+    @brief      Resets the array. It sets its size to zero and memset to zero all the data for p->capacity * p->itemsize bytes.
     @param      p               The pointer to the array object.
     @return     eores_OK upon success, eores_NOK_nullpointer if p is NULL
  **/
 extern eOresult_t eo_array_Reset(EOarray *p);
 
 
-extern uint16_t eo_array_Capacity(EOarray *p);
+/** @fn         extern void eo_array_Resize(EOarray *p, uint8_t size)
+    @brief      Resizes the array. It deos actions only if the new size can be contained in the array. It clears the removed or added elements.
+    @param      p               The pointer to the array object.
+    @param      size            The new size.
+ **/ 
+extern void eo_array_Resize(EOarray *p, uint8_t size);
 
+
+/** @fn         extern uint8_t eo_array_Capacity(EOarray *p)
+    @brief      tells the capacity of the array.
+    @param      p               The pointer to the array object.
+    @return     the capacity
+ **/ 
+extern uint8_t eo_array_Capacity(EOarray *p);
+
+
+/** @fn         extern uint8_t eo_array_ItemSize(EOarray *p)
+    @brief      tells the item size of the array.
+    @param      p               The pointer to the array object.
+    @return     the item size
+ **/ 
 extern uint8_t eo_array_ItemSize(EOarray *p);
 
+
+/** @fn         extern uint8_t eo_array_Size(EOarray *p)
+    @brief      tells the size of the array.
+    @param      p               The pointer to the array object.
+    @return     the size
+ **/ 
 extern uint8_t eo_array_Size(EOarray *p);
 
+
+/** @fn         extern uint16_t eo_array_UsedBytes(EOarray *p)
+    @brief      tells how much of the memory pointer by p is used (header + size*itemsize).
+    @param      p               The pointer to the array object.
+    @return     the used bytes including header
+ **/ 
 extern uint16_t eo_array_UsedBytes(EOarray *p);
 
-/** @fn         extern eOresult_t eo_array_PushBackItem(EOarray *p, const void *item)
+
+/** @fn         extern eOresult_t eo_array_PushBack(EOarray *p, const void *item)
     @brief      Adds an item at the back of the array. It does that only if the EOarray was created with non-zero itemsize
     @param      p               The pointer to the array object.
     @param      item            The item to be pushed back
@@ -124,11 +156,10 @@ extern uint16_t eo_array_UsedBytes(EOarray *p);
 extern eOresult_t eo_array_PushBack(EOarray *p, const void *item);
 
 
-/** @fn         extern void * eo_array_GetItem(EOarray *p, uint16_t pos, uint16_t *size)
+/** @fn         extern void * eo_array_At(EOarray *p, uint8_t pos)
     @brief      Gets a pointer to the item in position @e pos inside the object 
     @param      p               The pointer to the object.
     @param      pos             The position of the iterm
-    @param      size            pointer to the size in bytes occupied by the returned pointer.
     @return     The pointer, or NULL upon failure.
  **/
 extern void * eo_array_At(EOarray *p, uint8_t pos);
@@ -143,7 +174,18 @@ extern void * eo_array_At(EOarray *p, uint8_t pos);
  **/
 extern eOresult_t eo_array_PopBack(EOarray *p);
 
- 
+
+/** @fn         extern void eo_array_Assign(EOarray *p, uint8_t pos, const void *items, uint8_t nitems)
+    @brief      puts nitems pointed by items in positions starting from pos. the items must be disposed in a 
+                consecutive mode as in an array. if final position pos+nitems is higher than current size and lower
+                equal than capacity, then it resizes the array. if it is higher that capacity it does nothing.
+    @param      p               The pointer to the array object.
+    @param      pos             The position where to put the first item
+    @param      items           pointer to the items
+    @param      nitems          number of the items
+ **/
+extern void eo_array_Assign(EOarray *p, uint8_t pos, const void *items, uint8_t nitems);
+
 
 
 /** @}            
