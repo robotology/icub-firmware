75c09d8583e7958aa2d37a8e24283149e8f62aa2
Marco Accame <marco.accame@iit.it>
2014-01-13 15:31:31 +0000

    No commit message
    svn path=/trunk/iCub/firmware/; revision=27993

---
+++ emBODY/eBtest/arch-arm/libs/highlevel/abslayer/hal2/test-hal/app-test/stm32f4/hl_cfg_plus_target.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 iCub Facility - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef _HL_CFG_PLUS_TARGET_H_
+#define _HL_CFG_PLUS_TARGET_H_
+
+//#include "hl_target.h"
+
+// -- definitions of macros used in hl
+
+#if     !defined(HL_CFG_PLUS_TARGET_BY_FILE)
+    #warning HL: HL_CFG_PLUS_TARGET_BY_FILE is not defined and you are using hl_cfg_plus_target.h
+#endif
+
+ 
+#if   defined(HL_USE_BRD_MCBSTM32_F400)
+
+    #define HL_USE_MPU_NAME_STM32F407IG
+    
+#elif   defined(HL_USE_BRD_EMS004)    
+
+    #define HL_USE_MPU_NAME_STM32F407IG
+
+#else
+    #error ERR: cannot define MPU_NAME
+#endif
+
+
+#include "hl_mpu2arch.h"
+
+
+#endif  // include-guard
+
+
+
