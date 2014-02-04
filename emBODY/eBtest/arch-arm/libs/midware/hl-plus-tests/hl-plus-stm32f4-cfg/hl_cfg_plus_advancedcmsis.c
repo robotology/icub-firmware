1ba1a27746ddd7fdd377f6baf3e23a7d06a5eef6
Marco Accame <marco.accame@iit.it>
2014-02-04 13:59:22 +0000

    added an overridden hl_system_stm32fx_before_setsysclock() for ems4rd
    svn path=/trunk/iCub/firmware/; revision=28223

--- emBODY/eBtest/arch-arm/libs/midware/hl-plus-tests/hl-plus-stm32f4-cfg/hl_cfg_plus_advancedcmsis.c
+++ emBODY/eBtest/arch-arm/libs/midware/hl-plus-tests/hl-plus-stm32f4-cfg/hl_cfg_plus_advancedcmsis.c
@@ -26,6 +26,9 @@
 
 #include "hl_core.h"    // contains declarations in cmsis 
 
+#include "hl_cfg_plus_target.h"    // to see some more
+#include "hl_cfg_plus_modules.h"    // to see some more
+
 // // redefined the SystemCoreClock to have value ...
 // uint32_t SystemCoreClock = HSI_VALUE;
 // //
@@ -38,14 +41,65 @@
 // }
 
 
-#if     defined(HL_USE_BRD_EMS004)
+#if     defined(HL_USE_BRD_EMS4RD)
 
 // need to use a different SystemInit() because we dont have the normal clock as in mcbstm32f400 
 
-//#include "ems004_sys.c"
+#if     defined(HL_USE_CHIP_MICREL_KS8893)
+
+#include "hl_gpio.h"
+#include "hl_sys.h"
+
+extern void hl_system_stm32fx_before_setsysclock(void)
+{
+//     uint32_t i = 0;
+//     i = 1;
+//     i = i;
+//     RCC_ClocksTypeDef rcc_clockS;
+//         
+//     RCC_GetClocksFreq(&rcc_clockS);    
+
+//     SystemCoreClockUpdate();
+//     
+//     RCC_GetClocksFreq(&rcc_clockS);
+//         
+//     i = i;
+//     
+//     brd_eventviewer_init();
+//     evEntityId_t previd;
+//     
+//     previd = brd_eventviewer_switch(ev_ID_first_usrdef+2);    
+//     hl_sys_delay(10*1000);
+//     brd_eventviewer_switch(previd);
+//     
+//     previd = brd_eventviewer_switch(ev_ID_first_usrdef+2);    
+//     hl_sys_delay(10*1000);
+//     brd_eventviewer_switch(previd);    
+//     
+//     previd = brd_eventviewer_switch(ev_ID_first_usrdef+2);    
+//     hl_sys_delay(1000*1000);
+//     brd_eventviewer_switch(previd);   
+
+    SystemCoreClockUpdate();
+    hl_gpio_t ethslv = {.port = hl_gpio_portB, .pin = hl_gpio_pin2};
+    hl_gpio_pin_output_init(ethslv);
+    
+    hl_gpio_t notethrst = {.port = hl_gpio_portH, .pin = hl_gpio_pin1};
+    hl_gpio_pin_output_init(notethrst);
+    
+    hl_gpio_pin_write(ethslv, hl_gpio_valSET);
+    hl_gpio_pin_write(notethrst, hl_gpio_valRESET);
+    //hl_sys_delay_hsi(110*1000);    
+    hl_sys_delay(110*1000);
+    hl_gpio_pin_write(notethrst, hl_gpio_valSET);
+    uint32_t i = 0;
+    i++;
+    
+}
+#endif
 
 
-#endif//defined(HL_USE_BRD_EMS004)
+#endif//defined(HL_USE_BRD_EMS4RD)
 
 
 
