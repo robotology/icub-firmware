db3d0874a09fb05467e76693fd1165dd7a5873f4
Marco Accame <marco.accame@iit.it>
2014-02-04 14:13:48 +0000

    revised teh internal organisation of code
    svn path=/trunk/iCub/firmware/; revision=28226

--- emBODY/eBcode/arch-arm/libs/midware/hl-plus/src/chips/hl_chip_micrel_ks8893.c
+++ emBODY/eBcode/arch-arm/libs/midware/hl-plus/src/chips/hl_chip_micrel_ks8893.c
@@ -63,7 +63,8 @@
 
 #define I2CADDRESS          0xBE  
 
-#define REG0x01             0x01    
+#define REG0x01             0x01  
+#define REG0x06             0x06  
 #define REG0x1C             0x1C    
 #define REG0x2C             0x2C
 
@@ -81,7 +82,8 @@ extern const hl_chip_micrel_ks8893_cfg_t hl_chip_micrel_ks8893_cfg_default  =
         .pin  = hl_gpio_pinNONE
     },
     .resetval      = hl_gpio_valRESET
-    .extclockinit   = NULL
+    .extclockinit   = NULL,
+    .targetphymode
 };
 #endif
 
@@ -92,13 +94,13 @@ extern const hl_chip_micrel_ks8893_cfg_t hl_chip_micrel_ks8893_cfg_default  =
 
 typedef struct
 {
-    hl_chip_micrel_ks8893_cfg_t        config;
+    hl_chip_micrel_ks8893_cfg_t                 config;
 } hl_chip_micrel_ks8893_internal_item_t;
 
 typedef struct
 {
-    hl_bool_t                                  initted;
-    hl_chip_micrel_ks8893_internal_item_t*     items[1];   
+    hl_bool_t                                   initted;
+    hl_chip_micrel_ks8893_internal_item_t*      items[1];   
 } hl_chip_micrel_ks8893_theinternals_t;
 
 
@@ -111,8 +113,13 @@ static hl_bool_t s_hl_chip_micrel_ks8893_initted_is(void);
 
 static hl_result_t s_hl_chip_micrel_ks8893_hw_init(const hl_chip_micrel_ks8893_cfg_t *cfg, hl_chip_micrel_ks8893_internal_item_t *intitem);
 
-static void s_hl_chip_micrel_ks8893_phymode_get(hl_ethtrans_phymode_t* usedphymode);
+static void s_hl_chip_micrel_ks8893_phymode_set(hl_ethtrans_phymode_t targetphymode);
 
+static void s_hl_chip_micrel_ks8893_mii_phymode_get(hl_ethtrans_phymode_t* usedmiiphymode);
+
+static void s_hl_chip_micrel_ks8893_start(const hl_chip_micrel_ks8893_cfg_t *cfg);
+
+static void s_hl_chip_micrel_ks8893_reset(const hl_chip_micrel_ks8893_cfg_t *cfg);
 
 // --------------------------------------------------------------------------------------------------------------------
 // - definition (and initialisation) of static const variables
@@ -176,31 +183,34 @@ extern hl_result_t hl_chip_micrel_ks8893_init(const hl_chip_micrel_ks8893_cfg_t 
 }
 
 
-extern hl_result_t hl_chip_micrel_ks8893_configure(hl_ethtrans_phymode_t targetphymode, hl_ethtrans_phymode_t* usedphymode)
+extern hl_result_t hl_chip_micrel_ks8893_configure(hl_ethtrans_phymode_t* usedmiiphymode)
 {
     hl_chip_micrel_ks8893_internal_item_t *intitem = s_hl_chip_micrel_ks8893_theinternals.items[0];
+    
     
     //const uint8_t fd100 = 0x60;
     //const uint8_t fd010 = 0x20;
     uint8_t buff_write = 0x60; // FORCE FULL DUPLEX AND 100T
     uint8_t buff_read = 0xFF; 
     volatile uint32_t i = 1;
-    hl_i2c_regaddr_t regadr = {.numofbytes = 1, .bytes.one = 0};
+    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0};
     
     if(hl_false == s_hl_chip_micrel_ks8893_initted_is())
     {
         return(hl_res_NOK_generic);
     }  
+    
+    hl_ethtrans_phymode_t targetphymode = intitem->config.targetphymode;
 
     hl_i2c_t i2cid = intitem->config.i2cid;
 
-    regadr.bytes.one = REG0x01;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
+    regaddr.bytes.one = REG0x01;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
     if((buff_read&0x01))
     {   // already initted. to be initted again must pass through a reset
-        if(NULL != usedphymode)
+        if(NULL != usedmiiphymode)
         {
-            s_hl_chip_micrel_ks8893_phymode_get(usedphymode);
+            s_hl_chip_micrel_ks8893_mii_phymode_get(usedmiiphymode);
         }
         return(hl_res_OK);
     }
@@ -208,72 +218,31 @@ extern hl_result_t hl_chip_micrel_ks8893_configure(hl_ethtrans_phymode_t targetp
     
     if(hl_ethtrans_phymode_none == targetphymode)
     {
-        if(NULL != usedphymode)
+        if(NULL != usedmiiphymode)
         {
-            *usedphymode = hl_ethtrans_phymode_none;    
+            *usedmiiphymode = hl_ethtrans_phymode_none;    
         }   
         return(hl_res_OK);        
     }
 
+    // we execute here only if the micrel is not initted yet.
+    // however that does not happen
 
-    
-
-#if 0    
-    // configure mii
-    regadr.bytes.one = 0x06;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-    buff_write  = buff_read;
-    buff_write |= 0x10; 
-    hl_i2c_write(i2cid, I2CADDRESS, regadr, &buff_write, 1);
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-#endif
-
-    // 1. configure  switch's ports 1 and 2
-    switch(targetphymode)
-    {
-        case hl_ethtrans_phymode_auto:                   buff_write = 0x9F; break;
-        case hl_ethtrans_phymode_halfduplex10mbps:       buff_write = 0x00; break;
-        case hl_ethtrans_phymode_halfduplex100mbps:      buff_write = 0x40; break;
-        case hl_ethtrans_phymode_fullduplex10mbps:       buff_write = 0x20; break;
-        case hl_ethtrans_phymode_fullduplex100mbps:      buff_write = 0x60; break;
-        default:                                    buff_write = 0x00; break;
-    }    
-    
-    
-    // port 1
-    regadr.bytes.one = REG0x1C;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-    hl_i2c_write(i2cid, I2CADDRESS, regadr, &buff_write, 1);
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-     
-    // port 2 
-    regadr.bytes.one = REG0x2C;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-    hl_i2c_write(i2cid, I2CADDRESS, regadr, &buff_write, 1);   
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
+    // 1. config the phy modes
+    s_hl_chip_micrel_ks8893_phymode_set(targetphymode);
 
     // 2. start the switch
-    buff_write = 0x1;  
-    regadr.bytes.one = REG0x01;    
-    hl_i2c_write(i2cid, I2CADDRESS, regadr, &buff_write, 1);
+    s_hl_chip_micrel_ks8893_start(&intitem->config);
     
-
-    // 3. read back to verify
-    regadr.bytes.one = REG0x01;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &buff_read, 1);
-    if(!(buff_read&0x01))
-    {
-        hl_sys_on_error(hl_error_runtimefault, "hl_chip_micrel_ks8893_configure(): SWITCH not configured");
-    }
-    
-    s_hl_chip_micrel_ks8893_phymode_get(usedphymode);
+    // 3. get the phy mode
+    s_hl_chip_micrel_ks8893_mii_phymode_get(usedmiiphymode);
 
  
     return(hl_res_OK);
 }
 
 
-extern hl_result_t hl_chip_micrel_ks8893_mii_getphymode(hl_ethtrans_phymode_t* usedphymode)
+extern hl_result_t hl_chip_micrel_ks8893_mii_getphymode(hl_ethtrans_phymode_t* usedmiiphymode)
 {
 #if     !defined(HL_BEH_REMOVE_RUNTIME_PARAM_CHECK)    
     if(hl_false == s_hl_chip_micrel_ks8893_initted_is())
@@ -282,7 +251,7 @@ extern hl_result_t hl_chip_micrel_ks8893_mii_getphymode(hl_ethtrans_phymode_t* u
     }  
 #endif//!defined(HL_BEH_REMOVE_RUNTIME_PARAM_CHECK)
     
-    s_hl_chip_micrel_ks8893_phymode_get(usedphymode);
+    s_hl_chip_micrel_ks8893_mii_phymode_get(usedmiiphymode);
     return(hl_res_OK);
 }
 
@@ -322,7 +291,13 @@ static hl_bool_t s_hl_chip_micrel_ks8893_initted_is(void)
 static hl_result_t s_hl_chip_micrel_ks8893_hw_init(const hl_chip_micrel_ks8893_cfg_t *cfg, hl_chip_micrel_ks8893_internal_item_t *intitem)
 {   
     hl_i2c_t i2cid = cfg->i2cid;
+    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0 };
+    uint8_t data;
     
+    data = 0;
+    regaddr.numofbytes = 0;
+    data = data;
+    regaddr.numofbytes = regaddr.numofbytes;
     
     // 1. init external clock.
     
@@ -331,36 +306,32 @@ static hl_result_t s_hl_chip_micrel_ks8893_hw_init(const hl_chip_micrel_ks8893_c
         return(hl_res_NOK_generic);
     }
     
-    // 2. init reset pin
-    hl_gpio_pin_output_init(cfg->resetpin);
- 
-    // 3. reset the micrel
-    hl_gpio_val_t resetval = cfg->resetval;
-    hl_gpio_pin_write(cfg->resetpin, resetval);
-    hl_sys_delay(10*1000);
-    hl_gpio_pin_write(cfg->resetpin, (hl_gpio_valRESET == resetval) ? (hl_gpio_valSET) : (hl_gpio_valRESET));
-    hl_sys_delay(100);    
- 
-    // 4. verify if i2c is initted  
+    // 3. reset the chip
+    s_hl_chip_micrel_ks8893_reset(cfg);
+    
+    // 3. verify if i2c is initted  
     if(hl_false == hl_i2c_initted_is(i2cid))
     {
         return(hl_res_NOK_generic);
     }
     
     
-    // 5. verify that the micrel is responding
-    
+    // 4. verify that the micrel is responding   
     if(hl_res_OK != hl_i2c_ping(i2cid, I2CADDRESS))
     {
         return(hl_res_NOK_generic);
     }
     
+    
+    memcpy(&intitem->config, cfg, sizeof(hl_chip_micrel_ks8893_cfg_t));
+    
 //#warning INFO --> could add verification that on ic2 the device at that address is really a micrel switch ...    
 #if 0        
-    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0x00 }; 
-    uint8_t data;
+//    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0x00 }; 
+//    uint8_t data;
 
     // whoami: value must be WHOAMI_VAL
+    regaddr.numofbytes = 1;
     regaddr.bytes.one = WHOAMI_ADR;
     if(hl_res_OK != (res = hl_i2c_read(i2cid, I2CADDRESS, regaddr, &data, 1)))
     {
@@ -370,17 +341,85 @@ static hl_result_t s_hl_chip_micrel_ks8893_hw_init(const hl_chip_micrel_ks8893_c
     {
         return(hl_res_NOK_generic);
     }
-#endif        
+#endif   
+
+//     // read the reg01 register
+//     regaddr.numofbytes = 1;
+//     regaddr.bytes.one = REG0x01;
+//     hl_i2c_read(i2cid, I2CADDRESS, regaddr, &data, 1); 
+//     data = data;   
+
+//     // be sure the switch is stopped
+//     data = 0x0;  
+//     regaddr.bytes.one = REG0x01;    
+//     hl_i2c_write(i2cid, I2CADDRESS, regaddr, &data, 1);   
+
+    
+    // impose the phy mode     
+    s_hl_chip_micrel_ks8893_phymode_set(cfg->targetphymode); //hl_ethtrans_phymode_auto   
+    
+//     // be sure the switch is stopped
+//     data = 0x0;  
+//     regaddr.bytes.one = REG0x01;    
+//     hl_i2c_write(i2cid, I2CADDRESS, regaddr, &data, 1);   
     
     
-    memcpy(&intitem->config, cfg, sizeof(hl_chip_micrel_ks8893_cfg_t));
+    // start the micrel
+    s_hl_chip_micrel_ks8893_start(cfg);    
+    
     
     return(hl_res_OK);
 }
 
+static void s_hl_chip_micrel_ks8893_phymode_set(hl_ethtrans_phymode_t targetphymode)
+{
+    hl_chip_micrel_ks8893_internal_item_t *intitem = s_hl_chip_micrel_ks8893_theinternals.items[0];
+    hl_i2c_t i2cid = intitem->config.i2cid;
+    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0 };
+    uint8_t buff_write;
+    uint8_t buff_read;
+       
+
+    // mii will be kept in auto
+#if 0    
+    // configure rmii (port 0).
+    regaddr.bytes.one = REG0x06;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+    buff_write  = buff_read;
+    buff_write |= 0x10; 
+    hl_i2c_write(i2cid, I2CADDRESS, regaddr, &buff_write, 1);
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+#endif    
+         
+    // 1. configure  switch's ports 1 and 2
+    switch(targetphymode)
+    {
+        case hl_ethtrans_phymode_auto:                  buff_write = 0x9F; break;
+        case hl_ethtrans_phymode_halfduplex10mbps:      buff_write = 0x00; break;
+        case hl_ethtrans_phymode_halfduplex100mbps:     buff_write = 0x40; break;
+        case hl_ethtrans_phymode_fullduplex10mbps:      buff_write = 0x20; break;
+        case hl_ethtrans_phymode_fullduplex100mbps:     buff_write = 0x60; break;
+        default:                                        buff_write = 0x00; break;
+    }    
+    
+    
+    // port 1
+    regaddr.numofbytes = 1;
+    regaddr.bytes.one = REG0x1C;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+    hl_i2c_write(i2cid, I2CADDRESS, regaddr, &buff_write, 1);
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+     
+    // port 2 
+    regaddr.numofbytes = 1;
+    regaddr.bytes.one = REG0x2C;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+    hl_i2c_write(i2cid, I2CADDRESS, regaddr, &buff_write, 1);   
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);    
+}
 
 
-static void s_hl_chip_micrel_ks8893_phymode_get(hl_ethtrans_phymode_t* usedphymode)
+static void s_hl_chip_micrel_ks8893_mii_phymode_get(hl_ethtrans_phymode_t* usedmiiphymode)
 {
     hl_chip_micrel_ks8893_internal_item_t *intitem = s_hl_chip_micrel_ks8893_theinternals.items[0];
     hl_i2c_t i2cid = intitem->config.i2cid;
@@ -390,9 +429,9 @@ static void s_hl_chip_micrel_ks8893_phymode_get(hl_ethtrans_phymode_t* usedphymo
     uint8_t mux = 0;
     uint8_t speed = 0;
     uint8_t read = 0xFF; 
-    hl_i2c_regaddr_t regadr = {.numofbytes = 1, .bytes.one = 0};
-    regadr.bytes.one = 0x06;
-    hl_i2c_read(i2cid, I2CADDRESS, regadr, &read, 1);
+    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0};
+    regaddr.bytes.one = REG0x06;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &read, 1);
     if( (MIIHLFD & read) == MIIHLFD)
     {
         mux = 0;
@@ -409,14 +448,49 @@ static void s_hl_chip_micrel_ks8893_phymode_get(hl_ethtrans_phymode_t* usedphymo
     {
         speed = 1;
     }
-    if((0==mux)&&(0==speed))        *usedphymode = hl_ethtrans_phymode_halfduplex10mbps;
-    else if((0==mux)&&(1==speed))   *usedphymode = hl_ethtrans_phymode_halfduplex100mbps; 
-    else if((1==mux)&&(0==speed))   *usedphymode = hl_ethtrans_phymode_fullduplex10mbps; 
-    else if((1==mux)&&(1==speed))   *usedphymode = hl_ethtrans_phymode_fullduplex100mbps;          
+    if((0==mux)&&(0==speed))        *usedmiiphymode = hl_ethtrans_phymode_halfduplex10mbps;
+    else if((0==mux)&&(1==speed))   *usedmiiphymode = hl_ethtrans_phymode_halfduplex100mbps; 
+    else if((1==mux)&&(0==speed))   *usedmiiphymode = hl_ethtrans_phymode_fullduplex10mbps; 
+    else if((1==mux)&&(1==speed))   *usedmiiphymode = hl_ethtrans_phymode_fullduplex100mbps;          
     
 }
 
 
+static void s_hl_chip_micrel_ks8893_start(const hl_chip_micrel_ks8893_cfg_t *cfg)
+{
+    //hl_chip_micrel_ks8893_internal_item_t *intitem = s_hl_chip_micrel_ks8893_theinternals.items[0];
+    hl_i2c_t i2cid = cfg->i2cid;
+    hl_i2c_regaddr_t regaddr = {.numofbytes = 1, .bytes.one = 0 };
+    uint8_t buff_write = 0;
+    uint8_t buff_read = 0;
+         
+    // 2. start the switch
+    buff_write = 0x1;  
+    regaddr.bytes.one = REG0x01;    
+    hl_i2c_write(i2cid, I2CADDRESS, regaddr, &buff_write, 1);
+    
+
+    // 3. read back to verify
+    regaddr.bytes.one = REG0x01;
+    hl_i2c_read(i2cid, I2CADDRESS, regaddr, &buff_read, 1);
+    if(!(buff_read&0x01))
+    {
+        hl_sys_on_error(hl_error_runtimefault, "hl_chip_micrel_ks8893_configure(): SWITCH not configured");
+    }
+}
+
+static void s_hl_chip_micrel_ks8893_reset(const hl_chip_micrel_ks8893_cfg_t *cfg)
+{
+    // 2. init reset pin
+    hl_gpio_pin_output_init(cfg->resetpin);
+ 
+    // 3. reset the micrel
+    hl_gpio_val_t resetval = cfg->resetval;
+    hl_gpio_pin_write(cfg->resetpin, resetval);
+    hl_sys_delay(10*1000);
+    hl_gpio_pin_write(cfg->resetpin, (hl_gpio_valRESET == resetval) ? (hl_gpio_valSET) : (hl_gpio_valRESET));
+    hl_sys_delay(100);    
+}
 
 #endif//defined(HL_USE_CHIP_MICREL_KS8893)
 
