4011a439d7267c93018de969e7160b6e9ccf7efa
Valentina Gaggero <valentina.gaggero@iit.it>
2014-02-13 11:29:48 +0000

    restore host address (10.0.1.104) and use of local address from eeprom
    svn path=/trunk/iCub/firmware/; revision=28374

--- emBODY/eBcode/arch-arm/board/ems001/appl/v01/app_cfg/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems001/appl/v01/app_cfg/EOMtheEMSapplCfg_cfg.h
@@ -35,15 +35,15 @@ extern "C" {
 
 //  <o> host IP2 address <0-255>
 //  <i> default: 255
-#define EOMTHEEMSAPPLCFG_HOSTIPADDR2                            255
+#define EOMTHEEMSAPPLCFG_HOSTIPADDR2                            0
 
 //  <o> host IP3 address <0-255>
 //  <i> default: 72
-#define EOMTHEEMSAPPLCFG_HOSTIPADDR3                            72
+#define EOMTHEEMSAPPLCFG_HOSTIPADDR3                            1
 
 //  <o> host IP4 address <1-255>
 //  <i> default: 205
-#define EOMTHEEMSAPPLCFG_HOSTIPADDR4                            129
+#define EOMTHEEMSAPPLCFG_HOSTIPADDR4                            104
 
 
 //  </h>Remote Host
@@ -55,7 +55,7 @@ extern "C" {
 //  <o> ID of the EMS board     <1=> EB1    <2=> EB2    <3=> EB3    <4=> EB4    <5=> EB5    
 //                              <6=> EB6    <7=> EB7    <8=> EB8    <9=> EB9
 
-#define EOMTHEEMSAPPLCFG_ID_OF_EMSBOARD     2
+#define EOMTHEEMSAPPLCFG_ID_OF_EMSBOARD     1
 
 
       
@@ -221,7 +221,7 @@ extern "C" {
 
 //  <o> addresses       <0=> from eeprom 
 //                      <1=> from ipal_cfg.h                               
-#define ADDR_USEIPAL   1
+#define ADDR_USEIPAL   0
 
 #if (1 == ADDR_USEIPAL)
     #define EOMTHEEMSAPPLCFG_IPADDR_FROM_ENVIRONMENT        0
