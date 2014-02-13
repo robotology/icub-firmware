38b5a4224c0ed89419916ea3f1b119ddd7a88ac9
Valentina Gaggero <valentina.gaggero@iit.it>
2014-02-13 17:05:55 +0000

    recompiled all firmware using can protocol messages defined in iCub/firmware/canProtocolLib
    svn path=/trunk/iCub/firmware/; revision=28383

--- motorControllerDsp56f807/common_source_code/include/versions.h
+++ motorControllerDsp56f807/common_source_code/include/versions.h
@@ -7,10 +7,10 @@
 #ifndef __BUILD_VERSIONS__
 #define __BUILD_VERSIONS__
 
-#define BUILD_2BLL     74
-#define BUILD_2BLL_DC  74
-#define BUILD_2BLL_IE  74
-#define BUILD_4DC      74
+#define BUILD_2BLL     73
+#define BUILD_2BLL_DC  73
+#define BUILD_2BLL_IE  73
+#define BUILD_4DC      83
 
 #endif
 
