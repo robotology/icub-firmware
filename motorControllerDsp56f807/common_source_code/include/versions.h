8901154060b8838d6acff801bc2f5197f6e23abd
Valentina Gaggero <valentina.gaggero@iit.it>
2014-02-13 17:20:49 +0000

    ERRATA CORRIGE:in the last commit build version numbers are wrong!! In this commit they are increment. NEW BUILD VERSION is 75!!
    svn path=/trunk/iCub/firmware/; revision=28385

--- motorControllerDsp56f807/common_source_code/include/versions.h
+++ motorControllerDsp56f807/common_source_code/include/versions.h
@@ -7,10 +7,10 @@
 #ifndef __BUILD_VERSIONS__
 #define __BUILD_VERSIONS__
 
-#define BUILD_2BLL     73
-#define BUILD_2BLL_DC  73
-#define BUILD_2BLL_IE  73
-#define BUILD_4DC      83
+#define BUILD_2BLL     75
+#define BUILD_2BLL_DC  75
+#define BUILD_2BLL_IE  75
+#define BUILD_4DC      75
 
 #endif
 
