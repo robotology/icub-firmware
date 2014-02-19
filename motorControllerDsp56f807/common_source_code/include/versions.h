8311150f69c081ff2b1cf2a282923ec7c4c169c4
Marco Randazzo <marco.randazzo@iit.it>
2014-02-19 10:51:59 +0000

    *** BLL, 4DC, BLL2DC, BLLIE FIRMWARE RELEASE 76 *** new can protocol _kff_torque[] implemented in torque control
    svn path=/trunk/iCub/firmware/; revision=28415

--- motorControllerDsp56f807/common_source_code/include/versions.h
+++ motorControllerDsp56f807/common_source_code/include/versions.h
@@ -7,10 +7,10 @@
 #ifndef __BUILD_VERSIONS__
 #define __BUILD_VERSIONS__
 
-#define BUILD_2BLL     75
-#define BUILD_2BLL_DC  75
-#define BUILD_2BLL_IE  75
-#define BUILD_4DC      75
+#define BUILD_2BLL     76
+#define BUILD_2BLL_DC  76
+#define BUILD_2BLL_IE  76
+#define BUILD_4DC      76
 
 #endif
 
