56a9d2dbb20746c33d4c71a10e4d79e29dbe6eb3
Valentina Gaggero <valentina.gaggero@iit.it>
2014-03-12 14:08:42 +0000

    Added script for compiling executables of all ems boards in one shot modified EOMtheEMSapplCfg_cfg.h: in order to use the script, user should set "ID of the EMS board" field equal to "Use external file"
    svn path=/trunk/iCub/firmware/; revision=28632

---
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v1/proj/script/compileAllBoards_script.bat
@@ -0,0 +1,9 @@
+@echo off
+FOR /l %%i in (1,1,9) DO  (echo i'm compiling for board %%i
+ echo #define EOMTHEEMSAPPLCFG_ID_OF_EMSBOARD     %%i >..\..\app_cfg\extDefEmsBoardNum.h 
+ c:\keil\uv4\uv4 -b ..\ems4rd-v01.uvproj -o .\script\log%%i.txt -t "ems4rd"
+ echo ...done!
+ move ..\..\bin\ems004.hex ..\..\bin\ems004_eb%%i.hex 
+)
+
+pause
