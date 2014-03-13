4378baa5e1c79c4c16899afe35c0c7f5e5a1243b
Marco Accame <marco.accame@iit.it>
2014-03-13 15:01:01 +0000

    increased heap size due to upgrade of osal to rtx 4.73
    svn path=/trunk/iCub/firmware/; revision=28662

--- emBODY/eBcode/arch-arm/board/ems001/appl/v01/abslayer-cfg/hal_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems001/appl/v01/abslayer-cfg/hal_cfg.h
@@ -65,7 +65,7 @@
 //   <o> heap size         <0x0-0xFFFFFFFF:8>
 //   <i> define how much heap you want.
 #ifndef HAL_SYS_HEAPSIZE
- #define HAL_SYS_HEAPSIZE      0x0000C000
+ #define HAL_SYS_HEAPSIZE      0x0000C100
 #endif
 
 
