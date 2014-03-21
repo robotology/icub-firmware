19cd86efda482981a03f25eb6f01a35290a5869a
Valentina Gaggero <valentina.gaggero@iit.it>
2014-03-21 15:23:55 +0000

    added file eOprot_b10.c eOprot_b11.c to project modified script in order to compile for board 10 and 11. Attention: currently exe for board 10 and 11  don't work!!!!!
    svn path=/trunk/iCub/firmware/; revision=28730

--- emBODY/eBcode/arch-arm/board/ems004/appl/v1/proj/script/compileAllBoards_script.bat
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v1/proj/script/compileAllBoards_script.bat
@@ -1,5 +1,5 @@
 @echo off
-FOR /l %%i in (1,1,9) DO  (echo i'm compiling for board %%i
+FOR /l %%i in (1,1,11) DO  (echo i'm compiling for board %%i
  echo #define EOMTHEEMSAPPLCFG_ID_OF_EMSBOARD     %%i >..\..\app_cfg\extDefEmsBoardNum.h 
  c:\keil\uv4\uv4 -b ..\ems4rd-v01.uvproj -o .\script\log%%i.txt -t "ems4rd"
  echo ...done!
