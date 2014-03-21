12fa65dd571547249c0efd9c1a0122beacd27262
Valentina Gaggero <valentina.gaggero@iit.it>
2014-03-21 10:21:43 +0000

    in s_hal_core_cfg_on_fatalerror function exit from forever loop if error is a warning
    svn path=/trunk/iCub/firmware/; revision=28725

--- emBODY/eBcode/arch-arm/board/ems004/appl/v1/abslayer-cfg-stm32f4/hal_core_cfg.c
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v1/abslayer-cfg-stm32f4/hal_core_cfg.c
@@ -95,6 +95,11 @@ static void s_hal_core_cfg_on_fatalerror(hal_fatalerror_t errorcode, const char 
     {
         hal_trace_puts(errormsg);
     }
+    
+    if(hal_fatalerror_warning == errorcode)
+    {
+        return;
+    }
 
     hal_led_init(hal_led3, NULL);
 
