b8ab3b6f49aac18bc754d085eea87bcddc96f6a7
Marco Accame <marco.accame@iit.it>
2014-03-26 16:44:45 +0000

    removed unused api
    svn path=/trunk/iCub/firmware/; revision=28744

--- emBODY/eBcode/arch-arm/libs/highlevel/abslayer/hal2/api/hal_i2c.h
+++ emBODY/eBcode/arch-arm/libs/highlevel/abslayer/hal2/api/hal_i2c.h
@@ -109,7 +109,7 @@ typedef struct
  
 // - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
 
-extern const hal_i2c_cfg_t hal_i2c_cfg_default; // = { .mode = hal_i2c_mode_master, .speed = hal_i2c_speed_400kbps, .ownaddress = 0, .usedma = hal_false };
+extern const hal_i2c_cfg_t hal_i2c_cfg_default; // = { .mode = hal_i2c_mode_master, .speed = hal_i2c_speed_400kbps, .ownaddress = 0 };
 
 
 // - declaration of extern public functions ---------------------------------------------------------------------------
@@ -123,56 +123,13 @@ extern const hal_i2c_cfg_t hal_i2c_cfg_default; // = { .mode = hal_i2c_mode_mast
   */
 extern hal_result_t hal_i2c_init(hal_i2c_t id, const hal_i2c_cfg_t *cfg);
 
+
 /** @fn			extern hal_bool_t hal_i2c_initted_is(hal_i2c_t id)
     @brief  	this function tells if an i2c port has already been initted
     @param  	id              the id of i2c
     @return 	hal_true or hal_false
   */
 extern hal_bool_t hal_i2c_initted_is(hal_i2c_t id);
-
-// /** @fn			extern hal_result_t hal_i2c_transaction_begin(hal_i2c_t id, hal_i2c_devaddr_t devaddr)
-//     @brief  	this function begins a transaction: locks the bus for other attempts of hal_i2c_transaction_begin(), sends a START, write 
-//                 the address of the device to talk to.
-//     @param  	id              the id of i2c
-//     @param  	devaddr 	    the address of the device used for this transaction
-//     @return 	hal_res_NOK_generic on error else hal_res_OK
-//   */
-// extern hal_result_t hal_i2c_transaction_begin(hal_i2c_t id, hal_i2c_devaddr_t devaddr);
-
-// /** @fn			extern hal_result_t hal_i2c_transaction_transmit(hal_i2c_t id, uint8_t* data, uint16_t size, hal_bool_t sendstart, hal_bool_t sendstop)
-//     @brief  	this function transmits data inside a transaction: if sendstart is hal_true it sends a START and the address of the device with write flag, 
-//                 sends to the device size bytes, and if sendstop is hal_true it sends a STOP condition. 
-//     @param  	id              the id of i2c
-//     @param  	data	        the data to write
-//     @param      size            the size of data to write
-//     @param      sendstart       if hal_true a START is sent before sending data
-//     @param      sendstop        if hal_true a STOP is sent after having sent the data
-//     @return 	hal_res_NOK_generic on error else hal_res_OK
-//   */
-// extern hal_result_t hal_i2c_transaction_transmit(hal_i2c_t id, uint8_t* data, uint16_t size, hal_bool_t sendstart, hal_bool_t sendstop);
-
-// /** @fn			extern hal_result_t hal_i2c_transaction_receive(hal_i2c_t id, uint8_t* data, uint16_t size, hal_bool_t sendstart, hal_bool_t sendnack, hal_bool_t sendstop)
-//     @brief  	this function receives data inside a transaction: if sendstart is hal_true it sends a START and the address of the device with read flag, 
-//                 receives from the device size bytes, if sendnack is hal_true it does not ack last received byte, and finally if sendstop is hal_true it sends a STOP condition. 
-//     @param  	id              the id of i2c
-//     @param  	data	        the data to write
-//     @param      size            the size of data to write
-//     @param      sendstart       if hal_true a START is sent before receiving data
-//     @param      sendnack        if hal_true the ACK is not sent back to the device after last reading, so that the device stops transmitting
-//     @param      sendstop        if hal_true a STOP is sent after having received the data
-//     @return 	hal_res_NOK_generic on error else hal_res_OK
-//   */
-// extern hal_result_t hal_i2c_transaction_receive(hal_i2c_t id, uint8_t* data, uint16_t size, hal_bool_t sendstart, hal_bool_t sendnack, hal_bool_t sendstop);
-
-// //extern hal_result_t hal_i2c_transaction_stop(hal_i2c_t id, hal_i2c_devaddr_t devaddr);
-
-// /** @fn			extern hal_result_t hal_i2c_transaction_ends(hal_i2c_t id, hal_i2c_devaddr_t devaddr)
-//     @brief  	this function ends a transaction: unlocks the bus to allow others to use it
-//     @param  	id              the id of i2c
-//     @param  	devaddr 	    the address of the device used for the transaction
-//     @return 	hal_res_NOK_generic on error else hal_res_OK
-//   */
-// extern hal_result_t hal_i2c_transaction_end(hal_i2c_t id, hal_i2c_devaddr_t devaddr);
 
 
 /** @fn			extern hal_result_t hal_i2c_ping(hal_i2c_t id, hal_i2c_devaddr_t devaddr)
