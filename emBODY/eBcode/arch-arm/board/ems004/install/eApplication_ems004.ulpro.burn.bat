82171a3dc2b33d01833d66460141ad6708cdc1b2
Marco Accame <marco.accame@iit.it>
2014-03-28 17:54:03 +0000

    changed teh report file name
    svn path=/trunk/iCub/firmware/; revision=28793

--- emBODY/eBcode/arch-arm/board/ems004/install/eApplication_ems004.ulpro.burn.bat
+++ emBODY/eBcode/arch-arm/board/ems004/install/eApplication_ems004.ulpro.burn.bat
@@ -2,7 +2,7 @@ IF "%~1"=="" GOTO endparse
 mkdir rep
 copy ..\appl\v1\bin\ems004_eb%1.hex rep
 copy rep\ems004_eb%1.hex tools\target.hex 
-c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o .\rep\eUpdater_ems004.burn.report.txt -t ems4ulproburn
+c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o .\rep\eApplication_ems004_burn_report.txt -t ems4ulproburn
 del -Y tools\*.bak
 del -Y tools\*.dep
 del -Y tools\*.plg
