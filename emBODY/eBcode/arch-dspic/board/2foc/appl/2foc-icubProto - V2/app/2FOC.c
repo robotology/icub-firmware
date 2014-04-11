c7ae82febca49d622f0cc5d4ea9764f545836da2
Alessandro Scalzo <alessandro.scalzo@iit.it>
2014-04-11 17:02:57 +0000

    2FOC code cleanup.
    svn path=/trunk/iCub/firmware/; revision=28893

--- emBODY/eBcode/arch-dspic/board/2foc/appl/2foc-icubProto - V2/app/2FOC.c
+++ emBODY/eBcode/arch-dspic/board/2foc/appl/2foc-icubProto - V2/app/2FOC.c
@@ -169,7 +169,8 @@ void __attribute__((__interrupt__, no_auto_psv)) _DMA0Interrupt(void)
     extern tPID2 CurrentQPID;
 
     static const int PWM_CENTER = LOOPINTCY / 2;
-    static const int PWM_MAX = PWM_CENTER / 4;
+    static const int PWM_MAX = (19*PWM_CENTER)/20; // 95%
+    //static const int PWM_MAX = 475;
 
     static int hes_state_stored = 0;
     static int *ppwmH = NULL, *ppwmL = NULL, *ppwm0 = NULL;
