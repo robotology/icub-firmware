46300b92212300c58bc0b7e3ef98bbdc85b954f3
Marco Randazzo <marco.randazzo@iit.it>
2014-05-29 15:35:57 +0000

    check_in_position now handles all control modes
    svn path=/trunk/iCub/firmware/; revision=29130

--- motorControllerDsp56f807/common_source_code/pid.c
+++ motorControllerDsp56f807/common_source_code/pid.c
@@ -1195,17 +1195,24 @@ bool check_in_position(byte jnt)
 {
 	if (_control_mode[jnt] == MODE_POSITION ||
 	    _control_mode[jnt] == MODE_VELOCITY ||
+	    _control_mode[jnt] == MODE_MIXED    ||
 	    _control_mode[jnt] == MODE_IMPEDANCE_POS ||
 	    _control_mode[jnt] == MODE_IMPEDANCE_VEL)
+		{
+			//if (__abs(_position[jnt] - _set_point[jnt]) < INPOSITION_THRESHOLD && _ended[jnt])
+			if (_ended[jnt])
+				return true;
+			else
+				return false;
+		}
+	else if (_control_mode[jnt] == MODE_DIRECT)
 	{
-		//if (__abs(_position[jnt] - _set_point[jnt]) < INPOSITION_THRESHOLD && _ended[jnt])
-		if (_ended[jnt])
-			return true;
-		else
-			return false;
+		return true;
 	}
 	else
-		return false;				
+	{
+		return false;			
+	}			
 }
 
 /***************************************************************** 
