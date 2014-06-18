2b5e66899f52b6d584a990cd3d7a987c15d5b2fc
Marco Accame <marco.accame@iit.it>
2014-06-18 10:22:46 +0000

    now report file generates correctly
    svn path=/trunk/iCub/firmware/; revision=29232

--- emBODY/eBcode/arch-arm/board/ems004/install/eLoader_ems004.ulpro.burn.bat
+++ emBODY/eBcode/arch-arm/board/ems004/install/eLoader_ems004.ulpro.burn.bat
@@ -1,7 +1,7 @@
 mkdir rep
 copy ..\env\eLoader\bin\eLoader_ems004.hex rep
 copy rep\eLoader_ems004.hex tools\target.hex 
-c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o rep\eLoader_ems004_burn_report.txt -t ems4ulproburn
+c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o ..\rep\eLoader_ems004_burn_report.txt -t ems4ulproburn
 del -Y tools\*.bak
 del -Y tools\*.dep
 del -Y tools\*.plg
--- emBODY/eBcode/arch-arm/board/ems004/install/eUpdater_ems004.ulpro.burn.bat
+++ emBODY/eBcode/arch-arm/board/ems004/install/eUpdater_ems004.ulpro.burn.bat
@@ -1,7 +1,7 @@
 mkdir rep
 copy ..\env\eUpdater\bin\eUpdaterCANgtw_ems004_open.hex rep
 copy rep\eUpdaterCANgtw_ems004_open.hex tools\target.hex 
-c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o rep\eUpdater_ems004_burn_report.txt -t ems4ulproburn
+c:\keil\uv4\uv4 -f tools\burn_target.uvproj -o ..\rep\eUpdater_ems004_burn_report.txt -t ems4ulproburn
 del -Y tools\*.bak
 del -Y tools\*.dep
 del -Y tools\*.plg
