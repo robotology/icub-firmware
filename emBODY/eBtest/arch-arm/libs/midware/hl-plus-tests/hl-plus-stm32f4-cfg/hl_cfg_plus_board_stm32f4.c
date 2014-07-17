33e3e5606ab666fb3f0d7c2d681cfe0778a5d9ac
Marco Maggiali <marco.maggiali@iit.it>
2014-07-17 15:29:28 +0000

    add the support for the MC4_PLUS
    svn path=/trunk/iCub/firmware/; revision=29559

--- emBODY/eBtest/arch-arm/libs/midware/hl-plus-tests/hl-plus-stm32f4-cfg/hl_cfg_plus_board_stm32f4.c
+++ emBODY/eBtest/arch-arm/libs/midware/hl-plus-tests/hl-plus-stm32f4-cfg/hl_cfg_plus_board_stm32f4.c
@@ -262,7 +262,7 @@ extern const hl_timer_mapping_t* hl_timer_map = &s_hl_timer_mapping;
 
 
 //
-#elif   defined(HL_USE_BRD_EMS4RD) | defined(HL_USE_BRD_MC4PLUS)
+#elif   defined(HL_USE_BRD_EMS4RD)
 //
 
 #warning --> FOR MC4PLUS: must define the mappings ... so fare we assume they are equal to ems4rd
@@ -528,6 +528,260 @@ static const hl_spi_mapping_t s_hl_spi_mapping =
                 .af32   = GPIO_AF_SPI3               
             }             
         }         
+    } 
+};
+
+extern const hl_spi_mapping_t* hl_spi_map = &s_hl_spi_mapping;
+
+#endif//defined(HL_USE_UTIL_SPI)
+
+#elif   defined(HL_USE_BRD_MC4PLUS)
+//
+
+
+#if     defined(HL_USE_UTIL_I2C)
+
+#include "hl_i2c.h"
+
+static const hl_i2c_mapping_t s_hl_i2c_mapping =
+{
+    .supportedmask      = (1 << hl_i2c1) | (0 << hl_i2c2) | (1 << hl_i2c3),
+    .gpiomap            =
+    {   
+        {   // hl_i2c1 
+            .scl = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin6 },
+                .af32   = GPIO_AF_I2C1            
+            }, 
+            .sda = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin7 },
+                .af32   = GPIO_AF_I2C1            
+            } 
+        }, 
+        {   // hl_i2c2 
+            .scl = 
+            {
+                .gpio   = { .port = hl_gpio_portNONE,   .pin = hl_gpio_pinNONE },
+                .af32   = hl_NA32             
+            }, 
+            .sda = 
+            {
+                .gpio   = { .port = hl_gpio_portNONE,   .pin = hl_gpio_pinNONE },
+                .af32   = hl_NA32                    
+            } 
+        },  
+        {   // hl_i2c3 
+            .scl = 
+            {
+                .gpio   = { .port = hl_gpio_portH,      .pin = hl_gpio_pin7 },
+                .af32   = GPIO_AF_I2C3      
+            }, 
+            .sda = 
+            {
+                .gpio   = { .port = hl_gpio_portH,      .pin = hl_gpio_pin8 },
+                .af32   = GPIO_AF_I2C3      
+            } 
+        }        
+    }       
+};
+
+extern const hl_i2c_mapping_t* hl_i2c_map = &s_hl_i2c_mapping;
+
+
+#endif//defined(HL_USE_UTIL_I2C)
+
+
+#if     defined(HL_USE_UTIL_ETH)
+
+#include "hl_eth.h"
+
+static const hl_eth_mapping_t s_hl_eth_mapping =
+{
+    .supported          = hl_true,
+    .gpiomap            =
+    {
+        .mif            = hl_eth_mif_rmii,
+        .gpio_mif.rmii  =
+        {   // if ETH_RMII_CRS_DV is on portD <-> use GPIO_Remap_ETH on rx-rmii
+            .ETH_RMII_REF_CLK   =            
+            {   
+                .gpio   = { .port = hl_gpio_portA,     .pin = hl_gpio_pin1 }, 
+                .af32   = GPIO_AF_ETH
+            },  
+            .ETH_RMII_TX_EN     =            
+            {   
+                .gpio   = { .port = hl_gpio_portB,     .pin = hl_gpio_pin11 }, 
+                .af32   = GPIO_AF_ETH
+            },
+            .ETH_RMII_TXD0      =            
+            {   
+                .gpio   = { .port = hl_gpio_portB,     .pin = hl_gpio_pin12 }, 
+                .af32   = GPIO_AF_ETH
+            },
+            .ETH_RMII_TXD1      =            
+            {   
+                .gpio   = { .port = hl_gpio_portB,     .pin = hl_gpio_pin13 }, 
+                .af32   = GPIO_AF_ETH
+            },
+            .ETH_RMII_CRS_DV    =            
+            {   
+                .gpio   = { .port = hl_gpio_portA,     .pin = hl_gpio_pin7 }, 
+                .af32   = GPIO_AF_ETH
+            },
+            .ETH_RMII_RXD0      =            
+            {   
+                .gpio   = { .port = hl_gpio_portC,     .pin = hl_gpio_pin4 }, 
+                .af32   = GPIO_AF_ETH
+            },
+            .ETH_RMII_RXD1      =            
+            {   
+                .gpio   = { .port = hl_gpio_portC,     .pin = hl_gpio_pin5 }, 
+                .af32   = GPIO_AF_ETH
+            } 
+        },
+        .gpio_smi       =
+        {
+            .ETH_MDC        =            
+            {   
+                .gpio   = { .port = hl_gpio_portC,     .pin = hl_gpio_pin1 }, 
+                .af32   = GPIO_AF_ETH
+            },     
+            .ETH_MDIO       =            
+            {   
+                .gpio   = { .port = hl_gpio_portA,     .pin = hl_gpio_pin2 }, 
+                .af32   = GPIO_AF_ETH
+            }     
+        }      
+    }
+    
+};
+
+extern const hl_eth_mapping_t* hl_eth_map = &s_hl_eth_mapping;
+
+#endif//defined(HL_USE_UTIL_ETH)
+
+
+#if     defined(HL_USE_UTIL_ETHTRANS)
+
+#include "hl_ethtrans.h"
+
+static const hl_ethtrans_mapping_t s_hl_ethtrans_mapping = 
+{
+    .supported  = hl_true
+};
+extern const hl_ethtrans_mapping_t* hl_ethtrans_map = &s_hl_ethtrans_mapping;
+
+extern void* hl_ethtrans_chip_init_param = NULL;
+
+
+#endif//defined(HL_USE_UTIL_ETHTRANS)
+
+
+
+#if     defined(HL_USE_UTIL_CAN)
+
+#include "hl_can.h"
+
+static const hl_can_mapping_t s_hl_can_mapping =
+{
+    .supportedmask      = (1 << hl_can1) | (0 << hl_can2),
+    .gpiomap            =
+    {   
+        {   // hl_can1 
+            .rx = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin8 }, 
+                .af32   = GPIO_AF_CAN1       
+            }, 
+            .tx = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin9 }, 
+                .af32   = GPIO_AF_CAN1        
+            } 
+        }, 
+        {   // hl_can2 
+            .rx = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin5 }, 
+                .af32   = GPIO_AF_CAN2        
+            }, 
+            .tx = 
+            {
+                .gpio   = { .port = hl_gpio_portB,      .pin = hl_gpio_pin6 }, 
+                .af32   = GPIO_AF_CAN2             
+            } 
+        }   
+    } 
+};
+
+extern const hl_can_mapping_t* hl_can_map = &s_hl_can_mapping;
+
+
+#endif//defined(HL_USE_UTIL_CAN)
+
+
+#if     defined(HL_USE_UTIL_TIMER)
+
+#include "hl_timer.h"
+
+static const hl_timer_mapping_t s_hl_timer_mapping =
+{
+    .supportedmask      = (0 << hl_timer1) | (0 << hl_timer2) | (1 << hl_timer3) | (1 << hl_timer4) | 
+                          (0 << hl_timer5) | (1 << hl_timer6) | (1 << hl_timer7) | (0 << hl_timer8),
+};
+
+extern const hl_timer_mapping_t* hl_timer_map = &s_hl_timer_mapping;
+
+#endif//defined(HL_USE_UTIL_TIMER)
+
+
+
+#if     defined(HL_USE_UTIL_SPI)
+
+
+#include "hl_spi.h"
+
+static const hl_spi_mapping_t s_hl_spi_mapping =
+{
+    .supportedmask      = (1 << hl_spi1) | (1 << hl_spi2) ,
+    .gpiomap            =
+    { 
+       {    // hl_spi1 
+            .sck = 
+            {
+                .gpio   = { .port = hl_gpio_portC,      .pin = hl_gpio_pin10 }, 
+                .af32   = hl_NA32        
+            }, 
+            .miso = 
+            {
+                .gpio   = { .port = hl_gpio_portC,      .pin = hl_gpio_pin1 }, 
+                .af32   = hl_NA32               
+            },
+            .mosi = 
+            {
+                .gpio   = { .port = hl_gpio_portC,      .pin = hl_gpio_pin12 }, 
+                .af32   = hl_NA32               
+            }             
+        },         
+        {   // hl_spi2 
+            .sck = 
+            {
+                .gpio   = { .port = hl_gpio_portI,      .pin = hl_gpio_pin1 }, 
+                .af32   = GPIO_AF_SPI2        
+            }, 
+            .miso = 
+            {
+                .gpio   = { .port = hl_gpio_portC,      .pin = hl_gpio_pin2 }, 
+                .af32   = GPIO_AF_SPI2               
+            },
+            .mosi = 
+            {
+                .gpio   = { .port = hl_gpio_portI,      .pin = hl_gpio_pin3 }, 
+                .af32   = GPIO_AF_SPI2               
+            }             
+        },         
     } 
 };
 
