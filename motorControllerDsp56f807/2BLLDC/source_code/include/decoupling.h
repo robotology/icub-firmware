c13cc349bd5a969954af6ddbdd5722561f8cf011
Marco Randazzo <marco.randazzo@iit.it>
2014-09-14 08:34:19 +0000

    just added some #warning messages to check which parts of code which are effectively compiled
    svn path=/trunk/iCub/firmware/; revision=29668

--- motorControllerDsp56f807/2BLLDC/source_code/include/decoupling.h
+++ motorControllerDsp56f807/2BLLDC/source_code/include/decoupling.h
@@ -4,6 +4,11 @@
 #include "dsp56f807.h"
 
 #define USE_NEW_DECOUPLING 1
+#ifdef USE_NEW_DECOUPLING
+	#warning USING POSITION+TORQUE DECOUPLING ALGORITHM 
+#else
+    #warning USING POSITION DECOUPLING ALGORITHM ONLY
+#endif
 
 void decouple_positions (void);
 //void decouple_dutycycle (Int32 *);
--- motorControllerDsp56f807/2BLL_IE/source_code/include/decoupling.h
+++ motorControllerDsp56f807/2BLL_IE/source_code/include/decoupling.h
@@ -6,6 +6,13 @@
 //#define USE_NEW_DECOUPLING 1
 //#define USE_PARAMETRIC_DECOUPLING 1
 
+#ifdef USE_NEW_DECOUPLING
+	#warning USING POSITION+TORQUE DECOUPLING ALGORITHM 
+#else
+    #warning USING POSITION DECOUPLING ALGORITHM ONLY
+#endif
+
+
 void decouple_positions (void);
 void decouple_dutycycle (Int32 *);
 void decouple_dutycycle_new_motor(Int32 *);
