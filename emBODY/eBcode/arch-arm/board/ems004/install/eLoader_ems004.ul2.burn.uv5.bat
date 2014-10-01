8db1287eb9ed1c56dfdf0b4a67c5690603f2e745
Marco Accame <marco.accame@iit.it>
2014-10-01 07:30:36 +0000

    added programming scripts for ulink-2
    svn path=/trunk/iCub/firmware/; revision=29722

---
+++ emBODY/eBcode/arch-arm/board/ems004/install/eLoader_ems004.ul2.burn.uv5.bat
@@ -0,0 +1,10 @@
+mkdir rep
+copy ..\env\eLoader\bin\eLoader_ems4rd.hex rep
+copy rep\eLoader_ems4rd.hex tools\target.hex 
+C:\Keil_v5\UV4\UV4 -f tools\burn_target.uvprojx -o ..\rep\eLoader_ems4rd_burn_report.txt -t ems4ul2burn
+del /F tools\*.bak
+del /F tools\*.dep
+del /F tools\*.plg
+del /F tools\*.uvgui*
+del /F tools\*.htm
+del /F tools\target.hex
