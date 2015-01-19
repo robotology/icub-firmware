8ed39abc35f2608c63cf967ba596722038bb2092
Marco Accame <marco.accame@iit.it>
2015-01-19 13:12:13 +0000

    split functionalities of eom_emsconfigurator_hid_userdef_ProcessUserdefEvent() into two functions assocated to two different events. now one function manages rx of can frames and the other the timer used for sending/checking fw version of mc4 and 2foc boards
    svn path=/trunk/iCub/firmware/; revision=29950

--- emBODY/eBcode/arch-arm/board/ems001/appl/v01/app_cfg/overridden_configurator.c
+++ emBODY/eBcode/arch-arm/board/ems001/appl/v01/app_cfg/overridden_configurator.c
@@ -91,12 +91,12 @@
 // marco.accame on Nov 26 2014: the user-defined emsconfigurator_evt_userdef is triggered by the EOtheEMSapplBody to
 // tell the EOMtheEMSconfigurator that has received a CAN frame.
 
-extern void eom_emsconfigurator_hid_userdef_ProcessUserdefEvent(EOMtheEMSconfigurator* p)
+extern void eom_emsconfigurator_hid_userdef_ProcessUserdef00Event(EOMtheEMSconfigurator* p)
 {
-    static uint32_t waittimefor2foc = 0;
     eOresult_t  res;
     uint8_t     numofRXcanframe = 0;
-    uint32_t    canBoardsReady = 0;
+
+    
     EOappCanSP  *appcanSP = eo_emsapplBody_GetCanServiceHandle(eo_emsapplBody_GetHandle());
     EOappTheDB  *db = eo_emsapplBody_GetDataBaseHandle(eo_emsapplBody_GetHandle());
    
@@ -112,36 +112,48 @@ extern void eom_emsconfigurator_hid_userdef_ProcessUserdefEvent(EOMtheEMSconfigu
     eo_appCanSP_read(appcanSP, eOcanport1, numofRXcanframe, NULL);
     
 
-
+    // se non ho la skin, allora entro dentro
     if(!(eo_emsapplBody_HasDevice(eo_emsapplBody_GetHandle(), eo_emsapplbody_deviceid_skin)))
     {
         numofRXcanframe = 0;
         res = eo_appCanSP_GetNumOfRecCanframe(appcanSP, eOcanport2, &numofRXcanframe);
         if(eores_OK != res)
-        {
+        { // se c'e un errore sui parametri passati ovvero mai., 
             return;
         }
 #ifdef _GET_CANQUEUE_STATISTICS_
         eo_theEMSdgn_updateCanRXqueueStatisticsOnConfigMode(eOcanport2, numofRXcanframe);
 #endif
+        // se non ho la skin leggo sul can2 tutti i canframe presenti e per ognuno chiamo le funzioni di callback 
+        // sono i messaggi provenienti dai sensori strain
         eo_appCanSP_read(appcanSP, eOcanport2, numofRXcanframe, NULL);
     }
-    
-    if(eo_appTheDB_areConnectedCanBoardsReady(db,&canBoardsReady))
+        
+}
+
+
+// marco.accame on Jan 15 2015: the user-defined emsconfigurator_evt_userdef01 is triggered by a timer inside the EOtheEMSapplBody to
+// tell the that we must do again of verification of presence of can boards.
+
+extern void eom_emsconfigurator_hid_userdef_ProcessUserdef01Event(EOMtheEMSconfigurator* p)
+{
+    uint32_t readyCanBoardsMask = 0;    // keeps the boards that are ready. if bit pos i-th is 1, then the board in list i-th is OK   
+
+    EOappTheDB  *db = eo_emsapplBody_GetDataBaseHandle(eo_emsapplBody_GetHandle());
+
+
+    // verifico che le board mc4 ed 1foc siano ready, ovvero che abbiamo mandato la loro fw version
+    if(eobool_true == eo_appTheDB_areConnectedCanBoardsReady(db, &readyCanBoardsMask))
     {
-        eo_emsapplBody_setCanBoardsAreReady(eo_emsapplBody_GetHandle());
+        // se tutte le 1foc e le mc4 sono ready, allora setto che lo sono (stoppo il timer canBoardsReady_timer da 10 milli)
+        // e poi mando la configurazione alle board can
+        eo_emsapplBody_checkCanBoards_Stop(eo_emsapplBody_GetHandle());
         eo_emsapplBody_sendConfig2canboards(eo_emsapplBody_GetHandle());
     }
     else
-    {
-        if(waittimefor2foc<MAX_WAITFOR2FOC)
-        {
-            waittimefor2foc++;
-        }
-        else
-        {
-            eo_emsapplBody_checkCanBoardsAreReady(eo_emsapplBody_GetHandle(), canBoardsReady);
-        }
+    {  
+        // i check again if the can boards are ready. however, i dont check the boards already ready
+        eo_emsapplBody_checkCanBoardsAreReady(eo_emsapplBody_GetHandle(), readyCanBoardsMask);
     }
     
 }
