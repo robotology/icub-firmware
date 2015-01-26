096209e27122b722a64e3c8f375cc656ca38eef9
Marco Accame <marco.accame@iit.it>
2015-01-26 09:55:01 +0000

    version 1.51 which contains correction in acquisition of strain values
    svn path=/trunk/iCub/firmware/; revision=29984

--- emBODY/eBcode/arch-arm/board/ems004/appl/v1/app_cfg/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v1/app_cfg/EOMtheEMSapplCfg_cfg.h
@@ -84,7 +84,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          1
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          50
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          51
 //  </h>version
 
 //  <h> build date
@@ -93,11 +93,11 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        1
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          22
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          26
 //  <o> hour            <0-23> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         15
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          43
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          30
 //  </h>build date
 
 // </h>Info 
