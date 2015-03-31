b2a655422f8e034176874939d24ccdc048e0c822
Marco Accame <marco.accame@iit.it>
2015-03-31 14:11:46 +0000

    version 1.70
    svn path=/trunk/iCub/firmware/; revision=30220

--- emBODY/eBcode/arch-arm/board/ems004/appl/v1/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v1/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -93,11 +93,11 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        3
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          24
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          31
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         15
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         16
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          00
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          13
 //  </h>build date
 
 // </h>Info 
