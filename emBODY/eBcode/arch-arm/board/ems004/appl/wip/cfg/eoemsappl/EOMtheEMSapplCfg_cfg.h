776dec64ffc917e7347a29960ae55a6fe35660c1
marco.accame <marco.accame@iit.it>
2015-05-25 14:33:12 +0200

    changed version of application in order to test it. it works on icubgenova02, board eb1

--- emBODY/eBcode/arch-arm/board/ems004/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -77,7 +77,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          9
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          73
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          99
 //  </h>version
 
 //  <h> build date
@@ -86,11 +86,11 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        5
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          11
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          25
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         15
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         13
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          25
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          35
 //  </h>build date
 
 // </h>Info 
