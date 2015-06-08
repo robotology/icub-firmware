b1a9ea63876411ea0b3854b3af75e2556a29ea3d
marco.accame <marco.accame@iit.it>
2015-06-08 10:56:49 +0200

    version 2.1

--- emBODY/eBcode/arch-arm/board/ems004/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -81,7 +81,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          2
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          0
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          1
 //  </h>version
 
 //  <h> build date
@@ -90,9 +90,9 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        6
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          3
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          8
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         19
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         10
 //  <o> minute          <0-59> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          55
 //  </h>build date
