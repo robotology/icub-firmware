534ce082824ca8d5bac5bd3603b27fecc976e809
davege89 <davide.pollarolo@iit.it>
2015-09-17 16:11:16 +0200

    Temporary fix to compile mc4plus-WIP project. Since another EotheCANDiscovery is being developed (but still in progress for ems4rd), a macro is used to switch the behaviour for mc4plus (old can discovery) and ems4rd(new one)

--- emBODY/eBcode/arch-arm/embobj/plus/can/config-can-protocol/EoCANprotASpolling.c
+++ emBODY/eBcode/arch-arm/embobj/plus/can/config-can-protocol/EoCANprotASpolling.c
@@ -35,7 +35,11 @@
 #include "EOMtheEMSconfigurator.h"
 
 #include "EOtheCANservice.h"
+#if defined (USE_MC4PLUS)
+#include "EOtheCANdiscovery.h"
+#elif defined (USE_EMS4RD)
 #include "EOtheCANdiscovery2.h"
+#endif
 
 #include "EOMtheEMStransceiver.h"
 
@@ -233,9 +237,12 @@ extern eOresult_t eocanprotASpolling_parser_POL_AS_CMD__GET_FIRMWARE_VERSION(eOc
     detected.protocolversion.major      = frame->data[5];
     detected.protocolversion.minor      = frame->data[6];   
     
-    
-    eo_candiscovery2_OneBoardIsFound(eo_candiscovery2_GetHandle(), loc, match, &detected);
-    
+#if defined (USE_MC4PLUS)
+    eo_candiscovery_ManageDetectedBoard(eo_candiscovery_GetHandle(), loc, match, &detected);
+#elif defined (USE_EMS4RD)
+    eo_candiscovery2_OneBoardIsFound(eo_candiscovery2_GetHandle(), loc, match, &detected);   
+#endif
+   
     return eores_OK;
 }
 
