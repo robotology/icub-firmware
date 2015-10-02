d1fbfb3f257465e49f15bd13377236f8be38595a
marco.accame <marco.accame@iit.it>
2015-10-02 16:49:06 +0200

    modified object EOtheCANmapping so that it can safely load / unload boards and entities in runtime.
    the purpose is to initialize the object one at startup and then:
    - load boards and entities as long as robotInterface asks to activate a service which uses those boards,
    - unload them when robotInterface shuts down, so that at its next restart we can potentially use different boards or different services.

--- emBODY/eBcode/arch-arm/embobj/plus/can/EOtheCANmapping_hid.h
+++ emBODY/eBcode/arch-arm/embobj/plus/can/EOtheCANmapping_hid.h
@@ -55,7 +55,7 @@ struct EOtheCANmapping_hid
     eOcanmap_board_extended_t****   entitylocation;     // [ep][ent][index]-> pointer to
     eOcanmap_board_extended_t***    skinlocation;       // [index]-> array[] of up to 8 pointers to 
     uint8_t                         numofskinboardsindex[eocanmap_skins_maxnumberof];
-    eOcanmap_arrayof_locations_t    arrayofboardlocations;
+//    eOcanmap_arrayof_locations_t    arrayofboardlocations;
     uint32_t                        tobedefined;
 };
 
