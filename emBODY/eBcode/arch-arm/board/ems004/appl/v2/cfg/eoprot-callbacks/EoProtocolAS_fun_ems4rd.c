34c7589c84fdda52954b7dbeb7d3610a895efa3c
marco.accame <marco.accame@iit.it>
2015-10-14 15:12:57 +0200

    removed dead code. using the more compact eom_emsappl_Transmit_OccasionalROP() which loads the ROP and forces its tx

--- emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoprot-callbacks/EoProtocolAS_fun_ems4rd.c
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoprot-callbacks/EoProtocolAS_fun_ems4rd.c
@@ -57,7 +57,7 @@
 #include "EOtheSTRAIN.h"
 #include "EOtheInertial.h"
 
-#include "EOMtheEMSconfigurator.h"
+//#include "EOMtheEMSconfigurator.h"
 
 #include "EOMtheEMStransceiver.h"
 
@@ -92,9 +92,6 @@
 
 static eOresult_t s_on_fullscaleofstrain_received(EOtheSTRAIN* p, eObool_t operationisok);
     
-//static void s_process_mais_resolution(eOas_maisresolution_t resolution, eOas_mais_status_t* status);
-//static void s_signalGetFullScales(uint32_t id32, eObool_t signaloncefullscale);
-
 // useful for any debug activity. comment it out if not used
 //static void s_send_diagnostics(eOerrmanErrorType_t type, uint32_t code, uint16_t param, uint64_t par64);
 
@@ -118,187 +115,32 @@ static eOresult_t s_on_fullscaleofstrain_received(EOtheSTRAIN* p, eObool_t opera
 // --------------------------------------------------------------------------------------------------------------------
 
 
-//extern void eoprot_fun_INIT_as_mais_config(const EOnv* nv)
-//{
-//    eOas_mais_config_t* cfg = eo_nv_RAM(nv);
-//    
-//    cfg->datarate = 10;
-//    cfg->mode = eoas_maismode_txdatacontinuously;
-//    cfg->resolution = eoas_maisresolution_08;
-//}
-
-
-//extern void eoprot_fun_INIT_as_mais_status(const EOnv* nv)
-//{
-//    eOas_mais_status_t *status = eo_nv_RAM(nv);  
-//    
-//    // marco.accame: i init as for eoas_maisresolution_08 
-//    //               the array the15values can be initted for size 0 or 15 as i now use teh proper eo_array_Assign() method
-//    uint8_t capacity    = 15;
-//    uint8_t itemsize    = 1;
-//    uint8_t size        = 15;
-//    EOarray* array = eo_array_New(capacity, itemsize, &status->the15values);
-//    eo_array_Resize(array, size);
-//}
-
-
-
 extern void eoprot_fun_UPDT_as_mais_config(const EOnv* nv, const eOropdescriptor_t* rd)
 {
     eOas_mais_config_t *cfg = (eOas_mais_config_t*)rd->data;    
-    eo_mais_Config(eo_mais_GetHandle(), cfg);
-    
-//    uint8_t number = eoprot_ID2index(rd->id32);
-//    eOsmStatesEMSappl_t currentstate = eo_sm_emsappl_STerr;    
-//    eOcanprot_command_t command = {0};
-//    command.class = eocanprot_msgclass_pollingAnalogSensor;
-//    
-//    //if pc104 tell me to enable maistx, before to send cmd verify if i'm in RUN state:
-//    // if yes ==> ok no problem
-//    // if no ==> i'll send cmd when go to RUN state
-//    if(eoas_maismode_txdatacontinuously == cfg->mode)
-//    {
-//        // only if the appl is in RUN state we enable mais tx
-//        eom_emsappl_GetCurrentState(eom_emsappl_GetHandle(), &currentstate);
-//        if(eo_sm_emsappl_STrun == currentstate)
-//        {
-//            command.type  = ICUBCANPROTO_POL_AS_CMD__SET_TXMODE;
-//            command.value = &(cfg->mode);
-//            eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);
-//        }
-//    }    
-//    
-//    command.type  = ICUBCANPROTO_POL_AS_CMD__SET_CANDATARATE;
-//    command.value = &(cfg->datarate);
-//    eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);
-
-//    command.type  = ICUBCANPROTO_POL_AS_CMD__SET_RESOLUTION;
-//    command.value = &(cfg->resolution);
-//    eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);    
-//    
-//    
-//    eOas_mais_status_t *status = eo_entities_GetMaisStatus(eo_entities_GetHandle(), number);
-//    if(NULL == status)
-//    {
-//        return; //error
-//    }     
-
-//    s_process_mais_resolution((eOas_maisresolution_t)cfg->resolution, status);
+    eo_mais_Set(eo_mais_GetHandle(), cfg);
 }
 
 
 extern void eoprot_fun_UPDT_as_mais_config_mode(const EOnv* nv, const eOropdescriptor_t* rd)
 {
     eOas_maismode_t *maismode = (eOas_maismode_t*)rd->data;
-    eo_mais_ConfigMode(eo_mais_GetHandle(), *maismode);
-    
-//    //uint8_t number = eoprot_ID2index(rd->id32);
-//    eOas_maismode_t *maismode = (eOas_maismode_t*)rd->data;
-//    eOsmStatesEMSappl_t currentstate = eo_sm_emsappl_STerr;
-//    eOcanprot_command_t command = {0};
-//    command.class = eocanprot_msgclass_pollingAnalogSensor;
-//    command.type  = ICUBCANPROTO_POL_AS_CMD__SET_TXMODE;
-//    command.value = maismode;
-//    
-//    //if pc104 tell me to enable maistx, before to send cmd verify if i'm in RUN state:
-//    // if yes ==> ok no problem
-//    // if no ==> i'll send cmd when go to RUN state
-//    if(eoas_maismode_txdatacontinuously == *maismode)
-//    {
-//        //only if the appl is in RUN state enable mais tx
-//        eom_emsappl_GetCurrentState(eom_emsappl_GetHandle(), &currentstate);
-//        if(eo_sm_emsappl_STrun == currentstate)
-//        {
-//            eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);
-//        }
-//    }
-//    else
-//    {
-//         eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);
-//    }
+    eo_mais_SetMode(eo_mais_GetHandle(), *maismode);
 }
 
 
 extern void eoprot_fun_UPDT_as_mais_config_datarate(const EOnv* nv, const eOropdescriptor_t* rd)
 {
     uint8_t *datarate = (uint8_t*)rd->data;
-    eo_mais_ConfigDataRate(eo_mais_GetHandle(), *datarate);
-    
-//    eOcanprot_command_t command = {0};
-//    command.class = eocanprot_msgclass_pollingAnalogSensor;
-//    command.type  = ICUBCANPROTO_POL_AS_CMD__SET_CANDATARATE;
-//    command.value = datarate;
-//    
-//    eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);    
+    eo_mais_SetDataRate(eo_mais_GetHandle(), *datarate);
 }
 
 
 extern void eoprot_fun_UPDT_as_mais_config_resolution(const EOnv* nv, const eOropdescriptor_t* rd)
 {
     eOas_maisresolution_t *resolution = (eOas_maisresolution_t*)rd->data;    
-    eo_mais_ConfigResolution(eo_mais_GetHandle(), *resolution);
-    
-//    uint8_t number = eoprot_ID2index(rd->id32);
-//    eOcanprot_command_t command = {0};
-//    command.class = eocanprot_msgclass_pollingAnalogSensor;
-//    command.type  = ICUBCANPROTO_POL_AS_CMD__SET_RESOLUTION;
-//    command.value = resolution;
-//    
-//    eo_canserv_SendCommandToEntity(eo_canserv_GetHandle(), &command, rd->id32);    
-//    
-//    eOas_mais_status_t *status = eo_entities_GetMaisStatus(eo_entities_GetHandle(), number);
-//    if(NULL == status)
-//    {
-//        return; //error
-//    }     
-
-//    s_process_mais_resolution(*resolution, status);   
+    eo_mais_SetResolution(eo_mais_GetHandle(), *resolution);  
 }
-
-
-//extern void eoprot_fun_INIT_as_strain_config(const EOnv* nv)
-//{
-//    eOas_strain_config_t* straincfg = eo_nv_RAM(nv);
-//    
-//    straincfg->datarate = 10;
-//    straincfg->mode = eoas_strainmode_acquirebutdonttx;
-//    straincfg->signaloncefullscale = eobool_false;
-//}
-
-
-//extern void eoprot_fun_INIT_as_strain_status(const EOnv* nv)
-//{
-//    eOas_strain_status_t *status = eo_nv_RAM(nv);
-//    uint8_t capacity = 0;
-//    uint8_t itemsize = 0;
-//    uint8_t size = 0;
-//    EOarray* array = NULL;
-//    
-//    // marco.accame: cannot understand why in here size is zero and in others it was not zero. see why. 
-//    //               probably fullscale, being of itemsize = 2 is treated without the proper eo_array_ methods
-//    capacity    = 6;
-//    itemsize    = 2;
-//    size        = 0; 
-//    array = eo_array_New(capacity, itemsize, &status->fullscale);
-//    eo_array_Resize(array, size);
-
-
-//    // marco.accame: size can be 0 or 6. now i use teh proper eo_array_Assign method for calibratedvalues/uncalibratedvalues 
-//    //
-//    capacity    = 6;
-//    itemsize    = 2;
-//    size        = 6;
-//    array = eo_array_New(capacity, itemsize, &status->calibratedvalues);
-//    eo_array_Resize(array, size);   
-
-//    capacity    = 6;
-//    itemsize    = 2;
-//    size        = 6;
-//    array = eo_array_New(capacity, itemsize, &status->uncalibratedvalues);
-//    eo_array_Resize(array, size); 
-//}
-
-
 
 
 extern void eoprot_fun_UPDT_as_strain_config(const EOnv* nv, const eOropdescriptor_t* rd)
@@ -429,7 +271,9 @@ static eOresult_t s_on_fullscaleofstrain_received(EOtheSTRAIN* p, eObool_t opera
     ropdesc.data                    = NULL;
 
    
-    res = eo_transceiver_OccasionalROP_Load(eom_emstransceiver_GetTransceiver(eom_emstransceiver_GetHandle()), &ropdesc); 
+   
+//    res = eo_transceiver_OccasionalROP_Load(eom_emstransceiver_GetTransceiver(eom_emstransceiver_GetHandle()), &ropdesc); 
+    res = eom_emsappl_Transmit_OccasionalROP(eom_emsappl_GetHandle(), &ropdesc);
     if(eores_OK != res)
     {
         eo_errman_Error(eo_errman_GetHandle(), eo_errortype_error, "cannot sig<strain.fullscale>", NULL, &eo_errman_DescrRuntimeErrorLocal);        
@@ -444,18 +288,19 @@ static eOresult_t s_on_fullscaleofstrain_received(EOtheSTRAIN* p, eObool_t opera
         //des.sourcedevice = eo_errman_sourcedevice_localboard;
         //eo_errman_Error(eo_errman_GetHandle(), eo_errortype_debug, NULL, NULL, &des);
     }
-    
+//    
 
-    // 2. now i alert someone to transmit it
+//    // 2. now i alert someone to transmit it   
+//    eom_emsappl_SendTXRequest(eom_emsappl_GetHandle());
     
-    eOsmStatesEMSappl_t status;
-    eom_emsappl_GetCurrentState(eom_emsappl_GetHandle(), &status);
-    
-    // if application is in cfg state, then we send a request to configurator to send ropframe out. otherwise, if in RUN mode, the control-loop wills end it 
-    if(eo_sm_emsappl_STcfg == status)
-    {
-        eom_task_SetEvent(eom_emsconfigurator_GetTask(eom_emsconfigurator_GetHandle()), emsconfigurator_evt_ropframeTx); 
-    }
+//    eOsmStatesEMSappl_t status;
+//    eom_emsappl_GetCurrentState(eom_emsappl_GetHandle(), &status);
+//    
+//    // if application is in cfg state, then we send a request to configurator to send ropframe out. otherwise, if in RUN mode, the control-loop will send it 
+//    if(eo_sm_emsappl_STcfg == status)
+//    {
+//        eom_task_SetEvent(eom_emsconfigurator_GetTask(eom_emsconfigurator_GetHandle()), emsconfigurator_evt_ropframeTx); 
+//    }
     
     return(eores_OK);
 }
