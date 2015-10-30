32baa290dae16af2712ecbd63786c5f675f50de6
marco.accame <marco.accame@iit.it>
2015-10-30 17:04:06 +0100

    removed a comment

--- emBODY/eBcode/arch-arm/embobj/plus/ctrloop/EOMtheEMStransceiver.c
+++ emBODY/eBcode/arch-arm/embobj/plus/ctrloop/EOMtheEMStransceiver.c
@@ -308,7 +308,6 @@ extern eOresult_t eom_emstransceiver_Form(EOMtheEMStransceiver* p, EOpacket** tx
         *numberofrops = numofrops;
     }
     
-    //even if numofrops is equal to zero, i send a rop because it is used by pc104 to see the ems.
     // we now retrieve the out packet only if we have at least one rop to tx. it is the transmitter inside
     // the transceiver that decides which rops to tx. even if we call every ms we can tx only when needed
     // and we reduce tx rate.
