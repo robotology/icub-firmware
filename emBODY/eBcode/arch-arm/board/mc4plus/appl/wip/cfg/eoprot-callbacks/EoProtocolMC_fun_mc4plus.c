64c265570605f4d2a9248f02f0c77315b6a0ae43
davege89 <davide.pollarolo@iit.it>
2015-10-30 16:12:12 +0100

    Motors fault mask setting procedure adjusted to not clear the already set errors

--- emBODY/eBcode/arch-arm/board/mc4plus/appl/wip/cfg/eoprot-callbacks/EoProtocolMC_fun_mc4plus.c
+++ emBODY/eBcode/arch-arm/board/mc4plus/appl/wip/cfg/eoprot-callbacks/EoProtocolMC_fun_mc4plus.c
@@ -78,7 +78,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 // - declaration of static functions
 // --------------------------------------------------------------------------------------------------------------------
-static void s_eoprot_ep_mc_fun_MotorReactivationAttempt(uint8_t motor);
+static void s_eoprot_ep_mc_fun_MotorReactivationAttempt(uint8_t motor, uint32_t current_state);
 
 
 
@@ -508,10 +508,10 @@ extern void eoprot_fun_UPDT_mc_joint_cmmnds_calibration(const EOnv* nv, const eO
     // commento: la calib tipo 3 ha solo offset (in xml e' il calibration3. il calibration2 etc non viene usato).
     // per calib 3: viene usato solo il calibration3.
     
-    uint16_t state = eo_mcserv_GetMotorFaultMask(eo_mcserv_GetHandle(),jxx);
+    uint32_t state = eo_mcserv_GetMotorFaultMask(eo_mcserv_GetHandle(),jxx);
     if ((eo_mcserv_AreMotorsExtFaulted(eo_mcserv_GetHandle())) || (state & MOTOR_EXTERNAL_FAULT)) //or motors still faulted OR state (and so PWM) still need to be enabled
     {
-        s_eoprot_ep_mc_fun_MotorReactivationAttempt(jxx);
+        s_eoprot_ep_mc_fun_MotorReactivationAttempt(jxx, state);
     }
 
     //check for the type of calibration required
@@ -568,10 +568,10 @@ extern void eoprot_fun_UPDT_mc_joint_cmmnds_controlmode(const EOnv* nv, const eO
     eOprotIndex_t jxx = eoprot_ID2index(rd->id32);
 
     //if this joint was in external fault or the state (and so PWM) is not updated, reenable it
-    uint16_t state = eo_mcserv_GetMotorFaultMask(eo_mcserv_GetHandle(),jxx);
+    uint32_t state = eo_mcserv_GetMotorFaultMask(eo_mcserv_GetHandle(),jxx);
     if ((eo_mcserv_AreMotorsExtFaulted(eo_mcserv_GetHandle())) || (state & MOTOR_EXTERNAL_FAULT))
     {
-       s_eoprot_ep_mc_fun_MotorReactivationAttempt(jxx);
+       s_eoprot_ep_mc_fun_MotorReactivationAttempt(jxx, state);
     }
         
     eo_emsController_SetControlModeGroupJoints(jxx, (eOmc_controlmode_command_t)(*controlmode));       
@@ -727,14 +727,16 @@ extern void eoprot_fun_UPDT_mc_motor_config_maxcurrentofmotor(const EOnv* nv, co
 // --------------------------------------------------------------------------------------------------------------------
 // - definition of static functions 
 // --------------------------------------------------------------------------------------------------------------------
-static void s_eoprot_ep_mc_fun_MotorReactivationAttempt(uint8_t motor)
+static void s_eoprot_ep_mc_fun_MotorReactivationAttempt(uint8_t motor, uint32_t current_state)
 {
      eo_mcserv_EnableMotor(eo_mcserv_GetHandle(), motor);
      eo_mcserv_EnableFaultDetection(eo_mcserv_GetHandle());
      
-	 //simulate the CANframe used by 2FOC to signal the status   
-     uint8_t fault_mask[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}; //clear all the faults
-     eo_mcserv_SetMotorFaultMask(eo_mcserv_GetHandle(), motor, fault_mask);
+     //simulate the CANframe used by 2FOC to signal the status
+     //uint8_t fault_mask[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}; //clear all the faults
+     uint64_t fault_mask = (((uint64_t)(current_state & ~MOTOR_EXTERNAL_FAULT)) << 32) & 0xFFFFFFFF00000000; //adding the error to the current state
+    
+     eo_mcserv_SetMotorFaultMask(eo_mcserv_GetHandle(), motor, (uint8_t*)&fault_mask);
      
      //reports to emscontroller the changed mask
      eo_emsController_CheckFaults();
