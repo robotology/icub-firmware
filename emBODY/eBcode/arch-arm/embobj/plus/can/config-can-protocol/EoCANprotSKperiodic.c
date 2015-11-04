fbc06b25b2bfd9c3ada9ec0c01e415eca5003724
marco.accame <marco.accame@iit.it>
2015-11-04 15:54:08 +0100

    using new method eo_skin_AcceptCANframe() to decode the skin frame

--- emBODY/eBcode/arch-arm/embobj/plus/can/config-can-protocol/EoCANprotSKperiodic.c
+++ emBODY/eBcode/arch-arm/embobj/plus/can/config-can-protocol/EoCANprotSKperiodic.c
@@ -21,19 +21,9 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-#include "stdlib.h"
+
 #include "EoCommon.h"
-
-// in here is whatever is required to offer parsing of can frames and copying into protocol data structures.
-
-#include "EoProtocol.h"
-#include "EoProtocolSK.h"
-
-
-#include "EOtheErrorManager.h"
-#include "EoError.h"
-
-#include "EOtheCANmapping.h"
+#include "EOtheSKIN.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 // - declaration of extern public interface
@@ -69,8 +59,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 // - declaration of static functions
 // --------------------------------------------------------------------------------------------------------------------
-
-static void* s_eocanprotSKperiodic_get_entity(eOprotEndpoint_t endpoint, eOprot_entity_t entity, eOcanframe_t *frame, eOcanport_t port, uint8_t *index);
+// empty-section
 
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -83,48 +72,14 @@ static void* s_eocanprotSKperiodic_get_entity(eOprotEndpoint_t endpoint, eOprot_
 // - definition of extern public functions
 // --------------------------------------------------------------------------------------------------------------------
 
-__weak extern eObool_t eocanprotSKperiodic_redefinable_SkipParsingOf_ANY_PERIODIC_SKIN_MSG(eOsk_skin_t *skin)
-{
-    return(eobool_false);
-}
 
 extern eOresult_t eocanprotSKperiodic_parser_ANY_PERIODIC_SKIN_MSG(eOcanframe_t *frame, eOcanport_t port)
-{   
-    // this can frame is from skin only ... i dont do the check that the board must be a skin
-    // i retrieve the skin entity related to the frame    
-    eOsk_skin_t *skin = NULL;
-    eOprotIndex_t index = EOK_uint08dummy;
-    
-    if(NULL == (skin = s_eocanprotSKperiodic_get_entity(eoprot_endpoint_skin, eoprot_entity_sk_skin, frame, port, &index)))
-    {
-        #warning -> TODO: add diagnostics about not found board as in s_eo_icubCanProto_mb_send_runtime_error_diagnostics()
-        return(eores_OK);  
-    }        
-       
-    if(eobool_true == eocanprotSKperiodic_redefinable_SkipParsingOf_ANY_PERIODIC_SKIN_MSG(skin))
-    {
-        return(eores_OK);
-    }
-    
-    // otherwise we put the canframe content inside teh arrayofcandata
-    eOsk_candata_t candata = {0};
-    uint16_t info = EOSK_CANDATA_INFO(frame->size, frame->id);
-    candata.info = info;    
-    memcpy(candata.data, frame->data, sizeof(candata.data));   
-    
-    if(eores_OK != eo_array_PushBack((EOarray*)(&skin->status.arrayofcandata), &candata))
-    {   
-        eOerrmanDescriptor_t des = {0};
-        des.code            = eoerror_code_get(eoerror_category_Skin, eoerror_value_SK_arrayofcandataoverflow);
-        des.par16           = (frame->id & 0x0fff) | ((frame->size & 0x000f) << 12);
-        des.par64           = eo_common_canframe_data2u64((eOcanframe_t*)frame);
-        des.sourceaddress   = EOCANPROT_FRAME_GET_SOURCE(frame);
-        des.sourcedevice    = (eOcanport1 == port) ? (eo_errman_sourcedevice_canbus1) : (eo_errman_sourcedevice_canbus2);
-        eo_errman_Error(eo_errman_GetHandle(), eo_errortype_warning, NULL, NULL, &des); 
-    }
-
-    return(eores_OK);       
+{
+    eo_skin_AcceptCANframe(eo_skin_GetHandle(), frame, port);
+  
+    return(eores_OK);
 }
+
 
 
 
@@ -138,34 +93,8 @@ extern eOresult_t eocanprotSKperiodic_parser_ANY_PERIODIC_SKIN_MSG(eOcanframe_t 
 // --------------------------------------------------------------------------------------------------------------------
 // - definition of static functions 
 // --------------------------------------------------------------------------------------------------------------------
+// empty-section
 
-static void* s_eocanprotSKperiodic_get_entity(eOprotEndpoint_t endpoint, eOprot_entity_t entity, eOcanframe_t *frame, eOcanport_t port, uint8_t *index)
-{
-    void * ret = NULL;
-    uint8_t ii = 0;
-    eOcanmap_location_t loc = {0};
-    
-    loc.port = port;
-    loc.addr = EOCANPROT_FRAME_GET_SOURCE(frame);    
-    loc.insideindex = eocanmap_insideindex_none;
-    
-    ii = eo_canmap_GetEntityIndexExtraCheck(eo_canmap_GetHandle(), loc, endpoint, entity);
-    
-    if(EOK_uint08dummy == ii)
-    {     
-        #warning -> TODO: add diagnostics about not found board as in s_eo_icubCanProto_mb_send_runtime_error_diagnostics()
-        return(NULL);
-    }
-    
-    ret = eoprot_entity_ramof_get(eoprot_board_localboard, endpoint, entity, ii);
-    
-    if(NULL != index)
-    {
-        *index = ii;        
-    }  
-
-    return(ret);   
-}
 
 
 // --------------------------------------------------------------------------------------------------------------------
