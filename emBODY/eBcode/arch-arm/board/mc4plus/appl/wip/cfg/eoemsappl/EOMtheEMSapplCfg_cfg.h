d14f218bc68c17dd8abf368730b1af13db320a5d
davege89 <davide.pollarolo@iit.it>
2015-12-22 15:17:12 +0100

    MC4plus application now is version 2.15

--- emBODY/eBcode/arch-arm/board/mc4plus/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/mc4plus/appl/wip/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -74,7 +74,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          2
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          14
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          15
 //  </h>version
 
 //  <h> build date
@@ -83,11 +83,11 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        12
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          16
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          22
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         9
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         15
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          40
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          15
 //  </h>build date
 
 // </h>Info 
