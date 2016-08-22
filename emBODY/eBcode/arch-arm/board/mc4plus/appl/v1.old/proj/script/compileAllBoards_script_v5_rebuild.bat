3fb48636cb851f4c102611b971759246bc6e0b34
marco.accame <marco.accame@iit.it>
2016-08-22 16:24:37 +0200

    removed old projects and code for ems004, mc4plus and mc2plus applications

--- emBODY/eBcode/arch-arm/board/mc4plus/appl/v1.old/proj/script/compileAllBoards_script_v5_rebuild.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-FOR /l %%i in (1,1,11) DO  (echo i'm compiling for board %%i
- echo #define EOMTHEEMSAPPLCFG_ID_OF_EMSBOARD     %%i >..\..\app_cfg\extDefEmsBoardNum.h 
- C:\Keil_v5\UV4\UV4 -r ..\ems4rd-v01.uvprojx -o .\script\log%%i.txt -t "ems4rd"
- echo ...done!
- move ..\..\bin\ems004.hex ..\..\bin\ems004_eb%%i.hex 
-)
-
-pause
