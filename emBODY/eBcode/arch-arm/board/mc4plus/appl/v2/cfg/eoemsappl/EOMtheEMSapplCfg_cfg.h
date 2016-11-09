2c452c53a25022ed288bba4f37d03f1faad67bd1
Valentina Gaggero <valentina.gaggero@iit.it>
2016-11-09 21:53:41 +0100

    mc4plus mec v3 ver 2.142: changed version number. I used strange number becuase it isn't official

--- emBODY/eBcode/arch-arm/board/mc4plus/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/mc4plus/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -74,7 +74,11 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          2
 //  <o> minor           <0-255> 
+<<<<<<< HEAD
 #define EOMTHEEMSAPPLCFG_VERSION_MINOR          47
+=======
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          142
+>>>>>>> mc4plus mec v3 ver 2.142: changed version number. I used strange number becuase it isn't official
 //  </h>version
 
 //  <h> build date
@@ -83,9 +87,9 @@ extern "C" {
 //  <o> month           <1-12> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        1
 //  <o> day             <1-31> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          10
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          9
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         12
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         11
 //  <o> minute          <0-59> 
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          39
 //  </h>build date
