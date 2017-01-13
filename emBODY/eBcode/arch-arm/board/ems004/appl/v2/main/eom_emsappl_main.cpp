ea4feade842202e78c3e1c17bedb40d52db9ef7f
marco.accame <marco.accame@iit.it>
2017-01-13 17:06:00 +0100

    added a c++11 test version of the ems project

---
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v2/main/eom_emsappl_main.cpp
@@ -0,0 +1,351 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* @file       eom_emsappl_main.cpp
+	@brief      This file keeps the main of an application on ems using the embobj
+	@author     marco.accame@iit.it
+    @date       05/21/2012
+**/
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+
+#include "EOMtheSystem.h"
+
+#include "EOMtheEMSapplCfg.h"
+#include "EOMtheEMSappl.h"
+
+
+
+#include "EOtheLEDpulser.h"
+
+#include "EoError.h"
+#include "EOtheErrorManager.h"
+
+
+
+#include "EOtheCANmapping.h"
+
+//#include <limits>
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of external variables 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+ 
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+
+#define TEST_CPP11
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables, but better using _get(), _set() 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+
+static void s_eom_emsappl_main_init(void);
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+// empty-section
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+
+
+int main(void)
+{
+    EOMtheEMSapplCfg* emscfg = eom_emsapplcfg_Initialise();
+
+    eom_sys_Initialise( &emscfg->wsyscfg.msyscfg, 
+                        &emscfg->wsyscfg.mempoolcfg,                         
+                        &emscfg->wsyscfg.errmancfg,                 
+                        &emscfg->wsyscfg.tmrmancfg, 
+                        &emscfg->wsyscfg.cbkmancfg 
+                      );  
+    
+    eom_sys_Start(eom_sys_GetHandle(), s_eom_emsappl_main_init);
+}
+
+
+
+extern void eom_emstransceiver_callback_incaseoferror_in_sequencenumberReceived(EOreceiver *receiver)
+{   // the only reason of using the following two variables as static is: to reduce the use of stack.
+    static int64_t delta = 0;
+    static eOerrmanDescriptor_t errdes = 
+    {
+        EO_INIT(.code)          EOERRORCODE(eoerror_category_System, eoerror_value_SYS_transceiver_rxseqnumber_error),
+        EO_INIT(.sourcedevice)  eo_errman_sourcedevice_localboard,
+        EO_INIT(.sourceaddress) 0,
+        EO_INIT(.par16)         0,
+        EO_INIT(.par64)         0
+    };
+    
+    const eOreceiver_seqnum_error_t *err = eo_receiver_GetSequenceNumberError(receiver); 
+    
+    if(NULL == err)
+    {
+        errdes.code  = EOERRORCODE(eoerror_category_System, eoerror_value_SYS_runtimeerror);
+        errdes.par64 = 0x123467fabc222;
+        errdes.par16 = 1;       
+        eo_errman_Error(eo_errman_GetHandle(), eo_errortype_warning, NULL, NULL, &errdes);   
+        return;        
+    }
+    
+    if(1 == err->rec_seqnum)
+    {
+        // it is the first packet received from a remote transmitter freshly initted (i.e., robotInterface has just re-started bu this board was alive well before)
+        // thus, we dont issue an error but an info: 
+        errdes.code  = EOERRORCODE(eoerror_category_System, eoerror_value_SYS_transceiver_rxseqnumber_restarted);
+        errdes.par64 = err->exp_seqnum;
+        errdes.par16 = 1;       
+        eo_errman_Error(eo_errman_GetHandle(), eo_errortype_info, NULL, NULL, &errdes);   
+        
+        // ok, now we must set back the error code as it is normal.
+        errdes.code = EOERRORCODE(eoerror_category_System, eoerror_value_SYS_transceiver_rxseqnumber_error);
+    }  
+    else
+    {    
+        delta = err->rec_seqnum - err->exp_seqnum;
+        if(delta > EO_INT16_MAX)       delta = EO_INT16_MAX;  //32767
+        else if(delta < EO_INT16_MIN)  delta = EO_INT16_MIN;  //-32768;
+        
+        errdes.code             = EOERRORCODE(eoerror_category_System, eoerror_value_SYS_transceiver_rxseqnumber_error);
+        errdes.par16            = (int16_t)delta; 
+        errdes.par64            = err->exp_seqnum; 
+        eo_errman_Error(eo_errman_GetHandle(), eo_errortype_error, NULL, NULL, &errdes);
+    }
+}
+
+
+extern void eom_emstransceiver_callback_incaseoferror_invalidframe(EOreceiver *receiver)
+{  
+    static eOerrmanDescriptor_t errdesinvframe = 
+    {
+        EO_INIT(.code)          EOERRORCODE(eoerror_category_System, eoerror_value_SYS_transceiver_rxinvalidframe_error),
+        EO_INIT(.sourcedevice)  eo_errman_sourcedevice_localboard,
+        EO_INIT(.sourceaddress) 0,
+        EO_INIT(.par16)         0,
+        EO_INIT(.par64)         0
+    };
+
+    eo_errman_Error(eo_errman_GetHandle(), eo_errortype_error, NULL, NULL, &errdesinvframe);
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern hidden functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+#if defined(TEST_CPP11)
+
+#include <new>
+
+#include "EOtimer.h"
+
+#include "EOVtheCallbackManager.h"
+
+namespace embot {
+     typedef uint32_t relTime; // expressed in usec
+     enum class timeValue { microsec = 1, millisec = 1000, second = 1000000 };
+namespace sys {
+        
+class Timer
+{
+public:
+    
+    enum class Mode { oneshot = 0, forever = 1 };
+
+    enum class Status { idle = 0, running = 1, oneshotcompleted = 2 };
+    
+    Timer();
+    ~Timer();
+    bool start(eOreltime_t countdown, Mode mode, EOaction *action); 
+    bool stop(); 
+    
+    Status getStatus();
+
+private:
+    
+    struct Impl;
+    Impl *pImpl;    
+};
+
+}
+}
+
+void ciao(void*p)
+{
+    static uint8_t dd = 0;
+    
+    dd++;
+    dd = dd;
+}
+
+
+namespace embot {   
+namespace sys {
+    
+struct Timer::Impl
+{
+    EOtimer *tmr;
+    Impl() 
+    {
+        tmr = eo_timer_New();
+    }
+    ~Impl()
+    {
+        eo_timer_Delete(tmr);
+    }
+};
+
+Timer::Timer()
+: pImpl(new Impl)
+{   
+
+}
+
+Timer::~Timer()
+{   
+    delete pImpl;
+}
+
+
+bool Timer::start(relTime countdown, Mode mode, EOaction *action)
+{
+    eOresult_t r = eo_timer_Start(pImpl->tmr, eok_abstimeNOW, countdown, static_cast<eOtimerMode_t>(mode), action);    
+    return (eores_OK == r) ? true : false;
+}
+
+bool Timer::stop()
+{
+    eOresult_t r = eo_timer_Stop(pImpl->tmr);    
+    return (eores_OK == r) ? true : false;
+}
+
+Timer::Status Timer::getStatus()
+{
+    eOtimerStatus_t st = eo_timer_GetStatus(pImpl->tmr);    
+    return static_cast<Timer::Status>(st);   
+}
+
+}
+}
+
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of static functions 
+// --------------------------------------------------------------------------------------------------------------------
+
+
+/** @fn         static void s_eom_emsappl_main_init(void)
+    @brief      It initialises the emsappl 
+    @details    bla bla bla.
+ **/
+
+static void s_eom_emsappl_main_init(void)
+{  
+    char str[96];
+    snprintf(str, sizeof(str), "just started the user-defined function s_eom_emsappl_main_init()");
+    eo_errman_Trace(eo_errman_GetHandle(), str, "eom_sys_Start()");  
+
+    snprintf(str, sizeof(str), "s_eom_emsappl_main_init() is about to init EOtheLEDpulser + EOMtheEMSappl");
+    eo_errman_Trace(eo_errman_GetHandle(), str, "eom_sys_Start()");     
+    
+    // init leds via the EOtheLEDpulser object
+    eOledpulser_cfg_t ledpulsercfg =  
+    {
+        EO_INIT(.led_enable_mask)   (1 << eo_ledpulser_led_zero | 1 << eo_ledpulser_led_one | 1 << eo_ledpulser_led_two | 1 << eo_ledpulser_led_three | 1 << eo_ledpulser_led_four | 1 << eo_ledpulser_led_five),
+        EO_INIT(.led_init)          (eOint8_fp_uint8_cvoidp_t) hal_led_init,
+        EO_INIT(.led_on)            (eOint8_fp_uint8_t) hal_led_on,
+        EO_INIT(.led_off)           (eOint8_fp_uint8_t) hal_led_off,
+        EO_INIT(.led_toggle)        (eOint8_fp_uint8_t) hal_led_toggle
+    };
+
+    eo_ledpulser_Initialise(&ledpulsercfg);
+
+#if defined(TEST_CPP11)    
+    
+    EOaction_strg strg;
+    EOaction *act = (EOaction*)&strg;
+    eo_action_SetCallback(act, ciao, NULL, eov_callbackman_GetTask(eov_callbackman_GetHandle()));  
+    
+    embot::sys::Timer *tmr = new embot::sys::Timer;
+    const embot::relTime countdown = 1000*1000;
+    tmr->start(countdown, embot::sys::Timer::Mode::forever, act);
+    
+#endif    
+    
+    // init the ems application
+    EOMtheEMSapplCfg* emscfg = eom_emsapplcfg_GetHandle();    
+    eom_emsappl_Initialise(&emscfg->applcfg);
+
+
+    snprintf(str, sizeof(str), "quitting the user-defined function s_eom_emsappl_main_init()");
+    eo_errman_Trace(eo_errman_GetHandle(), str, "eom_sys_Start()");  
+}
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - end-of-file (leave a blank line after)
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
