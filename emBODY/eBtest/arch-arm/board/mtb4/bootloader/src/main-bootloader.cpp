9b66ae982961c22f5a0ce791c91ed023208665b2
marcoaccame <marco.accame@iit.it>
2017-04-06 18:35:43 +0200

    finalised teh bootloader main

--- emBODY/eBtest/arch-arm/board/mtb4/bootloader/src/main-bootloader.cpp
+++ emBODY/eBtest/arch-arm/board/mtb4/bootloader/src/main-bootloader.cpp
@@ -15,8 +15,10 @@
 #include "embot_sys_theStorage.h"
 
 #include "embot_app_theBootloader.h"
+#include "embot_app_bootloader_theCANparser.h"
 
-#include "embot_hw_FlashBurner.h"
+#include "embot_app_canprotocol.h"
+
 
 struct ActivityParam
 {
@@ -24,13 +26,16 @@ struct ActivityParam
 };
 
 static void bl_activity(void* param);
-
-void periodic_activity(embot::sys::Task *tsk, void *param);
-
 static ActivityParam activity_param = {0};
 
-//static embot::hw::FlashBurner *fb = nullptr;
+static const std::uint8_t NumOfSecondsBeforeJump = 5; 
+static const embot::common::relTime BlinkFastPeriod = 1*EOK_reltime100ms;
+static const embot::common::relTime BlinkSlowPeriod = 5*EOK_reltime100ms;
+static const embot::common::relTime BlinkMadlyPeriod = 5*EOK_reltime10ms;
 
+static const embot::app::canprotocol::versionOfBOOTLOADER vBL = {0, 129};
+static const std::uint8_t defADDRESS = 1;
+static const char defaultInfo32[] = {"I am a stm32l4"};
 
 int main(void)
 { 
@@ -42,8 +47,8 @@ int main(void)
     embot::app::theBootloader::Config config;
     config.userdeflauncher.callback = bl_activity;
     config.userdeflauncher.arg = &activity_param;
-    activity_param.blinkingperiod = 1*EOK_reltime100ms;
-    config.countdown = 5*embot::common::time1second;
+    activity_param.blinkingperiod = BlinkFastPeriod;
+    config.countdown = NumOfSecondsBeforeJump*embot::common::time1second;
     
     switch(cmd)
     {
@@ -54,7 +59,7 @@ int main(void)
         case embot::sys::theJumper::Command::stay:
         {   // we have received a command from the previous running process to stay in here forever
             config.countdown = 0;
-            activity_param.blinkingperiod = 5*EOK_reltime100ms;
+            activity_param.blinkingperiod = BlinkSlowPeriod;
         } break; 
         
         case embot::sys::theJumper::Command::jump:
@@ -62,10 +67,13 @@ int main(void)
             bootloader.jump(param); 
             // if we cannot jump to the specified address ... we stay in here forever
             config.countdown = 0;
-            activity_param.blinkingperiod = 5*EOK_reltime10ms;
+            activity_param.blinkingperiod = BlinkMadlyPeriod;
         } break;          
     }
 
+    // manage the basic canboardinfo
+//    embot::app::theCANboardInfo &canbrdinfo = embot::app::theCANboardInfo::getInstance();    
+//    canbrdinfo.synch(embot::app::canprotocol::Board::mtb, vBL, 1);
     
     bootloader.execute(config);
     
@@ -75,114 +83,112 @@ int main(void)
 
 
 
+
+
+static void eventbasedtask_onevent(embot::sys::Task *t, embot::common::Event evt, void *p);
+static void eventbasedtask_init(embot::sys::Task *t, void *p);
+
+static const embot::common::Event evRXcanframe = 0x00000001;
+
+static embot::sys::EventTask* eventbasedtask = nullptr;
+
+static void alerteventbasedtask(void *arg);
+
+static std::vector<embot::hw::can::Frame> outframes;
+
 static void bl_activity(void* param)
 {
-    
-    ActivityParam* pp = (ActivityParam*) param;
-    
-    uint32_t period = 0;
-    
+    // manage the led blinking
+    ActivityParam* pp = (ActivityParam*) param;    
+    uint32_t period = 0;    
     if(nullptr != pp)
     {
         period = pp->blinkingperiod;
-    }
-    
-    eOledpulser_cfg_t ledconfig = {0};
-    
+    }    
+    eOledpulser_cfg_t ledconfig = {0};    
     ledconfig.led_enable_mask   = (1 << eo_ledpulser_led_zero);
     ledconfig.led_init          = reinterpret_cast<eOint8_fp_uint8_cvoidp_t>(embot::hw::led::init_legacy);
     ledconfig.led_on            = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::on); 
     ledconfig.led_off           = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::off);
-    ledconfig.led_toggle        = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::toggle);
-    
+    ledconfig.led_toggle        = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::toggle);    
     eo_ledpulser_Initialise(&ledconfig);    
-
     eo_ledpulser_Start(eo_ledpulser_GetHandle(), eo_ledpulser_led_zero, period, 0);  
 
+    // manage the basic canboardinfo
+    embot::app::theCANboardInfo &canbrdinfo = embot::app::theCANboardInfo::getInstance();    
+    canbrdinfo.synch(embot::app::canprotocol::Board::mtb, vBL, defADDRESS, defaultInfo32);
     
-    embot::hw::button::init(embot::hw::button::BTN::zero);
-
-    // also start a periodic task which checks ....
-    embot::sys::PeriodicTask *taskper = new embot::sys::PeriodicTask;        
-    taskper->init(nullptr, periodic_activity, 1024, 30, 100*1000, nullptr);
-    
-    // init the storage object ....
-    
-    embot::app::theCANboardInfo &canbrdinfo = embot::app::theCANboardInfo::getInstance();
-    
-    canbrdinfo.synch(embot::app::theCANboardInfo::Type::mtb3, 1);
-    
-    uint8_t adr = canbrdinfo.getCANaddress();
-    adr = adr;
-    if(1 == adr)
-    {
-        canbrdinfo.setCANaddress(2);
-    }
-    
-    //fb = new embot::hw::FlashBurner;
+  
+    // start task waiting for can messages. 
+    eventbasedtask = new embot::sys::EventTask;  
+    const embot::common::relTime waitEventTimeout = 50*1000; //50*1000; //5*1000*1000;    
+    eventbasedtask->init(eventbasedtask_init, eventbasedtask_onevent, 4*1024, 200, waitEventTimeout, nullptr, nullptr);    
         
+    // start canparser
+    embot::app::bootloader::theCANparser &canparser = embot::app::bootloader::theCANparser::getInstance();
+    embot::app::bootloader::theCANparser::Config config;
+    canparser.initialise(config);  
+
+    // finally start can. i keep it as last because i dont want that the isr-handler calls its onrxframe() 
+    // before the eventbasedtask is created.
+    embot::hw::result_t r = embot::hw::resNOK;
+    embot::hw::can::Config canconfig; // default is tx/rxcapacity=8
+    canconfig.txcapacity = 12;
+    canconfig.onrxframe = embot::common::Callback(alerteventbasedtask, nullptr); 
+    r = embot::hw::can::init(embot::hw::can::Port::one, canconfig);
+    r = r;
 }
 
 
-
-void periodic_activity(embot::sys::Task *tsk, void *param)
+static void alerteventbasedtask(void *arg)
 {
-    static uint32_t x = 0;
-    if(true == embot::hw::button::pressed(embot::hw::button::BTN::zero))
+    if(nullptr != eventbasedtask)
     {
-        embot::app::theBootloader & bootloader = embot::app::theBootloader::getInstance();
-        bootloader.stopcountdown();
-        
-        eo_ledpulser_Start(eo_ledpulser_GetHandle(), eo_ledpulser_led_zero, 10*EOK_reltime100ms, 0); 
-        
-        x++;
-        // we stop countdown and we stay in bootloader w/ reduced frequency of ....
-        x = x; 
-#if 0        
-        static std::uint8_t data[40] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
-        static std::uint32_t size = 6;
-        static std::uint32_t adr = embot::hw::sys::addressOfApplication;
-        static std::uint8_t *dd = data;
-        
-        if(1 == x)
+        eventbasedtask->setEvent(evRXcanframe);
+    }
+}
+
+
+static void eventbasedtask_init(embot::sys::Task *t, void *p)
+{
+    embot::hw::result_t r = embot::hw::can::enable(embot::hw::can::Port::one);  
+    r = r;  
+
+    outframes.reserve(12);
+}
+    
+
+
+static void eventbasedtask_onevent(embot::sys::Task *t, embot::common::Event evt, void *p)
+{  
+    
+    if(true == embot::common::msk::check(evt, evRXcanframe))
+    {        
+        embot::hw::can::Frame frame;
+        std::uint8_t remaining = 0;
+        if(embot::hw::resOK == embot::hw::can::get(embot::hw::can::Port::one, frame, remaining))
         {
-            size = 6;
-            fb->add(adr, size, dd);    
-            adr += size;  
-            dd += size;
-        }
-        else if(2 == x)
-        {
-            size = 6;  
-            fb->add(adr, size, dd);    
-            adr += size;  
-            dd += size;            
-        }
-        else if(3 == x)
-        {
-            size = 4; 
-            fb->add(adr, size, dd);    
-            adr = embot::hw::sys::addressOfApplication+2048;  
-            dd += size;                        
-        }
-        else if(4 == x)
-        {
-            size = 6;
-            fb->add(adr, size, dd);    
-            adr += size;  
-            dd += size;
-            size = 6;  
-            fb->add(adr, size, dd);    
-            adr += size;  
-            dd += size;    
-            size = 4; 
-            fb->add(adr, size, dd); 
-            adr = embot::hw::sys::addressOfApplication+2048;  
-            dd += size;             
-        }
-        
-#endif        
-    }    
+            outframes.clear();            
+            embot::app::bootloader::theCANparser &canparser = embot::app::bootloader::theCANparser::getInstance();
+            if(true == canparser.process(frame, outframes))
+            {
+                std::uint8_t num = outframes.size();
+                for(std::uint8_t i=0; i<num; i++)
+                {
+                    embot::hw::can::put(embot::hw::can::Port::one, outframes[i]);                                       
+                }
+
+                embot::hw::can::transmit(embot::hw::can::Port::one);
+                
+            }
+
+            if(remaining > 0)
+            {
+                eventbasedtask->setEvent(evRXcanframe);                 
+            }
+        }        
+    }
+ 
 }
 
 
