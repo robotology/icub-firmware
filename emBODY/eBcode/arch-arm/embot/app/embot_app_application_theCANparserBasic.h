b38a4df8a7afa7d087f49c7abe243d10442f6d22
marcoaccame <marco.accame@iit.it>
2017-04-07 10:41:42 +0200

    added the appl-canparser-basic singleton it contains parsing for all the common can messages which a can board must have: - some bootloader-class messages used to detect board, manage additional info, make it jump to bootloader - all messages of mc-, as-, bl-classes used to change address and get firmware version. if i forgot anything which all the boards must manage, then that is the place where to put it.

---
+++ emBODY/eBcode/arch-arm/embot/app/embot_app_application_theCANparserBasic.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 iCub Facility - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef _EMBOT_APP_APPLICATION_THECANPARSERBASIC_H_
+#define _EMBOT_APP_APPLICATION_THECANPARSERBASIC_H_
+
+#include "embot_common.h"
+
+#include "embot_hw.h"
+
+#include "embot_sys.h"
+
+
+#include <vector>
+
+namespace embot { namespace app { namespace application {
+           
+    class theCANparserBasic
+    {
+    public:
+        static theCANparserBasic& getInstance()
+        {
+            static theCANparserBasic* p = new theCANparserBasic();
+            return *p;
+        }
+        
+        
+    public:
+        struct Config
+        {
+            std::uint32_t   dummy;
+            Config() : dummy(0) {}
+        }; 
+        
+        
+        bool initialise(Config &config); 
+        
+        bool process(const embot::hw::can::Frame &frame, std::vector<embot::hw::can::Frame> &replies);
+
+    private:
+        theCANparserBasic(); 
+
+    public:
+        // remove copy constructors and copy assignment operators
+        theCANparserBasic(const theCANparserBasic&) = delete;
+        theCANparserBasic(theCANparserBasic&) = delete;
+        void operator=(const theCANparserBasic&) = delete;
+        void operator=(theCANparserBasic&) = delete;
+
+    private:    
+        struct Impl;
+        Impl *pImpl;        
+    };       
+
+
+}}} // namespace embot { namespace app { namespace application
+
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
