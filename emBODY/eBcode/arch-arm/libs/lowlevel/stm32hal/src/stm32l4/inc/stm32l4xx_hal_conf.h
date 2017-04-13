ab4bfa419f713027276c06534c0cd8cd4bc6cbf2
marcoaccame <marco.accame@iit.it>
2017-04-13 16:15:52 +0200

    added inclusion of stm32l4xx_ll_gpio.h which is required by some c code files for mtb4 project

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/stm32l4/inc/stm32l4xx_hal_conf.h
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/stm32l4/inc/stm32l4xx_hal_conf.h
@@ -15,12 +15,15 @@ extern "C" {
 #if     defined(STM32HAL_BOARD_NUCLEO64)
     
     // IIT-EXT path is referred to stm32hal/api
-     #include "../src/board/nucleo64/inc/stm32l4xx_hal_conf_nucleo64.h" 
+    #include "../src/board/nucleo64/inc/stm32l4xx_hal_conf_nucleo64.h" 
     
 #elif   defined(STM32HAL_BOARD_MTB4)
     
     // IIT-EXT path is referred to stm32hal/api
-     #include "../src/board/mtb4/inc/stm32l4xx_hal_conf_mtb4.h"     
+    #include "../src/board/mtb4/inc/stm32l4xx_hal_conf_mtb4.h"   
+    // we also need some ll includes
+    #include "../src/stm32l4/inc/stm32l4xx_ll_gpio.h"
+    
 
 #else
     
