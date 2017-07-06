bade72c232ff452249d71d00a3a8aab92f142a0a
marcoaccame <marco.accame@iit.it>
2017-07-06 11:40:09 +0200

    advanced because we have changed code for icub-v3 (amo management in controller)

--- emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -74,7 +74,7 @@ extern "C" {
 //  <o> major           <0-255> 
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          3
 //  <o> minor           <0-255> 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          5
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          6
 
 //  </h>version
 
@@ -85,11 +85,11 @@ extern "C" {
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        6
 //  <o> day             <1-31> 
 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          9
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          22
 //  <o> hour            <0-23> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         14
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         16
 //  <o> minute          <0-59> 
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          41
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          12
 //  </h>build date
 // </h>Info 
 
