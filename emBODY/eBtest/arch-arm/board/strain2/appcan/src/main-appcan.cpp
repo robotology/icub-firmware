95d1a898a66c3f56c383189e4ee2cc32851851a4
marcoaccame <marco.accame@iit.it>
2017-07-14 10:47:10 +0200

    added the strain2 application (basic only) and tested it: it talks to canLoader.

---
+++ emBODY/eBtest/arch-arm/board/strain2/appcan/src/main-appcan.cpp
@@ -0,0 +1,247 @@
+
+
+//#include "EOtheLEDpulser.h"
+#include "embot_app_canprotocol.h"
+#include "embot_app_theCANboardInfo.h"
+
+#include "embot.h"
+
+#include "stm32hal.h" // to see bsp_led_init etc
+#include "embot_hw.h"
+
+#include "embot_hw_FlashStorage.h"
+#include "embot_sys_theStorage.h"
+
+#include "embot_app_theApplication.h"
+
+#include "embot_app_application_theCANparserBasic.h"
+
+//#include "embot_app_application_theCANparserMTB.h"
+
+//#include "embot_app_application_theSkin.h"
+//#include "embot_app_application_theIMU.h"
+
+
+static const embot::app::canprotocol::versionOfAPPLICATION vAP = {1, 0 , 3};
+static const embot::app::canprotocol::versionOfCANPROTOCOL vCP = {2, 0};
+
+static void userdeflauncher(void* param);
+static void userdefonidle(void* param);
+
+static const embot::common::Callback atsysteminit(userdeflauncher, nullptr);
+
+static const embot::common::Callback onidle(userdefonidle, nullptr);
+
+static const embot::app::theApplication::StackSizes stacksizes =  { 2048, 512 };
+
+static const embot::app::theApplication::UserDefOperations operations = { atsysteminit, onidle, {nullptr, nullptr} };
+
+#if defined(APPL_TESTZEROOFFSET)
+static const std::uint32_t address = embot::hw::sys::addressOfBootloader;
+#else
+static const std::uint32_t address = embot::hw::sys::addressOfApplication;
+#endif
+
+int main(void)
+{ 
+    embot::app::theApplication::Config config(embot::common::time1millisec, stacksizes, operations, address);
+    embot::app::theApplication &appl = embot::app::theApplication::getInstance();    
+    
+    appl.execute(config);  
+        
+    for(;;);    
+}
+
+
+static void start_evt_based(void);
+
+static void userdeflauncher(void* param)
+{
+//    eOledpulser_cfg_t ledconfig = {0};
+//    
+//    ledconfig.led_enable_mask   = (1 << eo_ledpulser_led_zero);
+//    ledconfig.led_init          = reinterpret_cast<eOint8_fp_uint8_cvoidp_t>(embot::hw::led::init_legacy); // (eOint8_fp_uint8_cvoidp_t) embot::hw::bsp::led_init_par;
+//    ledconfig.led_on            = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::on);
+//    ledconfig.led_off           = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::off);
+//    ledconfig.led_toggle        = reinterpret_cast<eOint8_fp_uint8_t>(embot::hw::led::toggle);
+//    
+//    eo_ledpulser_Initialise(&ledconfig);  
+//    eo_ledpulser_Start(eo_ledpulser_GetHandle(), eo_ledpulser_led_zero, 1000*1000, 0);    
+        
+    embot::app::theCANboardInfo &canbrdinfo = embot::app::theCANboardInfo::getInstance();
+    canbrdinfo.synch(vAP, vCP);
+    
+    
+    start_evt_based();
+      
+}
+
+
+
+static void eventbasedtask_onevent(embot::sys::Task *t, embot::common::EventMask evtmsk, void *p);
+static void eventbasedtask_init(embot::sys::Task *t, void *p);
+
+static const embot::common::Event evRXcanframe = 0x00000001;
+//static const embot::common::Event evSKINprocess = 0x00000002;
+//static const embot::common::Event evIMUprocess = 0x00000004;
+
+static const std::uint8_t maxOUTcanframes = 48;
+
+static embot::sys::EventTask* eventbasedtask = nullptr;
+
+static void alerteventbasedtask(void *arg);
+
+static std::vector<embot::hw::can::Frame> outframes;
+
+static void start_evt_based(void)
+{ 
+    // start task waiting for can messages. 
+    eventbasedtask = new embot::sys::EventTask;  
+    const embot::common::relTime waitEventTimeout = 50*1000; //50*1000; //5*1000*1000;    
+    eventbasedtask->init(eventbasedtask_init, eventbasedtask_onevent, 4*1024, 200, waitEventTimeout, nullptr, nullptr);    
+        
+    // start canparser basic
+    embot::app::application::theCANparserBasic &canparserbasic = embot::app::application::theCANparserBasic::getInstance();
+    embot::app::application::theCANparserBasic::Config configbasic;
+    canparserbasic.initialise(configbasic);  
+    
+    // start canparser strain2
+//    embot::app::application::theCANparserSTRAIN &canparserstr = embot::app::application::theCANparserSTRAIN::getInstance();
+//    embot::app::application::theCANparserSTRAIN::Config configstr;
+//    canparserstr.initialise(configstr);  
+    
+//    embot::app::application::theSkin &theskin = embot::app::application::theSkin::getInstance();
+//    embot::app::application::theSkin::Config configskin;
+//    configskin.tickevent = evSKINprocess;
+//    configskin.totask = eventbasedtask;
+//    theskin.initialise(configskin);   
+
+
+//    embot::app::application::theIMU &theimu = embot::app::application::theIMU::getInstance();
+//    embot::app::application::theIMU::Config configimu;
+//    configimu.tickevent = evIMUprocess;
+//    configimu.totask = eventbasedtask;
+//    theimu.initialise(configimu); 
+
+    // finally start can. i keep it as last because i dont want that the isr-handler calls its onrxframe() 
+    // before the eventbasedtask is created.
+    embot::hw::result_t r = embot::hw::resNOK;
+    embot::hw::can::Config canconfig; // default is tx/rxcapacity=8
+    canconfig.txcapacity = maxOUTcanframes;
+    canconfig.onrxframe = embot::common::Callback(alerteventbasedtask, nullptr); 
+    r = embot::hw::can::init(embot::hw::can::Port::one, canconfig);
+    r = r;
+}
+
+
+static void alerteventbasedtask(void *arg)
+{
+    if(nullptr != eventbasedtask)
+    {
+        eventbasedtask->setEvent(evRXcanframe);
+    }
+}
+
+
+static void eventbasedtask_init(embot::sys::Task *t, void *p)
+{
+    embot::hw::result_t r = embot::hw::can::enable(embot::hw::can::Port::one);  
+    r = r;  
+
+    outframes.reserve(maxOUTcanframes);
+    #warning --> we should init the objects which holds outframes with maxOUTcanframes ... so that no more than maxOUTcanframes are pushed_back
+}
+    
+
+
+static void eventbasedtask_onevent(embot::sys::Task *t, embot::common::EventMask eventmask, void *p)
+{     
+    if(0 == eventmask)
+    {   // timeout ... 
+        return;
+    }
+    
+    // we clear the frames to be trasmitted
+    outframes.clear();      
+    
+    
+    if(true == embot::common::msk::check(eventmask, evRXcanframe))
+    {        
+        embot::hw::can::Frame frame;
+        std::uint8_t remainingINrx = 0;
+        if(embot::hw::resOK == embot::hw::can::get(embot::hw::can::Port::one, frame, remainingINrx))
+        {            
+            embot::app::application::theCANparserBasic &canparserbasic = embot::app::application::theCANparserBasic::getInstance();
+//            embot::app::application::theCANparserMTB &canparsermtb = embot::app::application::theCANparserMTB::getInstance();
+            // process w/ the basic parser, if not recognised call the parse specific of the board
+            if(true == canparserbasic.process(frame, outframes))
+            {                   
+            }
+//            else if(true == canparsermtb.process(frame, outframes))
+//            {               
+//            }
+            
+            if(remainingINrx > 0)
+            {
+                eventbasedtask->setEvent(evRXcanframe);                 
+            }
+        }        
+    }
+    
+//    if(true == embot::common::msk::check(eventmask, evSKINprocess))
+//    {
+//        embot::app::application::theSkin &theskin = embot::app::application::theSkin::getInstance();
+//        theskin.tick(outframes);
+//        
+//        // we operate on the skin triangles by calling a skin.tick(outframes);
+//        // the evSKINprocess is emitted  by:
+//        // 1. a periodic timer started at the reception of a specific message.
+// 
+// 
+//        // the .tick(outframes) will do whatever it needs to do and it may emit some 
+//        // can frames for transmission. the can frames can be up to 16x2 = 32.
+//        // hence, how many packets? max of replies = 8 + max of broadcast = 32 --> 40.
+//        
+//    }
+    
+//    if(true == embot::common::msk::check(eventmask, evIMUprocess))
+//    {
+//        
+//        embot::app::application::theIMU &theimu = embot::app::application::theIMU::getInstance();
+//        theimu.tick(outframes);
+//        // we operate on the IMU  by calling a imu.process(outframes);
+//        // the evIMUprocess is emitted  by:
+//        // 1. a periodic timer started at the reception of a specific message.
+//        // 2. internally to imu.process() if a new tick is required (the IMU is read with DMA, whose interrupt triggers a send-event
+// 
+//        // the .process(outframes) will do whatever it needs to do and it may emit some 
+//        // can frames for transmission. the can frames can be up to ?.
+//        // hence, how many packets? 40 + ? = 48.
+//        
+//    }
+    
+    // if we have any packet we transmit them
+    std::uint8_t num = outframes.size();
+    if(num > 0)
+    {
+        for(std::uint8_t i=0; i<num; i++)
+        {
+            embot::hw::can::put(embot::hw::can::Port::one, outframes[i]);                                       
+        }
+
+        embot::hw::can::transmit(embot::hw::can::Port::one);  
+    } 
+ 
+}
+
+static void userdefonidle(void* param)
+{
+    static std::uint32_t cnt = 0;
+    
+    cnt++;
+}
+
+
+///
+
+
---
+++ emBODY/eBtest/arch-arm/board/strain2/appcan/src/others/if2hw_common.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2017 iCub Facility - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef _IF2HW_COMMON_H_
+#define _IF2HW_COMMON_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+  
+
+// - external dependencies --------------------------------------------------------------------------------------------
+
+#include "stm32hal.h"
+
+
+// - public #define  --------------------------------------------------------------------------------------------------
+
+// it forces AD7147 and I2C_Multi_SDA to use U32 or U16 data. 
+// the logical choice is U16 as everywhere the code uses only 2 bytes.
+#define if2hw_common_SKIN_USE_U16
+  
+// it shapes code to use either internal or externally allocated buffers ...    
+#define if2hw_common_AD7147_USE_EXTERNALDATA
+    
+// it shapes the code to use or not the data structure for controllling config of triangles as defined in AD7147
+#define if2hw_common_AD7147_USE_EXTERNALCONTROLTYPES    
+
+
+// - declaration of public user-defined types -------------------------------------------------------------------------
+
+
+#if defined(if2hw_common_SKIN_USE_U32)
+
+typedef uint32_t if2hw_data_ad7147_t;
+typedef uint32_t if2hw_data_i2cmultisda_t;
+
+#elif defined(if2hw_common_SKIN_USE_U16)
+    
+typedef uint16_t if2hw_data_ad7147_t;
+typedef uint16_t if2hw_data_i2cmultisda_t;
+
+#else
+    
+#error YOU MUST define either if2hw_common_SKIN_USE_U32 or if2hw_common_SKIN_USE_U16
+
+#endif
+
+
+// - declaration of extern public variables, ... but better using use _get/_set instead -------------------------------
+
+
+// - declaration of extern public functions ---------------------------------------------------------------------------
+
+extern void if2hw_common_delay(uint64_t usec);
+
+ 
+
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif 
+ 
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
