5dfc9be8969ba82dc394eef97251347f7c757f32
marcoaccame <marco.accame@iit.it>
2017-10-31 17:32:22 +0100

    mtb4 bootloader gives address 13 by default

--- emBODY/eBcode/arch-arm/board/mtb4/bootloader/src/main-bootloader.cpp
+++ emBODY/eBcode/arch-arm/board/mtb4/bootloader/src/main-bootloader.cpp
@@ -39,8 +39,8 @@ static const embot::common::relTime BlinkSlowPeriod = 5*EOK_reltime100ms;
 static const embot::common::relTime BlinkMadlyPeriod = 5*EOK_reltime10ms;
 
 static const embot::app::canprotocol::versionOfBOOTLOADER vBL = {1, 0};
-static const std::uint8_t defADDRESS = 1;
-static const char defaultInfo32[] = {"I am a stm32l4"};
+static const std::uint8_t defADDRESS = 13;
+static const char defaultInfo32[] = {"I am a c++11 mtb4"};
 
 int main(void)
 { 
