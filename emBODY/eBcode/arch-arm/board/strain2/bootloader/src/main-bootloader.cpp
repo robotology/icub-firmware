db46846c5f1c72f5486c51eb9e88df317190e2f6
marcoaccame <marco.accame@iit.it>
2017-11-27 12:43:11 +0100

    strain2.bootloader is now v 1.1

--- emBODY/eBcode/arch-arm/board/strain2/bootloader/src/main-bootloader.cpp
+++ emBODY/eBcode/arch-arm/board/strain2/bootloader/src/main-bootloader.cpp
@@ -35,7 +35,7 @@ static const embot::common::relTime BlinkFastPeriod = 2*EOK_reltime100ms;
 static const embot::common::relTime BlinkSlowPeriod = 5*EOK_reltime100ms;
 static const embot::common::relTime BlinkMadlyPeriod = 5*EOK_reltime10ms;
 
-static const embot::app::canprotocol::versionOfBOOTLOADER vBL = {1, 0};
+static const embot::app::canprotocol::versionOfBOOTLOADER vBL = {1, 1};
 static const std::uint8_t defADDRESS = 1;
 static const char defaultInfo32[] = {"I am a strain2"};
 
