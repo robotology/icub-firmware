ac966ea89e93daeed22c24fbaa73392b60c739e7
marcoaccame <marco.accame@iit.it>
2018-10-16 09:13:15 +0200

    reverted strain2 and mtb4 to use stm32hal lib with driver version 1.72 details of revert: - from sha = 7e1c44e1eeb7010e75a7c859f0ee478773dbc12c   (msg = Mtb4 updaterofbootloader ver 100.2:  updated hal with stm   library version 183 - date = 03/10/2018 17:07:48) - down to sha = 53ecae35e9c15d5c2b10b0a7e716eb19685d886d   (msg = strain2 ver 1.5.0: updated hal with stm library version 183 -   date = 28/08/2018 09:31:51)

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/mtb4/src/can.c
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/mtb4/src/can.c
@@ -55,15 +55,15 @@ void MX_CAN1_Init(void)
   hcan1.Instance = CAN1;
   hcan1.Init.Prescaler = 2;
   hcan1.Init.Mode = CAN_MODE_NORMAL;
-  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_2TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_5TQ;
-  hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = ENABLE;
-  hcan1.Init.AutoWakeUp = ENABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
-  hcan1.Init.ReceiveFifoLocked = DISABLE;
-  hcan1.Init.TransmitFifoPriority = ENABLE;
+  hcan1.Init.SJW = CAN_SJW_1TQ;
+  hcan1.Init.BS1 = CAN_BS1_2TQ;
+  hcan1.Init.BS2 = CAN_BS2_5TQ;
+  hcan1.Init.TTCM = DISABLE;
+  hcan1.Init.ABOM = ENABLE;
+  hcan1.Init.AWUM = ENABLE;
+  hcan1.Init.NART = DISABLE;
+  hcan1.Init.RFLM = DISABLE;
+  hcan1.Init.TXFP = ENABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
