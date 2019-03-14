d0d4c4f25653d2710e421b24b0054294e32f2135
marcoaccame <marco.accame@iit.it>
2019-03-14 08:53:13 +0100

    strain2: adjusted the versions of btl (2.0), upd (100.2.0), app (2.0.1). now btl imposes def address = 13.

--- emBODY/eBcode/arch-arm/board/strain2/application/src/main-strain2-application.cpp
+++ emBODY/eBcode/arch-arm/board/strain2/application/src/main-strain2-application.cpp
@@ -83,7 +83,7 @@ const embot::hw::SI7051::Config SI7051config = embot::hw::bsp::strain2::thermome
 #endif // TEST_ENABLED
 
 
-static const embot::app::canprotocol::versionOfAPPLICATION vAP = {1, 5 , 4};
+static const embot::app::canprotocol::versionOfAPPLICATION vAP = {2, 0, 1};
 static const embot::app::canprotocol::versionOfCANPROTOCOL vCP = {2, 0};
 
 static void userdeflauncher(void* param);
