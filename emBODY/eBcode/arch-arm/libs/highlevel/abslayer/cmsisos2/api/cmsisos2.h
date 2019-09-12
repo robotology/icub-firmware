0798c029a40d9372e6129de0056dab2ed9941d84
marco.accame <marco.accame@iit.it>
2019-09-12 13:06:32 +0200

    added cmsis-rtos2 from the pack ARM::CMSIS version 5.6.0 of 2019-07-10 from ARM\CMSIS\5.6.0\CMSIS\RTOS2 to emBODY\eBcode\arch-arm\libs\highlevel\abslayer\cmsisos2\src\RTOS2 original commit is ... as it is distributed from ARM

---
+++ emBODY/eBcode/arch-arm/libs/highlevel/abslayer/cmsisos2/api/cmsisos2.h
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (C) 2019 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef _CMSISOS2_H_
+#define _CMSISOS2_H_
+
+#ifdef  __cplusplus
+extern "C"
+{
+#endif
+
+
+#include "cmsis_os2.h"
+#include "../RTX/Include/rtx_os.h"
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  // include-guard
+
