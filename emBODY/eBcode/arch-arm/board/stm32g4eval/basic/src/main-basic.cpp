0c7bb8901551a87659979bbf6ff235f59e448df6
marco.accame <marco.accame@iit.it>
2020-06-23 14:44:33 +0200

    stm32g4eval: added basic application

---
+++ emBODY/eBcode/arch-arm/board/stm32g4eval/basic/src/main-basic.cpp
@@ -0,0 +1,142 @@
+
+/*
+ * Copyright (C) 2020 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+#include "stm32hal.h"
+
+
+// stm32hal requires two functions: one which inits and one that gets the 1 ms time.
+static void stm32hal_tick1msecinit();
+static uint32_t stm32hal_tick1msecget();
+static const stm32hal_config_t systickcfg = { stm32hal_tick1msecinit, stm32hal_tick1msecget };
+
+// they use STM32 HAL 
+static void led_init();
+static void led_toggle();
+
+// --------------------------------------------------------------------------------------------------------------------
+
+int main(void)
+{ 
+    stm32hal_init(&systickcfg);
+    
+    led_init();
+        
+    for(;;)
+    {
+        HAL_Delay(500);
+        led_toggle();
+    }    
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+// we need to provide stm32hal a timebase in units of 1 ms (as the hal driver by stm requires). 
+// we can use what we want. typically it is used the systick timer which increments a volatile
+// counter. the systick is initted @ 1 khz 
+
+static volatile uint64_t s_1mstickcount = 0; // it must be volatile
+constexpr uint32_t s_rate1khz = 1000;
+
+void SysTick_Handler(void)
+{
+    s_1mstickcount++;
+}
+
+static void stm32hal_tick1msecinit()
+{
+    HAL_SYSTICK_Config(SystemCoreClock/s_rate1khz);
+}
+
+static uint32_t stm32hal_tick1msecget()
+{
+    return s_1mstickcount;
+}
+
+// the led section. we use PB0
+
+#if defined(STM32HAL_BOARD_STM32G4EVAL)
+
+    #warning we are using board stm32g4eval, led is on pg9
+    // led1 is on PG9
+    static GPIO_TypeDef * GPIOLED = GPIOG;
+    static const uint16_t PinLED = GPIO_PIN_9;
+    static void clockEnableLED() {  __HAL_RCC_GPIOG_CLK_ENABLE(); }
+
+#elif defined(STM32HAL_BOARD_NUCLEOH7)
+
+    #warning we are using board nucleoh7, led is on pb0
+    // led1 is on PG9
+    static GPIO_TypeDef * GPIOLED = GPIOB;
+    static const uint16_t PinLED = GPIO_PIN_0;
+    static void clockEnableLED() {  __HAL_RCC_GPIOB_CLK_ENABLE(); }
+    
+//static void led_init()
+//{
+//    // start clock
+//    __HAL_RCC_GPIOB_CLK_ENABLE();
+//    
+//    // configure pb0
+//    GPIO_InitTypeDef GPIO_InitStruct = {0};
+//    GPIO_InitStruct.Pin = GPIO_PIN_0;
+//    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//    GPIO_InitStruct.Pull = GPIO_NOPULL;
+//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+//    
+//    // switch it on
+//    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET); 
+//    
+//    // switch it off
+//    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);    
+//}
+
+//static void led_toggle()
+//{
+//    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);    
+//}
+
+// needed because we have removed that from stm32hal
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) 
+{     
+    // nothing
+}  
+
+#endif
+
+static void led_init()
+{
+    // start clock
+    clockEnableLED();
+    
+    // configure led
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = PinLED;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOLED, &GPIO_InitStruct);
+    
+    // switch it on
+    HAL_GPIO_WritePin(GPIOLED, PinLED, GPIO_PIN_SET); 
+    
+    // switch it off
+    HAL_GPIO_WritePin(GPIOLED, PinLED, GPIO_PIN_RESET);    
+}
+
+static void led_toggle()
+{
+    HAL_GPIO_TogglePin(GPIOLED, PinLED);    
+}
+      
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
