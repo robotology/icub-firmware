5e2af8a4248cb44927beb2dfac6a3be67645da17
marco.accame <marco.accame@iit.it>
2020-09-08 09:25:55 +0200

    stm32g4eval: added bootloader + can basic application

---
+++ emBODY/eBcode/arch-arm/board/stm32g4eval/examples/can-btl/src/main-evthreadcan.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+#include "embot_app_skeleton_os_bootloader.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// bootloader info
+
+constexpr std::uint8_t defADDRESS = 12;
+constexpr embot::app::theCANboardInfo::bootloaderInfo btlInfo 
+{ 
+    embot::prot::can::Board::sg3, 
+    embot::prot::can::versionOfBOOTLOADER {1, 6}, 
+    defADDRESS,                                                  
+    "I am a sg3" 
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+int main(void)
+{ 
+    embot::app::skeleton::os::bootloader::run(btlInfo);
+    for(;;);    
+}
+   
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
