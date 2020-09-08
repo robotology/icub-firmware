4fae25b840bd2461807e13efe1de05bd80f9409a
marco.accame <marco.accame@iit.it>
2020-09-08 09:25:55 +0200

    stm32g4eval: added bootloader + can basic application

---
+++ emBODY/eBcode/arch-arm/board/stm32g4eval/examples/can-app/src/main-evthreadcan.cpp
@@ -0,0 +1,174 @@
+
+/*
+ * Copyright (C) 2019 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Valentina Gaggero
+ * email:   valentina.gaggero@iit.it
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+#include "embot_app_skeleton_os_evthreadcan.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// config start
+
+constexpr embot::app::theCANboardInfo::applicationInfo applInfo 
+{ 
+    embot::prot::can::versionOfAPPLICATION {0, 0, 7},    
+    embot::prot::can::versionOfCANPROTOCOL {2, 0}    
+};
+
+constexpr std::uint16_t threadIDLEstacksize = 512;
+constexpr std::uint16_t threadINITstacksize = 2048;
+constexpr std::uint16_t threadEVNTstacksize = 4096;
+constexpr std::uint8_t maxINPcanframes = 16;
+constexpr std::uint8_t maxOUTcanframes = 48;
+constexpr embot::core::relTime threadEVNTtimeout = 50*embot::core::time1millisec;
+
+static void *paramINIT = nullptr;
+static void *paramIDLE = nullptr;
+static void *paramERR = nullptr;
+static void *paramEVNT = nullptr;
+
+constexpr embot::os::theTimerManager::Config tmcfg {};
+constexpr embot::os::theCallbackManager::Config cmcfg {};
+    
+    
+static const embot::app::skeleton::os::basic::sysConfig syscfg { threadINITstacksize, paramINIT, threadIDLEstacksize, paramIDLE, paramERR, tmcfg, cmcfg};
+
+static const embot::app::skeleton::os::evthreadcan::evtConfig evtcfg { threadEVNTstacksize, paramEVNT, threadEVNTtimeout};
+
+static const embot::app::skeleton::os::evthreadcan::canConfig cancfg { maxINPcanframes, maxOUTcanframes };
+
+// config end
+// --------------------------------------------------------------------------------------------------------------------
+
+
+class mySYS final : public embot::app::skeleton::os::evthreadcan::SYSTEMevtcan
+{
+public:
+    mySYS(const embot::app::skeleton::os::basic::sysConfig &cfg) 
+        : SYSTEMevtcan(cfg) {}
+        
+    void userdefOnIdle(embot::os::Thread *t, void* idleparam) const override;
+    void userdefonOSerror(void *errparam) const override;
+    void userdefInit_Extra(embot::os::EventThread* evtsk, void *initparam) const override;
+};
+
+
+class myEVT final : public embot::app::skeleton::os::evthreadcan::evThreadCAN
+{
+public:
+    myEVT(const embot::app::skeleton::os::evthreadcan::evtConfig& ecfg, const embot::app::skeleton::os::evthreadcan::canConfig& ccfg, const embot::app::theCANboardInfo::applicationInfo& a) 
+        : evThreadCAN(ecfg, ccfg, a) {}
+        
+    void userdefStartup(embot::os::Thread *t, void *param) const override;
+    void userdefOnTimeout(embot::os::Thread *t, embot::os::EventMask eventmask, void *param) const override;
+    void userdefOnEventRXcanframe(embot::os::Thread *t, embot::os::EventMask eventmask, void *param, const embot::prot::can::Frame &frame, std::vector<embot::prot::can::Frame> &outframes) const override;
+    void userdefOnEventANYother(embot::os::Thread *t, embot::os::EventMask eventmask, void *param, std::vector<embot::prot::can::Frame> &outframes) const override;                   
+};
+
+
+static const mySYS mysys { syscfg };
+static const myEVT myevt { evtcfg, cancfg, applInfo };
+constexpr embot::app::skeleton::os::evthreadcan::CFG cfg{ &mysys, &myevt };
+
+// --------------------------------------------------------------------------------------------------------------------
+
+int main(void)
+{ 
+    embot::app::skeleton::os::evthreadcan::run(cfg);
+    for(;;);    
+}
+
+
+// - here is the tailoring of the board: strain2, mtb4, psc, etc.
+
+
+#include "embot_os_theScheduler.h"
+#include "embot_app_theLEDmanager.h"
+#include "embot_app_application_theCANparserBasic.h"
+
+#include "embot_hw_can.h"
+
+void mySYS::userdefOnIdle(embot::os::Thread *t, void* idleparam) const
+{
+    static int a = 0;
+    a++;        
+}
+
+void mySYS::userdefonOSerror(void *errparam) const
+{
+    static int code = 0;
+    embot::os::theScheduler::getInstance().getOSerror(code);
+    for(;;);    
+}
+
+
+void mySYS::userdefInit_Extra(embot::os::EventThread* evtsk, void *initparam) const
+{
+    // inside the init thread: put the init of many things ...  
+    
+    // led manager
+    static const std::initializer_list<embot::hw::LED> allleds = {embot::hw::LED::one};  
+    embot::app::theLEDmanager &theleds = embot::app::theLEDmanager::getInstance();     
+    theleds.init(allleds);    
+    theleds.get(embot::hw::LED::one).pulse(embot::core::time1second);    
+
+    // init of can basic paser
+    embot::app::application::theCANparserBasic::getInstance().initialise({});    
+
+    // ...
+}
+
+void myEVT::userdefStartup(embot::os::Thread *t, void *param) const
+{
+    // inside startup of evnt thread: put the init of many things ... 
+
+    // ...
+}
+
+
+void myEVT::userdefOnTimeout(embot::os::Thread *t, embot::os::EventMask eventmask, void *param) const
+{
+    static uint32_t cnt = 0;
+    cnt++;  
+    uint8_t data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    embot::hw::can::Frame frame {0x210, 1, data};
+
+    embot::hw::can::put(embot::hw::CAN::one, frame);                                       
+    embot::hw::can::transmit(embot::hw::CAN::one);  
+    
+}
+
+
+void myEVT::userdefOnEventRXcanframe(embot::os::Thread *t, embot::os::EventMask eventmask, void *param, const embot::prot::can::Frame &frame, std::vector<embot::prot::can::Frame> &outframes) const
+{    
+    // process w/ the basic parser, if not recognised call the parse specific of the board
+    if(true == embot::app::application::theCANparserBasic::getInstance().process(frame, outframes))
+    {                   
+    }
+//    else if(true == otherparser.process(frame, outframes))
+//    {
+//    }
+    // ...
+}
+
+void myEVT::userdefOnEventANYother(embot::os::Thread *t, embot::os::EventMask eventmask, void *param, std::vector<embot::prot::can::Frame> &outframes) const
+{
+    constexpr embot::os::Event evXXX = 0x00000001 << 31;
+
+    if(true == embot::core::binary::mask::check(eventmask, evXXX))
+    {        
+        // do things      
+    }
+    
+    // ...    
+}
+   
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
+
