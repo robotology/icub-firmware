a45fc683074a5ce959193444c92ffacd393856cd
marco.accame <marco.accame@iit.it>
2020-11-10 17:56:17 +0100

    pmc -> added an alternative test program for reading the fap sensors

---
+++ emBODY/eBcode/arch-arm/board/pmc/bsp/embot_hw_bsp_pmc.h
@@ -0,0 +1,31 @@
+
+/*
+ * Copyright (C) 2020 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef _EMBOT_HW_BSP_PMC_H_
+#define _EMBOT_HW_BSP_PMC_H_
+
+#include "embot_core.h"
+#include "embot_hw_types.h"
+
+
+constexpr embot::hw::TLV493D fapJ4 = embot::hw::TLV493D::one;
+//constexpr embot::hw::TLV493D fapU27 = embot::hw::TLV493D::two;
+constexpr embot::hw::TLV493D fapJ5 = embot::hw::TLV493D::two;
+constexpr embot::hw::TLV493D fapJ6 = embot::hw::TLV493D::three;
+constexpr embot::hw::TLV493D fapJ7 = embot::hw::TLV493D::four;
+
+
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
