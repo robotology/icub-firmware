8c592e2062fe7d245ca198045513269389fe4111
Ines <ines.sorrentino@iit.it>
2020-12-15 14:44:13 +0100

    2foc firmware - fix compilation error. `typedef char int8_t` changed to `typedef signed char int8_t`

--- emBODY/eBcode/arch-dspic/board/2foc/appl/2FOC-V3/src/ecan.c
+++ emBODY/eBcode/arch-dspic/board/2foc/appl/2FOC-V3/src/ecan.c
@@ -824,7 +824,7 @@ void __attribute__((interrupt, no_auto_psv)) _DMA2Interrupt(void)
 
 //VALE
 
-typedef     char                    int8_t;
+typedef     signed char             int8_t;
 typedef     unsigned char           uint8_t;
 typedef     signed int              int16_t;
 typedef     unsigned int            uint16_t;
