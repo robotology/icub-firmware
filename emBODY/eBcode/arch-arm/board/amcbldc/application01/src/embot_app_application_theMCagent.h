13d5c1e1b2186d0290f8bba025d3fd4fba0ecfe9
marco.accame <marco.accame@iit.it>
2021-07-13 13:29:22 +0200

    updated agent of MC CAN protocol of board amcbldc

--- emBODY/eBcode/arch-arm/board/amcbldc/application01/src/embot_app_application_theMCagent.h
+++ emBODY/eBcode/arch-arm/board/amcbldc/application01/src/embot_app_application_theMCagent.h
@@ -33,7 +33,9 @@ namespace embot { namespace app { namespace application {
         bool initialise(const Config &config);   
                 
         // interface to CANagentMC
-        virtual bool get(const embot::prot::can::motor::periodic::Message_EMSTO2FOC_DESIRED_CURRENT::Info &info);
+        virtual bool get(const embot::prot::can::motor::periodic::Message_EMSTO2FOC_DESIRED_CURRENT::Info &info);        
+        virtual bool get(const embot::prot::can::motor::polling::Message_SET_CONTROL_MODE::Info &info);
+        virtual bool get(const embot::prot::can::motor::polling::Message_GET_CONTROL_MODE::Info &info, embot::prot::can::motor::polling::Message_GET_CONTROL_MODE::ReplyInfo &replyinfo);
         
     private:
         theMCagent(); 
