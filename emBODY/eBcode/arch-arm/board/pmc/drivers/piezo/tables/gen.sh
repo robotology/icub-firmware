1ccf9e10a54c7b6fe271ee35dffcd8480ebee7e3
Andrea Merello <andrea.merello@gmail.com>
2021-10-08 10:47:40 +0200

    PMC driver: update wave tables - shrink in range 2V to 44V

--- emBODY/eBcode/arch-arm/board/pmc/drivers/piezo/tables/gen.sh
+++ emBODY/eBcode/arch-arm/board/pmc/drivers/piezo/tables/gen.sh
@@ -2,6 +2,6 @@
 mkdir -p generated
 cd generated
 
-../faulhaber_csv_to_c.py ../srcdata/delta.csv 3 8192 65407 128 delta_8192
-../faulhaber_csv_to_c.py ../srcdata/delta.csv 3 1024 65407 128 delta_1024
-../faulhaber_csv_to_c.py ../srcdata/rhomb.csv 7 8192 65407 128 rhomb_8192
+../faulhaber_csv_to_c.py ../srcdata/delta.csv 3 8192 60707 2760 delta_8192
+../faulhaber_csv_to_c.py ../srcdata/delta.csv 3 1024 60707 2760 delta_1024
+../faulhaber_csv_to_c.py ../srcdata/rhomb.csv 7 8192 60707 2760 rhomb_8192
