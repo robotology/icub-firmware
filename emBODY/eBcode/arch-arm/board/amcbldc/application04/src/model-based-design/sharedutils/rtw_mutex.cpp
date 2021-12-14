f7ed4a56329956b587ba98c5a3ef1113bf515554
Simone Girardi <simone.girardi@iit.it>
2021-12-14 10:57:06 +0100

    AMC_BLDC: Add rtw_mutex sources

---
+++ emBODY/eBcode/arch-arm/board/amcbldc/application04/src/model-based-design/sharedutils/rtw_mutex.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2021 iCub Tech Facility - Istituto Italiano di Tecnologia
+ * Author:  Ugo Pattacini
+ * email:   ugo.pattacini@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include "rtw_mutex.h"
+
+void rtw_mutex_lock(void){}
+void rtw_mutex_unlock(void){}
