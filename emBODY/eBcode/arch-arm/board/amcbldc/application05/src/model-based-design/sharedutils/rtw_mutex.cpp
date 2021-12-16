124bd64dd5c43d6cb06b2c8523a039fddc134737
marco.accame <marco.accame@iit.it>
2021-12-16 11:41:28 +0100

    amcbldc: added mutual exclusion between a user thread and DMA1_Channel2_IRQHandler()

--- emBODY/eBcode/arch-arm/board/amcbldc/application05/src/model-based-design/sharedutils/rtw_mutex.cpp
+++ emBODY/eBcode/arch-arm/board/amcbldc/application05/src/model-based-design/sharedutils/rtw_mutex.cpp
@@ -17,10 +17,69 @@
 
 #include "rtw_mutex.h"
 
-void rtw_mutex_lock(void) {
 
+// marco.accame: this code provides protection vs concurrent access between
+// a user thread and the DMA IRQHandler for DMA1 Channel2. the mechanism
+// is that the user-thread disables the interrupt to lock and reanables it
+// to unlock. the IRQHandler must not do anything, so if we are in an 
+// exception (irq handler) we dont lock / unlock. 
+// you can use macro TEST_CALLING to verify the correct mechanism.
+
+#include "stm32hal.h"
+
+static bool IsException(void); 
+
+//#define TEST_CALLING
+
+
+#if defined(TEST_CALLING)
+volatile int32_t test_numThreadMode {0};
+volatile int32_t test_numHandlerMode {0};
+#endif
+
+void rtw_mutex_lock(void) 
+{    
+    if(false == IsException())
+    {
+        NVIC_DisableIRQ(DMA1_Channel2_IRQn);  
+#if defined(TEST_CALLING)        
+        test_numThreadMode++;
+#endif                
+    }  
+#if defined(TEST_CALLING)    
+    else
+    {
+        test_numHandlerMode++;
+    }
+#endif
 }
 
-void rtw_mutex_unlock(void) {
-    
+void rtw_mutex_unlock(void) 
+{
+    if(false == IsException())
+    { 
+#if defined(TEST_CALLING)        
+        test_numThreadMode--;    
+#endif        
+        NVIC_EnableIRQ(DMA1_Channel2_IRQn);
+    }
+#if defined(TEST_CALLING)    
+    else
+    {
+        test_numHandlerMode--;
+    }  
+#endif    
 }
+
+
+// static funtions
+static bool IsException(void) 
+{
+    return(__get_IPSR() != 0U);
+}
+
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
