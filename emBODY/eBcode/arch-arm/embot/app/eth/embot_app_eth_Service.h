eac33adb96fdc1c0aaf4e894bdd89f33b3be465b
marco.accame <marco.accame@iit.it>
2022-06-10 12:31:00 +0200

    added missing files for amc-appl-00

---
+++ emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_Service.h
@@ -0,0 +1,100 @@
+
+
+/*
+ * Copyright (C) 2022 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef __EMBOT_APP_ETH_SERVICE_H_
+#define __EMBOT_APP_ETH_SERVICE_H_
+
+#include "embot_core.h"
+#include "embot_core_binary.h"
+
+#include "EoCommon.h"
+#include "EOnv.h"
+#include "EOrop.h"
+#include "EoManagement.h"
+
+
+#include <vector>
+#include <memory>
+
+
+namespace embot { namespace app { namespace eth {
+    
+    typedef eOresult_t (*eOservice_onendofoperation_fun_t) (void* p, eObool_t operationisok);
+    
+    struct DescriptorCANframe
+    {
+        eOcanport_t port {eOcanport1};
+        eOcanframe_t *frame {nullptr};
+        uint32_t extra {0};       
+        constexpr DescriptorCANframe() = default;            
+        constexpr DescriptorCANframe(eOcanport_t p, eOcanframe_t *f, uint32_t e) : port(p), frame(f), extra(e) {};
+    };
+      
+    struct DescriptorROP
+    {
+        eOropdescriptor_t *rd {nullptr};
+        EOnv *nv {nullptr};
+        uint32_t extra {0};       
+        constexpr DescriptorROP() = default;            
+        constexpr DescriptorROP(eOropdescriptor_t *r, EOnv *n, uint32_t e) : rd(r), nv(n), extra(e) {};
+    };
+
+
+    class Service
+    {
+        // pure interface
+        public:
+        
+        using fpOnEndOfOperation = bool (*)(void *caller, Service *s, eOmn_serv_configuration_t *sc, bool operationisok);
+        
+        enum class Category : uint8_t { // the same values as eOmn_serv_category_t
+            mc = 0, ft = 9, test = 200, none = 255 
+        };
+        
+        enum class State : uint8_t { // the same values of eOmn_serv_state_t 
+            notsupported = 0, idle = 1, verifying = 2, verified = 3, activated = 4, 
+            failureofverify = 5, started = 6, fatalerror = 7
+        };
+        
+        
+        virtual Category category() const = 0;
+        
+        virtual State state() const = 0;
+        //virtual bool Verify(const eOmn_serv_configuration_t * servcfg, eOservice_onendofoperation_fun_t onverify, bool activateafterverify) = 0;
+        virtual bool verify(const eOmn_serv_configuration_t * servcfg, bool activateafterverify, fpOnEndOfOperation onendoperation, void *caller) = 0;
+        virtual bool activate(const eOmn_serv_configuration_t * servcfg) = 0;
+        virtual bool deactivate() = 0; //const eOmn_serv_configuration_t * servcfg) = 0;
+        virtual bool start() = 0;
+        virtual bool stop() = 0;
+        virtual bool setregulars(eOmn_serv_arrayof_id32_t* arrayofid32, uint8_t& numberofthem) = 0;
+        virtual bool tick(bool resetstatus = false) = 0;
+//        virtual bool AcceptCANframe(const canFrameDescriptor &canframedescriptor) = 0;
+        virtual bool report() = 0;   
+        
+        virtual bool process(const DescriptorCANframe &canframedescriptor) = 0;
+        virtual bool process(const DescriptorROP &ropdescriptor) = 0;
+                
+    protected:
+        virtual ~Service() {};    // cannot delete from interface but only from derived object
+ 
+    }; 
+    
+    // maybe put it inside theServices
+    
+    Service * get(Service::Category category);
+
+
+}}} // namespace embot { namespace app { namespace eth
+
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
---
+++ emBODY/eBcode/arch-arm/embot/hw/hal_embot.cpp
@@ -0,0 +1,216 @@
+
+/*
+ * Copyright (C) 2022 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "hal.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - implementation of selected functions using embot::hw 
+// --------------------------------------------------------------------------------------------------------------------
+
+// --- basic
+
+#include "embot_hw_types.h"
+
+constexpr hal_result_t halresult(embot::hw::result_t r)
+{ 
+    return (embot::hw::resOK == r) ? hal_res_OK : hal_res_NOK_generic;
+}
+
+constexpr hal_boolval_t halbool(bool b)
+{ 
+    return (b) ? hal_true : hal_false;
+}
+
+// --- hal_can
+
+#include "embot_hw_can.h"
+
+//constexpr hal_can_t convert(embot::hw::CAN id)
+//{ 
+//    if(embot::core::tointegral(id) >= embot::core::tointegral(embot::hw::CAN::maxnumberof))
+//    {
+//        return hal_can1;
+//    }
+//    return static_cast<hal_can_t>(id); 
+//};
+
+constexpr embot::hw::CAN convert(hal_can_t id)
+{ 
+    return static_cast<embot::hw::CAN>(id); 
+};
+
+extern hal_boolval_t hal_can_supported_is(hal_can_t id)
+{
+    return halbool(embot::hw::can::supported(convert(id)));
+}
+
+//extern hal_boolval_t hal_can_initted_is(hal_can_t id);
+
+extern hal_result_t hal_can_init(hal_can_t id, const hal_can_cfg_t *cfg)
+{
+    if(nullptr == cfg)
+    {
+        return hal_res_NOK_generic;
+    }
+      
+    embot::hw::can::Config config 
+    { 
+        cfg->capacityoftxfifoofframes, cfg->capacityofrxfifoofframes,
+        {cfg->callback_on_tx, cfg->arg_cb_tx}, 
+        {},
+        {cfg->callback_on_rx, cfg->arg_cb_rx}    
+    };
+    
+    return halresult(embot::hw::can::init(convert(id), config));
+}
+
+extern hal_result_t hal_can_enable(hal_can_t id)
+{
+    return halresult(embot::hw::can::enable(convert(id)));
+}
+
+
+//extern hal_result_t hal_can_disable(hal_can_t id);
+
+extern hal_result_t hal_can_put(hal_can_t id, hal_can_frame_t *frame, hal_can_send_mode_t sm)
+{
+    if(nullptr == frame)
+    {
+        return hal_res_NOK_generic;
+    }
+    
+    embot::hw::can::Frame fr
+    {
+        frame->id, frame->size, frame->data
+    };
+    
+    if(embot::hw::resOK != embot::hw::can::put(convert(id), fr))
+    {
+        return hal_res_NOK_generic;
+    }        
+ 
+    if(hal_can_send_normprio_now == sm)
+    {
+        return hal_can_transmit(id);
+    }
+    
+    return hal_res_OK;
+}
+
+
+extern hal_result_t hal_can_transmit(hal_can_t id)
+{
+    return halresult(embot::hw::can::transmit(convert(id)));   
+}
+
+
+extern hal_result_t hal_can_received(hal_can_t id, uint8_t *numberof)
+{
+    if(nullptr == numberof)
+    {
+        return hal_res_NOK_generic;
+    }
+    *numberof = embot::hw::can::outputqueuesize(convert(id));
+    return hal_res_OK;
+}
+
+
+
+extern hal_result_t hal_can_get(hal_can_t id, hal_can_frame_t *frame, uint8_t *remaining)
+{
+    if(nullptr == frame)
+    {
+        return hal_res_NOK_generic;
+    } 
+
+    embot::hw::can::Frame fr{};
+    uint8_t rem {0};
+    if(embot::hw::resNOK == embot::hw::can::get(convert(id), fr, rem))
+    {
+        return hal_res_NOK_generic;
+    }
+    
+    // copy fr
+    frame->id = fr.id;
+    frame->id_type = hal_can_frameID_std;
+    frame->frame_type = hal_can_frame_data;
+    frame->size = fr.size;
+    std::memmove(frame->data, frame->data, sizeof(frame->data));
+    if(nullptr != remaining)
+    {
+        *remaining = rem;
+    }
+    
+    return hal_res_OK;
+}
+
+
+extern hal_result_t hal_can_out_get(hal_can_t id, uint8_t *numberof)
+{
+    if(nullptr == numberof)
+    {
+        return hal_res_NOK_generic;
+    }
+    
+    *numberof = embot::hw::can::outputqueuesize(convert(id));
+    
+    return hal_res_OK;
+}
+
+//extern hal_result_t hal_can_getstatus(hal_can_t id, hal_can_status_t *status);
+
+
+// --- hal_timer
+
+
+#include "embot_hw_timer.h"
+
+#warning TODO implement and use embot::hw::timer
+    
+embot::hw::TIMER convert(hal_timer_t id)
+{
+    if(hal_timerNONE == id)
+    {
+        return embot::hw::TIMER::none;
+    }
+    return static_cast<embot::hw::TIMER>(id);
+}
+
+hal_timer_status_t convert(embot::hw::timer::Status s)
+{
+    return static_cast<hal_timer_status_t>(s);
+}
+    
+extern hal_result_t hal_timer_init(hal_timer_t id, const hal_timer_cfg_t *cfg, hal_reltime_t *error)
+{
+    embot::hw::timer::Config config {cfg->countdown, static_cast<embot::hw::timer::Mode>(cfg->mode), {cfg->callback_on_exp, cfg->arg}};
+    return halresult(embot::hw::timer::init(convert(id), config));
+}
+
+extern hal_result_t hal_timer_start(hal_timer_t id)
+{
+    return halresult(embot::hw::timer::start(convert(id)));
+}
+
+extern hal_result_t hal_timer_stop(hal_timer_t id)
+{
+    return halresult(embot::hw::timer::stop(convert(id)));
+}
+
+extern hal_timer_status_t hal_timer_status_get(hal_timer_t id)
+{
+    embot::hw::timer::Status s = embot::hw::timer::status(convert(id));
+    return convert(s);
+}
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
