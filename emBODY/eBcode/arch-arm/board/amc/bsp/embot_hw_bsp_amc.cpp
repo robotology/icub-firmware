e39ab51d1f7e91eaee8366264c4200c9e5339e20
Simone Girardi <simone.girardi@iit.it>
2022-06-22 13:17:10 +0200

    AMC: Add embot::hw:timer to embot_hw_bsp_amc.cpp

--- emBODY/eBcode/arch-arm/board/amc/bsp/embot_hw_bsp_amc.cpp
+++ emBODY/eBcode/arch-arm/board/amc/bsp/embot_hw_bsp_amc.cpp
@@ -1377,6 +1377,177 @@ namespace embot { namespace hw { namespace timer {
     }    
 }}}
 
+#else
+
+namespace embot { namespace hw { namespace timer {
+    #if   defined(STM32HAL_BOARD_AMC) 
+    // sadly we cannot use constexpr because of the reinterpret_cast<> inside TIM6 etc.
+    TIM_HandleTypeDef htim13;
+    static const PROP tim13p = { .TIMx = TIM13,  .handle = &htim13,  .clock = embot::hw::CLOCK::none, .speed = 200 * 1000000, .isonepulse = false, .mastermode = true };
+        
+    TIM_HandleTypeDef htim15;
+    static const PROP tim15p = { .TIMx = TIM15,  .handle = &htim15,  .clock = embot::hw::CLOCK::none, .speed = 200 * 1000000, .isonepulse = false, .mastermode = true };
+        
+    TIM_HandleTypeDef htim16;
+    static const PROP tim16p = { .TIMx = TIM16,  .handle = &htim16,  .clock = embot::hw::CLOCK::none, .speed = 200 * 1000000, .isonepulse = false, .mastermode = true };
+        
+    constexpr BSP thebsp {        
+        // maskofsupported
+        mask::pos2mask<uint32_t>(TIMER::thirteen) | mask::pos2mask<uint32_t>(TIMER::fifteen) | mask::pos2mask<uint32_t>(TIMER::sixteen),        
+        // properties
+        {{
+            nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,     // from 1 to 8
+            nullptr, nullptr, nullptr, nullptr, &tim13p, nullptr, &tim15p, &tim16p      // from 9 to 16            
+        }}        
+    };
+    
+    void BSP::init(embot::hw::TIMER h) const {}    
+    #else
+        #error embot::hw::timer::thebsp must be defined    
+    #endif
+        
+    const BSP& getBSP() 
+    {
+        return thebsp;
+    }    
+}}}
+
+// in here it is implemented in the way the good old hal2 was doing: the handler directly manages the callback
+// instead the stm hal make a lot of calls before actually calling the callback code, hence it is slower.
+
+#include "embot_hw_timer.h"
+
+void manageInterrupt(embot::hw::TIMER t, TIM_HandleTypeDef *htim)
+{
+    if(__HAL_TIM_GET_FLAG(htim, TIM_FLAG_UPDATE) != RESET)
+    {
+        if(__HAL_TIM_GET_IT_SOURCE(htim, TIM_IT_UPDATE) !=RESET)
+        {
+            __HAL_TIM_CLEAR_IT(htim, TIM_IT_UPDATE);
+            embot::hw::timer::execute(t);
+        }
+    }   
+} 
+
+
+extern "C" {
+    void TIM8_UP_TIM13_IRQHandler(void)
+    {
+        #warning TODO: cambiare il modo in cui si chiama la callback. usare le callback di stm32
+        manageInterrupt(embot::hw::TIMER::thirteen, &embot::hw::timer::htim13);
+    }
+    
+    void TIM15_IRQHandler(void)
+    {
+        manageInterrupt(embot::hw::TIMER::fifteen, &embot::hw::timer::htim15);
+    }
+    
+    void TIM16_IRQHandler(void)
+    {
+        manageInterrupt(embot::hw::TIMER::sixteen, &embot::hw::timer::htim16);
+    }
+}
+
+
+extern "C"
+{
+    void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
+    {
+      if(tim_baseHandle->Instance==TIM13)
+      {
+      /* USER CODE BEGIN TIM13_MspInit 0 */
+
+      /* USER CODE END TIM13_MspInit 0 */
+        /* TIM13 clock enable */
+        __HAL_RCC_TIM13_CLK_ENABLE();
+
+        /* TIM13 interrupt Init */
+        HAL_NVIC_SetPriority(TIM8_UP_TIM13_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(TIM8_UP_TIM13_IRQn);
+      /* USER CODE BEGIN TIM13_MspInit 1 */
+
+      /* USER CODE END TIM13_MspInit 1 */
+      }
+      else if(tim_baseHandle->Instance==TIM15)
+      {
+      /* USER CODE BEGIN TIM15_MspInit 0 */
+
+      /* USER CODE END TIM15_MspInit 0 */
+        /* TIM15 clock enable */
+        __HAL_RCC_TIM15_CLK_ENABLE();
+
+        /* TIM15 interrupt Init */
+        HAL_NVIC_SetPriority(TIM15_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(TIM15_IRQn);
+      /* USER CODE BEGIN TIM15_MspInit 1 */
+
+      /* USER CODE END TIM15_MspInit 1 */
+      }
+      else if(tim_baseHandle->Instance==TIM16)
+      {
+      /* USER CODE BEGIN TIM16_MspInit 0 */
+
+      /* USER CODE END TIM16_MspInit 0 */
+        /* TIM15 clock enable */
+        __HAL_RCC_TIM16_CLK_ENABLE();
+
+        /* TIM16 interrupt Init */
+        HAL_NVIC_SetPriority(TIM16_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(TIM16_IRQn);
+      /* USER CODE BEGIN TIM15_MspInit 1 */
+
+      /* USER CODE END TIM15_MspInit 1 */
+      }
+    }
+
+    void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
+    {
+
+      if(tim_baseHandle->Instance==TIM13)
+      {
+      /* USER CODE BEGIN TIM13_MspDeInit 0 */
+
+      /* USER CODE END TIM13_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM13_CLK_DISABLE();
+
+        /* TIM13 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(TIM8_UP_TIM13_IRQn);
+      /* USER CODE BEGIN TIM13_MspDeInit 1 */
+
+      /* USER CODE END TIM13_MspDeInit 1 */
+      }
+      else if(tim_baseHandle->Instance==TIM15)
+      {
+      /* USER CODE BEGIN TIM15_MspDeInit 0 */
+
+      /* USER CODE END TIM15_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM15_CLK_DISABLE();
+
+        /* TIM15 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(TIM15_IRQn);
+      /* USER CODE BEGIN TIM15_MspDeInit 1 */
+
+      /* USER CODE END TIM15_MspDeInit 1 */
+      }
+      else if(tim_baseHandle->Instance==TIM16)
+      {
+      /* USER CODE BEGIN TIM16_MspDeInit 0 */
+
+      /* USER CODE END TIM16_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM16_CLK_DISABLE();
+
+        /* TIM16 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(TIM16_IRQn);
+      /* USER CODE BEGIN TIM16_MspDeInit 1 */
+
+      /* USER CODE END TIM16_MspDeInit 1 */
+      }
+    }
+}
+
 #endif // timer
 
 // - end-of-file (leave a blank line after)----------------------------------------------------------------------------
