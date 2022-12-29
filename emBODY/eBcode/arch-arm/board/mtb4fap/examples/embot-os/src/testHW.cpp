6332e2c8b6d8473b93839b32d64cf484685e27bc
Marco Accame <marco.accame@iit.it>
2022-12-29 15:52:35 +0100

    Emulated I2C driver for the `mtb4fap` board (#330)

--- emBODY/eBcode/arch-arm/board/mtb4fap/examples/embot-os/src/testHW.cpp
+++ emBODY/eBcode/arch-arm/board/mtb4fap/examples/embot-os/src/testHW.cpp
@@ -18,6 +18,10 @@
 #include "embot_hw_bsp.h"
 #include "embot_hw_led.h"
 #include "embot_hw_sys.h"
+#include "embot_hw_timer.h"
+
+#include "embot_hw_i2ce.h"
+
 
 
 //#define TEST_CAN_ENABLED
@@ -25,6 +29,8 @@
 //#define TEST_TLV_EMULATED
 
 #define TEST_I2C_EMULATED
+
+#define TEST_I2C_EMULATED_embot
 
 #if defined(TEST_CAN_ENABLED)
 #include "embot_hw_can.h"
@@ -39,6 +45,13 @@ void tlv_tick();
 #endif
 
 #if defined(TEST_I2C_EMULATED)
+
+#if defined(TEST_I2C_EMULATED_embot)
+
+// just
+#include "embot_hw_i2ce.h"
+
+#else
 
 constexpr embot::hw::GPIO scl = {embot::hw::GPIO::PORT::A, embot::hw::GPIO::PIN::four};     // scl
 constexpr embot::hw::GPIO sda1 = {embot::hw::GPIO::PORT::A, embot::hw::GPIO::PIN::eight};   // sda1
@@ -62,15 +75,54 @@ void i2c_sda_output_off(embot::hw::GPIO sda);
 void i2c_send_ACK(embot::hw::GPIO sda);
 void i2c_send_NACK(embot::hw::GPIO sda);
 
-void i2c_scl_delay();
+volatile bool go {false};
+void lock()
+{
+    go = false;
+}
+void release(void *p)
+{
+    go = true;
+}
+void hwtimerwait()
+{
+    go = false;
+    embot::hw::timer::start(embot::hw::TIMER::six);
+    for(;;)
+    {
+        if(go) break;
+    }
+}
+
+    
+
+void i2c_scl_delay(void)
+{
+//    constexpr embot::core::Time semiperiod = 5*embot::core::time1microsec;
+//    constexpr embot::core::Time semiperiod = 1*embot::core::time1microsec;
+    constexpr embot::core::Time semiperiod = 5;
+    embot::hw::sys::delay(semiperiod);
+//    embot::hw::sys::onemicro();
+    
+//    hwtimerwait();
+    
+}
 
 embot::hw::gpio::State i2c_sda_status(embot::hw::GPIO sda);
 embot::hw::gpio::State i2c_read_ACK(embot::hw::GPIO sda);
 
-void i2c_write_byte(unsigned char data);
+bool i2c_write_byte(unsigned char data);
 unsigned char i2c_read_byte(void);
+
+bool I2C_init();
+bool I2C_ping(uint8_t adr);
+bool I2C_read(uint8_t adr, size_t n, uint8_t *data);
+bool I2C_write(uint8_t adr, size_t n, uint8_t *data);
+
+#endif
 	
 #endif  
+
 
 void testHWinit(embot::os::Thread *owner)
 {
@@ -83,8 +135,159 @@ void testHWinit(embot::os::Thread *owner)
 #endif    
 
 #if defined(TEST_I2C_EMULATED)  
-		i2c_gpio_init(sda1);	
+ 
+
+#if defined(TEST_I2C_EMULATED_embot)
+
+    embot::hw::i2ce::init(embot::hw::I2CE::one, {});
+
+
+#else    
+    
+    embot::hw::timer::Config tc 
+    {
+        2*embot::core::time1microsec,
+        embot::hw::timer::Mode::oneshot,
+        {release, nullptr}
+    };
+    embot::hw::timer::init(embot::hw::TIMER::six, tc);
+    embot::hw::sys::smalldelayinit();
+    I2C_init();
+	
+#endif
+
+
 #endif  	
+}
+
+uint8_t data2reset[1] = {0};
+void testI2Creset()
+{
+#if defined(TEST_I2C_EMULATED_embot)
+
+    // to be done
+    
+#else    
+    static volatile uint32_t n {0};
+    volatile bool ok {false};
+    ok = I2C_write(0xbc, sizeof(data2reset), data2reset);
+    
+    if(ok)
+    {
+        n++;
+    }
+    
+#endif    
+       
+    embot::hw::sys::delay(200*1000);
+    
+}
+
+void testI2Cping()
+{
+    static volatile embot::core::Time t {0};
+    static volatile embot::core::Time duration {0};
+    
+    volatile bool found1 = false;
+    volatile bool found2 = false;
+    static volatile uint32_t hereitis = 0;
+    static volatile uint32_t unluckyyou = 0;
+    t = embot::core::now();
+#if defined(TEST_I2C_EMULATED_embot)
+    found1 = embot::hw::i2ce::ping(embot::hw::I2CE::one, 0xbc, 3*embot::core::time1millisec);
+#else    
+    found1 = I2C_ping(0xbc);
+#endif    
+    duration = embot::core::now() - t;
+#if defined(TEST_I2C_EMULATED_embot)
+    found2 = embot::hw::i2ce::ping(embot::hw::I2CE::one, 0x3e, 3*embot::core::time1millisec);
+#else     
+    found2 = I2C_ping(0x3e);
+#endif
+    
+    if(found1 || found2)
+    {
+        hereitis++;
+    }
+    else
+    {
+        unluckyyou++;
+    }
+}
+
+void testI2Cdiscover()
+{
+#if defined(TEST_I2C_EMULATED_embot)
+    static volatile uint32_t therearesome = 0;
+    static volatile uint32_t unluckyyou = 0;
+    std::vector<embot::hw::i2ce::ADR> adrs {};
+    volatile bool foundany = embot::hw::i2ce::discover(embot::hw::I2CE::one, adrs);
+    volatile size_t ss = adrs.size();
+    if(foundany)
+    {
+        therearesome = ss;
+        volatile embot::hw::i2ce::ADR a {0};
+        for(size_t i=0; i<ss; i++)
+        {
+            a = adrs[i];
+            a = a;
+        }
+    }
+    else
+    {
+        unluckyyou++;
+    }
+#endif   
+}
+
+uint8_t data2read[10] = {0};
+void testI2Cread()
+{
+#if defined(TEST_I2C_EMULATED_embot)
+    static volatile embot::core::Time t {0};
+    static volatile embot::core::Time duration {0};
+    
+    volatile embot::hw::result_t r {embot::hw::resNOK};
+    volatile uint8_t nn {0};
+    memset(data2read, 0, sizeof(data2read));
+    embot::core::Data da {data2read, sizeof(data2read)};
+    t = embot::core::now();
+    r = embot::hw::i2ce::receive(embot::hw::I2CE::one, 0xbc, da, 5*embot::core::time1millisec);
+    duration = embot::core::now() - t;
+    if(embot::hw::resNOK == r)
+    {
+        nn++;
+    }
+#else    
+    static volatile uint32_t n {0};
+    volatile bool ok {false};
+    memset(data2read, 0, sizeof(data2read));
+    ok = I2C_read(0xbc, sizeof(data2read), data2read);
+    
+    if(ok)
+    {
+        n++;
+    }
+#endif    
+}
+
+uint8_t data2write[4] = {0};
+void testI2Cwrite()
+{
+#if defined(TEST_I2C_EMULATED_embot)
+
+    // to be done
+#else    
+    static volatile uint32_t n {0};
+    volatile bool ok {false};
+    memmove(data2write, data2read, sizeof(data2write));
+    ok = I2C_write(0xbc, sizeof(data2write), data2write);
+    
+    if(ok)
+    {
+        n++;
+    }
+#endif    
 }
 
 void testHWtick()
@@ -93,8 +296,23 @@ void testHWtick()
     tlv_tick();  
 #endif
 
-	#if defined(TEST_I2C_EMULATED)
-		i2c_gpio_tick();  
+#if defined(TEST_I2C_EMULATED)
+    
+    
+#if defined(TEST_I2C_EMULATED_embot)
+
+    testI2Cping();
+    testI2Cdiscover();
+    testI2Cread();
+    
+#else    
+
+ //   testI2Creset();
+    testI2Cping();
+    testI2Cread();
+    testI2Cwrite();
+#endif        
+    
 #endif  
 }
 
@@ -258,83 +476,147 @@ the new emulated driver for i2c needs:
 
 #if defined(TEST_I2C_EMULATED)
 
+
+#if defined(TEST_I2C_EMULATED_embot)
+
+    // we use the code moved to embot::hw::i2c2
+
+#else
+
+//#define DONTUSE_EMBOT_HW_GPIO
+#define DONTDEINIT
+
 /* emulated i2c basic functions
    ref. https://www.nxp.com/docs/en/application-note/AN12841.pdf */
 
 static constexpr embot::hw::gpio::Config out 
 { 
-	  embot::hw::gpio::Mode::OUTPUTpushpull, 
+    embot::hw::gpio::Mode::OUTPUTpushpull, 
   	embot::hw::gpio::Pull::pullup, 
   	embot::hw::gpio::Speed::veryhigh 
 };
 
 static constexpr embot::hw::gpio::Config in 
 { 
-		embot::hw::gpio::Mode::INPUT, 
-		embot::hw::gpio::Pull::pullup, 
+    embot::hw::gpio::Mode::INPUT, 
+    embot::hw::gpio::Pull::pullup, 
   	embot::hw::gpio::Speed::veryhigh 
 };    
 
+GPIO_InitTypeDef GPIO_sda_inp = {0};
+GPIO_InitTypeDef GPIO_sda_out = {0};
 void i2c_gpio_init(embot::hw::GPIO sda) 
 {
+    GPIO_sda_inp.Pin = GPIO_PIN_8;
+    GPIO_sda_inp.Mode = GPIO_MODE_INPUT;
+    GPIO_sda_inp.Pull = GPIO_PULLUP;
+    GPIO_sda_inp.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    
+    GPIO_sda_out.Pin = GPIO_PIN_8;
+    GPIO_sda_out.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_sda_out.Pull = GPIO_PULLUP;
+    GPIO_sda_out.Speed = GPIO_SPEED_FREQ_VERY_HIGH;  
+    
 	embot::hw::gpio::init(scl, out);
+    
 	i2c_sda_output(sda);
 }
 
+
+
 void i2c_sda_input(embot::hw::GPIO sda) 
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_Init(GPIOA, &GPIO_sda_inp);
+    #else 
 	embot::hw::gpio::init(sda, in);
+    #endif
 }
 
 void i2c_sda_output(embot::hw::GPIO sda) 
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_Init(GPIOA, &GPIO_sda_out);
+//    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);		
+//    LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);            
+
+    #else    
 	embot::hw::gpio::init(sda, out);
+    #endif
 }
 
 void i2c_sda_input_off(embot::hw::GPIO sda) 
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8);
+    #else 
+    #if defined(DONTDEINIT)
+    #else    
 	embot::hw::gpio::deinit(sda);
+    #endif
+    #endif
 }
 
 void i2c_sda_output_off(embot::hw::GPIO sda) 
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8);
+    #else 
+    #if defined(DONTDEINIT)
+    #else      
 	embot::hw::gpio::deinit(sda);
+    #endif
+    #endif
 }
 
 embot::hw::gpio::State i2c_sda_status(embot::hw::GPIO sda) 
 {
-	embot::hw::gpio::State s;
-	
-	s = embot::hw::gpio::get(sda);
-	
-	return s;
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    GPIO_PinState s = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8);
+    return static_cast<embot::hw::gpio::State>(s);
+    #else	
+	return embot::hw::gpio::get(sda);
+	#endif
+
 }
 
 void i2c_scl_low(void)
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+    #else
 	embot::hw::gpio::set(scl, embot::hw::gpio::State::RESET);
+    #endif
 }
 
 void i2c_scl_high(void)
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+    #else
 	embot::hw::gpio::set(scl, embot::hw::gpio::State::SET);
+    #endif
 }
 
 void i2c_sda_low(embot::hw::GPIO sda)
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+    #else    
 	embot::hw::gpio::set(sda, embot::hw::gpio::State::RESET);
+    #endif
 }
 
 void i2c_sda_high(embot::hw::GPIO sda)
 {
+    #if defined(DONTUSE_EMBOT_HW_GPIO)
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+    #else
 	embot::hw::gpio::set(sda, embot::hw::gpio::State::SET);
+    #endif
 }
 
-void i2c_scl_delay(void)
-{
-  /* ref. https://www.nxp.com/docs/en/application-note/AN12841.pdf 3.3.2 */
-  embot::core::wait(5.7*embot::core::time1microsec);
-}
+
 
 /* START condition */
 void i2c_start(embot::hw::GPIO sda)
@@ -361,29 +643,29 @@ void i2c_stop(embot::hw::GPIO sda)
 /* master reads ACK signal */
 embot::hw::gpio::State i2c_read_ACK(embot::hw::GPIO sda)
 {
-	embot::hw::gpio::State  ack;
- /* the 9th clock start */
- i2c_scl_low();
+    embot::hw::gpio::State  ack;
+    /* the 9th clock start */
+    i2c_scl_low();
 
- /* disable PTB6(SDA) output, enable PTB6(SDA) input */
- i2c_sda_output_off(sda);
- i2c_sda_input(sda);
+    /* disable PTB6(SDA) output, enable PTB6(SDA) input */
+    i2c_sda_output_off(sda);
+    i2c_sda_input(sda);
 
- i2c_scl_delay();
- i2c_scl_high();
- /* read ACK signal when SCL is high */
- ack = i2c_sda_status(sda);
- i2c_scl_delay();
- /* the 9th clock end */
+    i2c_scl_delay();
+    i2c_scl_high();
+    /* read ACK signal when SCL is high */
+    ack = i2c_sda_status(sda);
+    i2c_scl_delay();
+    /* the 9th clock end */
 
- /* disable SDA input, enable SDA output */
- i2c_sda_input_off(sda);
- i2c_sda_output(sda);
+    /* disable SDA input, enable SDA output */
+    i2c_sda_input_off(sda);
+    i2c_sda_output(sda);
 
- /* SCL hold low to wait */
- i2c_scl_low();
- i2c_sda_high(sda);
- return ack;
+    /* SCL hold low to wait */
+    i2c_scl_low();
+    i2c_sda_high(sda);
+    return ack;
 }
 
 /* master sends NACK to slave */
@@ -403,39 +685,43 @@ void i2c_send_NACK(void)
 }
 
 /* master writes a byte to salve and check ACK signal */
-void i2c_write_byte(unsigned char data)
+bool i2c_write_byte(unsigned char data)
 {
- unsigned char i;
- /* write 8 bits data */
- for(i=0;i<8;i++)
-{
- i2c_scl_low();
- i2c_scl_delay();
+    bool ret = true;
+    unsigned char i;
+    /* write 8 bits data */
+    for(i=0;i<8;i++)
+    {
+        i2c_scl_low();
+        i2c_scl_delay();
 
- /* Data can be changed only while SCL is low */
- if(data & 0x80)
- {
- i2c_sda_high(sda1);
- }
- else
- {
- i2c_sda_low(sda1);
- }
+        /* Data can be changed only while SCL is low */
+        if(data & 0x80)
+        {
+            i2c_sda_high(sda1);
+        }
+        else
+        {
+            i2c_sda_low(sda1);
+        }
 
- /* Data must be held stable while SCL is high */
- i2c_scl_high();
- i2c_scl_delay();
- data = data<<1;
- }
- /* Must set SCL to low. If SCL is high, SDA change to High/Low will cause Stop/Start signal.*/
- i2c_scl_low();
- /* check ACK signal, ACK signal is 0, NACK signal is 1 */
- if(i2c_read_ACK(sda1) == embot::hw::gpio::State::SET)
- {
- /* receive NACK signal,stop data transfer */
- i2c_stop(sda1);
- while(1);
- }
+        /* Data must be held stable while SCL is high */
+        i2c_scl_high();
+        i2c_scl_delay();
+        data = data<<1;
+    }
+    /* Must set SCL to low. If SCL is high, SDA change to High/Low will cause Stop/Start signal.*/
+    i2c_scl_low();
+    /* check ACK signal, ACK signal is 0, NACK signal is 1 */
+    if(i2c_read_ACK(sda1) == embot::hw::gpio::State::SET)
+    {
+        /* receive NACK signal,stop data transfer */
+        i2c_stop(sda1);
+        ret = false;
+//        while(1);
+    }
+    
+    return ret;
 }
 
 /* master reads a byte from slave */
@@ -474,12 +760,190 @@ unsigned char i2c_read_byte(void)
  return data;
 }
 
-	void i2c_gpio_tick() 
+void i2c_gpio_tick() 
 {
 
 }
 
-#endif
+
+bool I2C_readbyte(uint8_t *val)
+{
+    bool r = true;
+    
+    uint8_t data = 0;
+
+    /* disable PTB6(SDA) output, enable PTB6(SDA) input */
+    i2c_sda_output_off(sda1);
+    i2c_sda_input(sda1);
+    i2c_scl_delay(); // messo io
+     /* write 8 bits data */
+    for(uint8_t i=0;i<8;i++)
+    {
+        i2c_scl_low();
+        i2c_scl_delay();
+        /* Data must be held stable while SCL is high */
+        i2c_scl_high();
+        i2c_scl_delay(); // messo io
+        i2c_scl_delay(); // messo io
+        data = data<<1;
+        /* read SDA data */
+        if(i2c_sda_status(sda1) == embot::hw::gpio::State::SET)
+        {
+            data |= 0x01;
+        }
+        else
+        {
+            data &= ~0x01;
+        }
+        i2c_scl_delay();
+    }
+    /* disable PTB6(SDA) input, enable PTB6(SDA) output */
+    i2c_sda_input_off(sda1);
+    i2c_sda_output(sda1);
+
+    i2c_scl_low(); // set SCL to low
+
+    *val = data;
+ 
+    return r;
+}
+
+bool I2C_init()
+{
+    bool r = true;
+    
+    embot::hw::gpio::init(scl, out);
+    embot::hw::gpio::set(scl, embot::hw::gpio::State::SET);
+    
+    embot::hw::gpio::init(sda1, out);
+    embot::hw::gpio::set(sda1, embot::hw::gpio::State::SET);
+
+    return r;
+}
+
+bool I2C_ping(uint8_t adr)
+{
+    bool r = false;
+    
+    uint8_t wadr = adr | 0;
+    
+    i2c_start(sda1);
+    
+    r = i2c_write_byte(wadr);
+    
+    i2c_stop(sda1);
+    
+    return r;
+}
+
+uint8_t I2Cerror = 0;
+
+uint8_t I2C_geterror()
+{
+    return I2Cerror;
+}
+
+bool I2C_read(uint8_t adr, size_t n, uint8_t *data)
+{
+    bool r = false;
+    
+    I2Cerror = 0;
+    
+    uint8_t wadr = adr | 1;
+    i2c_start(sda1);
+    
+    r = i2c_write_byte(wadr);
+    
+    if(false == r)
+    {
+        I2Cerror = 1;
+        return r;
+    }
+    
+    for(uint8_t k=0; k<n; k++)
+    {
+        //data[k] = i2c_read_byte();
+        
+        I2C_readbyte(&data[k]);
+        
+        if(k == (n-1))
+        {
+            i2c_send_NACK(sda1);
+            i2c_stop(sda1);
+            break;            
+        }
+        else
+        {
+            i2c_send_ACK(sda1);
+        }
+        
+        i2c_scl_delay();        
+    }
+    
+    return r;
+}
+
+
+bool I2C_write(uint8_t adr, size_t n, uint8_t *data)
+{
+    bool r = false;
+    
+    I2Cerror = 0;
+    
+    uint8_t wadr = adr | 0;
+    i2c_start(sda1);
+    
+    r = i2c_write_byte(wadr);
+    
+    if(false == r)
+    {
+        I2Cerror = 1;
+        return r;
+    }
+    
+    for(uint8_t k=0; k<n; k++)
+    {       
+        r = i2c_write_byte(data[k]);     
+    }
+  
+    i2c_stop(sda1);   
+    
+    return r;
+}
+
+void i2c_send_ACK(embot::hw::GPIO sda)
+{
+    /* the 9th clock start */
+    i2c_scl_low();
+    /*send ACK signal */
+    i2c_sda_low(sda1);
+    i2c_scl_delay();
+    i2c_scl_high();
+    i2c_scl_delay();
+    /* the 9th clock start */
+    /* SCL hold low to wait */
+    i2c_scl_low();
+    i2c_sda_high(sda1);    
+}
+
+void i2c_send_NACK(embot::hw::GPIO sda)
+{
+    /* the 9th clock start*/
+    i2c_scl_low();
+    /*send NACK signal */
+    i2c_sda_high(sda1);
+    i2c_scl_delay();
+    i2c_scl_high();
+    i2c_scl_delay();
+    /* the 9th clock start */
+    /* SCL hold low to wait */
+    i2c_scl_low();
+    i2c_sda_high(sda1);    
+}
+
+#endif  // #if defined(TEST_I2C_EMULATED_embot)
+
+#endif  // #if defined(TEST_I2C_EMULATED)
 
 
 // - end-of-file (leave a blank line after)----------------------------------------------------------------------------
