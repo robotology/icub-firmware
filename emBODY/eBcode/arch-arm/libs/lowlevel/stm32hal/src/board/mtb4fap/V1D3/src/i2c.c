334fdedb582f970ce7f543f6416798a5bd442b8e
Marco Accame <marco.accame@iit.it>
2023-01-19 15:24:34 +0100

    mtb4fap runs at 16 MHz (#336)

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/mtb4fap/V1D3/src/i2c.c
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/mtb4fap/V1D3/src/i2c.c
@@ -43,7 +43,11 @@ void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
+#if defined(STM32HAL_mtb4fap64mhz)
   hi2c1.Init.Timing = 0x10707DBC;
+#else
+  hi2c1.Init.Timing = 0x00303D5B;
+#endif
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -86,7 +90,11 @@ void MX_I2C2_Init(void)
 
   /* USER CODE END I2C2_Init 1 */
   hi2c2.Instance = I2C2;
+#if defined(STM32HAL_mtb4fap64mhz)  
   hi2c2.Init.Timing = 0x10707DBC;
+#else
+  hi2c2.Init.Timing = 0x00303D5B;
+#endif  
   hi2c2.Init.OwnAddress1 = 0;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
