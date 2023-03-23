5b1247585575e848c41ad9905f6b209414097dee
marco.accame <marco.accame@iit.it>
2023-03-23 12:42:27 +0100

    advanced versions: ems v 3.64, build 23 mar 2023 mc4plus v 3.67, build 23 mar 2023 mc2plus v 3.46, build 23 mar 2023

--- emBODY/eBcode/arch-arm/board/mc4plus/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/mc4plus/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -86,7 +86,7 @@ extern "C" {
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          (VERSION_MAJOR_OFFSET+3)
 //  <o> minor           <0-255> 
 
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          66
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          67
 
 //  </h>version
 
@@ -96,11 +96,11 @@ extern "C" {
 //  <o> month           <1-12>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        3
 //  <o> day             <1-31>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          8
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          23
 //  <o> hour            <0-23>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         17
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         11
 //  <o> minute          <0-59>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          25
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          55
 
 //  </h>build date
 
