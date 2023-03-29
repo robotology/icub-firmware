b84406ec7a6ec4c4b66844b78e75a557143725a6
marco.accame <marco.accame@iit.it>
2023-03-29 15:25:24 +0200

    mtb4c: updated to support stm32hal v1D3 and v190

--- emBODY/eBcode/arch-arm/board/mtb4c/application/src/main-appcan.cpp
+++ emBODY/eBcode/arch-arm/board/mtb4c/application/src/main-appcan.cpp
@@ -18,7 +18,7 @@ constexpr embot::app::theCANboardInfo::applicationInfo applInfo
     embot::prot::can::versionOfAPPLICATION {22, 0, 0},    
     embot::prot::can::versionOfCANPROTOCOL {20, 0} 
 #else    
-    embot::prot::can::versionOfAPPLICATION {2, 0, 0},    
+    embot::prot::can::versionOfAPPLICATION {2, 1, 0},    
     embot::prot::can::versionOfCANPROTOCOL {2, 0} 
 #endif    
 };
