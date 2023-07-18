da2bf036a4394fcc7994d6093ec8a6f70786c5a7
marco.accame <marco.accame@iit.it>
2023-07-18 16:17:22 +0200

    amcbldc: removed the TIM1_UP_TIM16 interrupt because it executes every 6 us (so wastes time) and is useless

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/amcbldc/v122/src/tim.c
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/src/board/amcbldc/v122/src/tim.c
@@ -316,8 +316,8 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     /* TIM1 interrupt Init */
     HAL_NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn);
-    HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
+//    HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0, 0);
+//    HAL_NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
     HAL_NVIC_SetPriority(TIM1_TRG_COM_TIM17_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(TIM1_TRG_COM_TIM17_IRQn);
     HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0, 0);
@@ -479,7 +479,7 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
     /* HAL_NVIC_DisableIRQ(TIM1_BRK_TIM15_IRQn); */
   /* USER CODE END TIM1:TIM1_BRK_TIM15_IRQn disable */
 
-    HAL_NVIC_DisableIRQ(TIM1_UP_TIM16_IRQn);
+//    HAL_NVIC_DisableIRQ(TIM1_UP_TIM16_IRQn);
     HAL_NVIC_DisableIRQ(TIM1_TRG_COM_TIM17_IRQn);
     HAL_NVIC_DisableIRQ(TIM1_CC_IRQn);
   /* USER CODE BEGIN TIM1_MspDeInit 1 */
