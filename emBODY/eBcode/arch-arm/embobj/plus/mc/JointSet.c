robotology/icub-firmware

commit 5fea0ee9fb8bbf2c6fa935a86ddd38670c7e9a5a

Use of `embot::app::eth::theEncoderReader` in all ETH boards (#399)

emBODY/eBcode/arch-arm/embobj/plus/mc/JointSet.c
@@ -33,7 +33,8 @@
 #warning USE_EMBOT_theServices is defined: removed some code
 // marco.accame: use objects embot::app::eth::theEncoderReader and ... future ones
 #else
-#include "EOappEncodersReader.h"
+//#include "EOtheEncoderReader.h"
+#include "embot_app_eth_theEncoderReader.h"
 #include "EOtheMAIS.h"
 #include "EOthePOS.h"
 #endif
@@ -1720,7 +1721,10 @@ void JointSet_calibrate(JointSet* o, uint8_t e, eOmc_calibrator_t *calibrator)
             if (eo_mais_isAlive(eo_mais_GetHandle()))
             {
                 float computedJntEncoderResolution = (float)(calibrator->params.type6.vmax - calibrator->params.type6.vmin) / (float) (jconfig->userlimits.max  - jconfig->userlimits.min);
-            
+
+#if 1
+                embot::app::eth::theEncoderReader::getInstance().Scale({e, embot::app::eth::encoder::v1::Position::every}, {computedJntEncoderResolution, 0});
+#else
                 eOresult_t res = eo_appEncReader_UpdatedMaisConversionFactors(eo_appEncReader_GetHandle(), e, computedJntEncoderResolution);
                 if(eores_OK != res)
                 {    
@@ -1731,7 +1735,7 @@ void JointSet_calibrate(JointSet* o, uint8_t e, eOmc_calibrator_t *calibrator)
                     ////debug code ended
                     return;
                 }
-
+#endif 
                 AbsEncoder_config_resolution(o->absEncoder+e, computedJntEncoderResolution);
             
                 //Now I need to re-init absEncoder because I chenged maisConversionFactor, therefore the values returned by EOappEncoreReder are changed.
@@ -1790,6 +1794,10 @@ void JointSet_calibrate(JointSet* o, uint8_t e, eOmc_calibrator_t *calibrator)
             
             int32_t offset = (((float)calibrator->params.type7.vmin)/computedJntEncoderResolution) - jconfig->userlimits.min;
             
+
+#if 1
+            embot::app::eth::theEncoderReader::getInstance().Scale({e, embot::app::eth::encoder::v1::Position::every}, {computedJntEncoderResolution, offset});
+#else            
             eOresult_t res = eo_appEncReader_UpdatedHallAdcOffset(eo_appEncReader_GetHandle(), e, offset);
             if(eores_OK != res)
             {    
@@ -1812,7 +1820,7 @@ void JointSet_calibrate(JointSet* o, uint8_t e, eOmc_calibrator_t *calibrator)
                 ////debug code ended
                 return;
             }
-
+#endif       
             AbsEncoder_config_resolution(o->absEncoder+e, computedJntEncoderResolution);
             
             //Now I need to re init absEncoder because I chenged hallADCConversionFactor, therefore the values returned by EOappEncoreReder are changed.

emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theEncoderReaderLegacy.cpp
@@ -1,0 +1,205 @@
+
+/*
+ * Copyright (C) 2023 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - public interface
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "embot_app_eth_theEncoderReader.h"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "EOtheEncoderReader.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - pimpl: private implementation (see scott meyers: item 22 of effective modern c++, item 31 of effective c++
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+struct embot::app::eth::theEncoderReader::Impl
+{
+    
+    Impl() = default;
+    
+    bool initialise();
+    
+    bool Verify(const Config &config, const OnEndOfOperation &onverify, bool activateafterverify);                            
+    bool Activate(const Config &config);    
+    bool Deactivate();   
+    bool StartReading();    
+    bool Read(uint8_t position, embot::app::eth::encoder::v1::valueInfo &primary, embot::app::eth::encoder::v1::valueInfo &secondary);
+    bool SendReport();                            
+    bool IsReadingAvailable();
+    bool Diagnostics_Tick();
+    embot::app::eth::encoder::v1::Type GetType(const embot::app::eth::encoder::v1::Target &target);
+    bool Scale(const embot::app::eth::encoder::v1::Target &target, const embot::app::eth::encoder::v1::Scaler &scaler);
+    
+    // advanced
+    bool read(const embot::app::eth::encoder::experimental::Target &target, embot::app::eth::encoder::experimental::Value &value);
+
+};
+
+
+bool embot::app::eth::theEncoderReader::Impl::initialise()
+{
+    eo_encoderreader_Initialise();
+    
+    return true;
+}
+
+bool embot::app::eth::theEncoderReader::Impl::Verify(const Config &config, const OnEndOfOperation &onverify, bool activateafterverify)
+{ 
+    return eores_OK == eo_encoderreader_Verify(eo_encoderreader_GetHandle(), config.carrayofjomodes, onverify.callback, static_cast<eObool_t>(activateafterverify), config.dc);
+}
+
+
+bool embot::app::eth::theEncoderReader::Impl::Activate(const Config &config)
+{
+    return eores_OK == eo_encoderreader_Activate(eo_encoderreader_GetHandle(), config.carrayofjomodes, config.dc);
+}
+
+bool embot::app::eth::theEncoderReader::Impl::Deactivate()
+{
+    return eores_OK == eo_encoderreader_Deactivate(eo_encoderreader_GetHandle());
+}
+
+bool embot::app::eth::theEncoderReader::Impl::StartReading()
+{
+    return eores_OK == eo_encoderreader_StartReading(eo_encoderreader_GetHandle());
+}
+
+bool embot::app::eth::theEncoderReader::Impl::Read(uint8_t position, embot::app::eth::encoder::v1::valueInfo &primary, embot::app::eth::encoder::v1::valueInfo &secondary)
+{
+    eOencoderreader_valueInfo_t *e1 = reinterpret_cast<eOencoderreader_valueInfo_t*>(&primary);
+    eOencoderreader_valueInfo_t *e2 = reinterpret_cast<eOencoderreader_valueInfo_t*>(&primary);
+    return eores_OK == eo_encoderreader_Read(eo_encoderreader_GetHandle(), position, e1, e2);
+}
+
+bool embot::app::eth::theEncoderReader::Impl::SendReport()
+{
+    return eores_OK == eo_encoderreader_SendReport(eo_encoderreader_GetHandle());
+}
+
+bool embot::app::eth::theEncoderReader::Impl::Diagnostics_Tick()
+{    
+    return eores_OK == eo_encoderreader_Diagnostics_Tick(eo_encoderreader_GetHandle());
+}
+
+bool embot::app::eth::theEncoderReader::Impl::IsReadingAvailable()
+{    
+    return eobool_true == eo_encoderreader_IsReadingAvailable(eo_encoderreader_GetHandle());
+}
+
+embot::app::eth::encoder::v1::Type  embot::app::eth::theEncoderReader::Impl::GetType(const embot::app::eth::encoder::v1::Target &target)
+{
+    eOmc_encoder_t t = eo_encoderreader_GetType(eo_encoderreader_GetHandle(), target.jomo, static_cast<eOencoderreader_Position_t>(embot::core::tointegral(target.pos)));    
+    return static_cast<embot::app::eth::encoder::v1::Type>(t);
+}
+
+bool embot::app::eth::theEncoderReader::Impl::Scale(const embot::app::eth::encoder::v1::Target &target, const embot::app::eth::encoder::v1::Scaler &scaler)
+{
+    // for now only mais and absanalog accept to be scaled ... but in here i dont verify
+    eOencoderreader_Scaler sca {scaler.scale, scaler.offset};
+    return eores_OK == eo_encoderreader_Scale(eo_encoderreader_GetHandle(), target.jomo, static_cast<eOencoderreader_Position_t>(embot::core::tointegral(target.pos)), &sca);
+}
+
+bool embot::app::eth::theEncoderReader::Impl::read(const embot::app::eth::encoder::experimental::Target &target, embot::app::eth::encoder::experimental::Value &value)
+{
+    value.raw = 0x123456789A;
+    value.error = embot::app::eth::encoder::experimental::Error::NONE;
+    
+    return true;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - the class
+// --------------------------------------------------------------------------------------------------------------------
+
+embot::app::eth::theEncoderReader& embot::app::eth::theEncoderReader::getInstance()
+{
+    static theEncoderReader* p = new theEncoderReader();
+    return *p;
+}
+
+embot::app::eth::theEncoderReader::theEncoderReader()
+{
+    pImpl = std::make_unique<Impl>();
+}
+
+embot::app::eth::theEncoderReader::~theEncoderReader() { }
+
+bool embot::app::eth::theEncoderReader::initialise()
+{
+    return pImpl->initialise();
+}
+
+bool embot::app::eth::theEncoderReader::Verify(const Config &config, const OnEndOfOperation &onverify, bool activateafterverify)
+{
+    return pImpl->Verify(config, onverify, activateafterverify); 
+}
+
+
+bool embot::app::eth::theEncoderReader::Activate(const Config &config)
+{
+    return pImpl->Activate(config);
+}
+
+bool embot::app::eth::theEncoderReader::Deactivate()
+{
+    return pImpl->Deactivate();
+}
+
+bool embot::app::eth::theEncoderReader::StartReading()
+{
+    return pImpl->StartReading();
+}
+
+bool embot::app::eth::theEncoderReader::Read(uint8_t position, embot::app::eth::encoder::v1::valueInfo &primary, embot::app::eth::encoder::v1::valueInfo &secondary)
+{
+    return pImpl->Read(position, primary, secondary);
+}
+
+bool embot::app::eth::theEncoderReader::SendReport()
+{
+    return pImpl->SendReport();
+}
+
+bool embot::app::eth::theEncoderReader::IsReadingAvailable()
+{
+    return pImpl->IsReadingAvailable();
+}
+
+bool embot::app::eth::theEncoderReader::Diagnostics_Tick()
+{
+    return pImpl->Diagnostics_Tick();
+}
+
+embot::app::eth::encoder::v1::Type  embot::app::eth::theEncoderReader::GetType(const embot::app::eth::encoder::v1::Target &target)
+{
+    return pImpl->GetType(target);
+}
+
+bool embot::app::eth::theEncoderReader::Scale(const embot::app::eth::encoder::v1::Target &target, const embot::app::eth::encoder::v1::Scaler &scaler)
+{
+    return pImpl->Scale(target, scaler);
+}
+
+bool embot::app::eth::theEncoderReader::read(const embot::app::eth::encoder::experimental::Target &target, embot::app::eth::encoder::experimental::Value &value)
+{
+    return pImpl->read(target, value);
+}
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+

emBODY/eBcode/arch-arm/board/amc/examples/appl-01/cfg/theApplication_config.h
@@ -40,14 +40,14 @@ namespace embot { namespace app { namespace eth {
             Process::eApplication,
 #if defined(WRIST_MK2)
     #if defined(WRIST_MK2_RIGHT)
-            {101, 6},
+            {101, 7},
     #else
-            {102, 6},
+            {102, 7},
     #endif            
 #else            
-            {103, 6},  
+            {103, 7},  
 #endif            
-            {2023, Month::Jul, Day::twentyfour, 18, 00}
+            {2023, Month::Jul, Day::twentysix, 11, 11}
         },
         .OStick = 1000*embot::core::time1microsec,
         .OSstacksizeinit = 10*1024,

emBODY/eBcode/arch-arm/board/amc/examples/appl-01/proj/appl-01-wrist.uvprojx
@@ -16,8 +16,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>
@@ -2536,8 +2536,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>
@@ -4884,8 +4884,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>
@@ -7742,8 +7742,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>
@@ -9376,8 +9376,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>
@@ -11010,8 +11010,8 @@
         <TargetCommonOption>
           <Device>STM32H745IIKx:CM7</Device>
           <Vendor>STMicroelectronics</Vendor>
-          <PackID>Keil.STM32H7xx_DFP.3.0.0</PackID>
-          <PackURL>http://www.keil.com/pack/</PackURL>
+          <PackID>Keil.STM32H7xx_DFP.3.1.1</PackID>
+          <PackURL>https://www.keil.com/pack/</PackURL>
           <Cpu>IRAM(0x38000000,0x00010000) IRAM2(0x24000000,0x00080000) IROM(0x08000000,0x00100000) XRAM(0x20000000,0x00020000) CPUTYPE("Cortex-M7") FPU3(DFPU) CLOCK(12000000) ELITTLE</Cpu>
           <FlashUtilSpec></FlashUtilSpec>
           <StartupFile></StartupFile>

emBODY/eBcode/arch-arm/board/amcbldc/application/proj/amcbldc-application2.uvoptx
@@ -1284,7 +1284,7 @@
 
   <Group>
     <GroupName>others::motorhal2</GroupName>
-    <tvExp>1</tvExp>
+    <tvExp>0</tvExp>
     <tvExpOptDlg>0</tvExpOptDlg>
     <cbSel>0</cbSel>
     <RteFlg>0</RteFlg>

emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -81,7 +81,7 @@ extern "C" {
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          (VERSION_MAJOR_OFFSET+3)
 //  <o> minor           <0-255> 
 //  <o> minor           <0-255>
-#define EOMTHEEMSAPPLCFG_VERSION_MINOR          70
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          71
 
 //  </h>version
 
@@ -91,11 +91,11 @@ extern "C" {
 //  <o> month           <1-12>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        7
 //  <o> day             <1-31>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          24
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          26
 //  <o> hour            <0-23>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         18
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         11
 //  <o> minute          <0-59>
-#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          00
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          11
 //  </h>build date
 // </h>Info 
 

commit 857f1258ff73bc129a5cca359ac38eeebb8f1bef

Update evaluation of final offset for calibratio14 + bugfix
Update evaluation on final offset to be passed by JointSet to POS
service so that user set calibration5 param for calibration type 14
as it is printed when port state is read
Add fix to bug introduced by PR #399:
values not updated by state and stateExt ports

emBODY/eBcode/arch-arm/embobj/plus/mc/JointSet.c
@@ -2067,11 +2067,8 @@ void JointSet_calibrate(JointSet* o, uint8_t e, eOmc_calibrator_t *calibrator)
                 //1) Fix the offset with the rotation raw
                 int32_t offset_raw = (calibrator->params.type14.offset + calibrator->params.type14.rotation ) % ((int32_t)DEG2ICUB*360);
                 
-                //2) Take offset value of calibation parameter with sign given by invertdirection
-                int32_t offset_fix = (calibrator->params.type14.invertdirection == 1) ? -(offset_raw) : offset_raw;
-                
-                //3) Convert offset to decideg for POS service
-                int32_t offset = ((ICUB2DEG)*(CTRL_UNITS)(offset_fix))*10.0f;
+                //3) Convert offset to decideg for POS service and add "-" sign to offset_raw to correct raw position (always minus sign independently from invertDirection value)
+                int32_t offset = ((ICUB2DEG)*(CTRL_UNITS)((-1)*offset_raw))*10.0f;
                 
                 ////debug code
                 snprintf(info, sizeof(info), "CALIB 14 j%d: or=%d o=%d", e, offset_raw, offset);

emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theEncoderReaderLegacy.cpp
@@ -80,7 +80,7 @@ bool embot::app::eth::theEncoderReader::Impl::StartReading()
 bool embot::app::eth::theEncoderReader::Impl::Read(uint8_t position, embot::app::eth::encoder::v1::valueInfo &primary, embot::app::eth::encoder::v1::valueInfo &secondary)
 {
     eOencoderreader_valueInfo_t *e1 = reinterpret_cast<eOencoderreader_valueInfo_t*>(&primary);
-    eOencoderreader_valueInfo_t *e2 = reinterpret_cast<eOencoderreader_valueInfo_t*>(&primary);
+    eOencoderreader_valueInfo_t *e2 = reinterpret_cast<eOencoderreader_valueInfo_t*>(&secondary);
     return eores_OK == eo_encoderreader_Read(eo_encoderreader_GetHandle(), position, e1, e2);
 }
 