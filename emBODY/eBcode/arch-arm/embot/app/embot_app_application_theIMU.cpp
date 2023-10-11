95f22d25416a013d28dc9ebb4d4891bab4a477d4
tumme <davide.tome@iit.it>
2023-10-11 11:59:10 +0200

    imu bosch - fixed euler angles

--- emBODY/eBcode/arch-arm/embot/app/embot_app_application_theIMU.cpp
+++ emBODY/eBcode/arch-arm/embot/app/embot_app_application_theIMU.cpp
@@ -411,14 +411,14 @@ bool embot::app::application::theIMU::Impl::processdata(std::vector<embot::prot:
 
             // The Bosch euler representation can be either the windows(default) or android, but in YARP
             // we have a different representation(see https://github.com/robotology/yarp/blob/0481f994c6e03897d038c5f1d1078145646a1772/src/libYARP_dev/src/yarp/dev/MultipleAnalogSensorsInterfaces.h#L302-L348)
-#ifdef STM32HAL_BOARD_RFE 
-   					info.value.x = -imuacquisition.data.eul.z;
+#if defined STM32HAL_BOARD_STRAIN2 ||  defined STM32HAL_BOARD_STRAIN2C 
+						info.value.x = -imuacquisition.data.eul.z;
+            info.value.y = imuacquisition.data.eul.y;
+            info.value.z = -imuacquisition.data.eul.x;		
+#else		
+						info.value.x = -imuacquisition.data.eul.z;
             info.value.y = -imuacquisition.data.eul.y;
             info.value.z = -imuacquisition.data.eul.x;
-#else
-   					info.value.x = imuacquisition.data.eul.z;
-            info.value.y = -imuacquisition.data.eul.y;
-            info.value.z = imuacquisition.data.eul.x;					
 #endif
             msg.load(info);
             msg.get(frame);
