39f87ddf68f8e38210d44151d08615bf4ec445a1
Davide Tomé <davide.tome@iit.it>
2023-11-16 08:47:31 +0100

    hal2.h - add missing header (#440)

---
+++ emBODY/eBcode/arch-arm/libs/highlevel/abslayer/hal2/api/hal2.h
@@ -0,0 +1,25 @@
+
+/*
+ * Copyright (C) 2023 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef __HAL2_H_
+#define __HAL2_H_
+
+// it just wraps the usual hal interface
+
+#include "hal.h"
+#include "hal_motor.h"
+#include "hal_adc.h"
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
