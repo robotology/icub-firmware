a04eedc11d9ec6e3576c53943b97c450b12f0f96
marco.accame <marco.accame@iit.it>
2023-11-24 09:04:02 +0100

    the wip ICC service object in nearly final form

--- emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theICCservice.cpp
+++ emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theICCservice.cpp
@@ -17,6 +17,11 @@
 // - external dependencies
 // --------------------------------------------------------------------------------------------------------------------
 
+#include "embot_os_rtos.h"
+#include "embot_hw_icc_ltr.h"
+#include "embot_hw_icc_ltr_bsp.h"
+
+#include <array>
 
 using namespace std;
 
@@ -25,45 +30,465 @@ using namespace std;
 // --------------------------------------------------------------------------------------------------------------------
 
 
-
+void tRXicc(void *p) { reinterpret_cast<embot::os::Thread*>(p)->run(); }
+void tTXicc(void *p) { reinterpret_cast<embot::os::Thread*>(p)->run(); }
+    
 struct embot::app::eth::theICCservice::Impl
 {
-
     bool _initted {false};
-
+    Config _config {};
     
     constexpr static const char _objname[] = "theICCservice";
     
     Impl() = default;
     
-    bool initialise();
+    bool initialise(const Config &config);
+        
+    bool set(modeTX txm);
+    bool set(const embot::core::Callback &onr);
+    bool set(ItemParser itmp);
     
-    bool add(const embot::app::eth::mc::messaging::Location &des, const embot::prot::can::Frame &frame);                            
+    bool put(const Item &item, embot::core::relTime timeout);   
+    size_t output() const;
+    bool flush(const embot::core::Callback &cbk);
+    bool flush(embot::core::relTime timeout);
+    bool flushed() const;
+    
+    size_t input() const;
+    bool get(Item &item, size_t &remaining); 
+    bool parse();        
 
 private:    
+    
+    // we exchange this data structure
+    struct lrtData
+    {
+        size_t size {0};
+        std::array<Item, Config::maxcapacity> items {};
+        void clear()
+        {
+            size = 0;
+            //std::memset(items.data(), 0, items.size()*sizeof(Item));
+        }            
+    };
+    static_assert(964 == sizeof(lrtData), "lrtData is not 964");
+    
+    struct iccInfo
+    {        
+        embot::hw::icc::LTR ltr {embot::hw::icc::LTR::one}; 
+        lrtData ltrdata {};
+        embot::core::Data dd {};
+        constexpr iccInfo(embot::hw::icc::LTR l) : ltr(l), dd(&ltrdata, sizeof(lrtData)) {}
+    };
+    
+    static constexpr embot::hw::icc::LTR ltrA {embot::hw::icc::LTR::one};
+    static constexpr embot::hw::icc::LTR ltrB {embot::hw::icc::LTR::two};
+    
+//    static_assert(644 == sizeof(lrtData), "lrtData is too big to fit in LTR::one");
+//    
+//    static_assert(20 == sizeof(Item) , "lrtData is too big to fit in LTR::one");
+//    
+//    static constexpr embot::hw::icc::MEM m = embot::hw::icc::ltr::bsp::getBSP().getPROP(ltrA)->mem;
+    
+//    static_assert(embot::hw::icc::ltr::size(embot::hw::icc::LTR::one) > 1, "not 1024");
+    
     void todo();
+    
+    static bool defaultparser(const Item &item) { return true; };
+    
+    ItemParser itemparser {defaultparser};
+
+// what the Impl needs:
+// 
+
+    std::vector<Item> _TX_fifo {};
+    embot::os::rtos::mutex_t *_TX_mtx {nullptr};
+    
+    std::vector<Item> _RX_fifo {};
+    embot::os::rtos::mutex_t *_RX_mtx {nullptr};
+    
+    embot::os::EventThread *tRX {nullptr};
+    static constexpr embot::os::Event evtRX = 1;
+    // static void rxcbk(void *p) { Impl *i = reinterpret_cast<Impl*>(p); i->tRX->setEvent(evRX); }
+    static void rxICCcbk(void *p) { 
+        reinterpret_cast<Impl*>(p)->tRX->setEvent(evtRX); 
+    }
+    
+    static void tRXstartup(embot::os::Thread *t, void *p);
+    static void tRXonevent(embot::os::Thread *t, os::EventMask m, void *p);
+    
+    iccInfo _iccRX {embot::hw::icc::LTR::one};
+    iccInfo _iccTX {embot::hw::icc::LTR::two};
+    
+    embot::os::EventThread *tTX {nullptr};
+    static constexpr embot::os::Event evtTX = 2;
+    volatile bool _TXacked {false};
+    volatile bool _flushed {false}; // as _TXacked but true also when we doid not need to tx anything
+    embot::os::rtos::semaphore_t *txsemaphore {nullptr};
+    embot::core::Callback cbkOnTXdone {}; 
+            
+        
+    static void tTXstartup(embot::os::Thread *t, void *p);
+    static void tTXonevent(embot::os::Thread *t, os::EventMask m, void *p);
+
+    static void donothing(void *p) {}
+    constexpr static embot::core::Callback cbkDoNothing {donothing, nullptr};
+        
+    static void txackcbk(void *p)
+    {
+        Impl *impl = reinterpret_cast<Impl*>(p);
+        
+        impl->_flushed = impl->_TXacked = embot::hw::icc::ltr::acked(impl->_iccTX.ltr, true);
+        
+        if(true == impl->cbkOnTXdone.isvalid())
+        {
+            impl->cbkOnTXdone.execute();
+        }
+        else
+        {
+            embot::os::rtos::semaphore_release(impl->txsemaphore);
+        }
+        
+    }
 };
 
 
-bool embot::app::eth::theICCservice::Impl::initialise()
+bool embot::app::eth::theICCservice::Impl::initialise(const Config &config)
 {
     if(true == _initted)
     {
         return true;
     }
         
-    embot::core::print("embot::app::eth::theICCservice::Impl::initialise()");
+    // embot::core::print("embot::app::eth::theICCservice::Impl::initialise()");
      
     // ok, do something
+    _config = config;
+    
+
+    _iccRX.ltr = _config.rxltr;    
+    _iccTX.ltr = _config.txltr;
+
+//    // this object must be used by the two cores (A an B)
+//    // the LRT that TX in one core must be the same that receives in the other one 
+//    switch(_config.direction)
+//    {
+//        default:
+//        case Direction::A2B:
+//        {
+//            _iccRX.ltr = embot::hw::icc::LTR::one;
+//            _iccTX.ltr = embot::hw::icc::LTR::two;
+//            
+//        } break;
+//        
+//        case Direction::B2A:
+//        {
+//            _iccRX.ltr = embot::hw::icc::LTR::two;
+//            _iccTX.ltr = embot::hw::icc::LTR::one;            
+//        } break;        
+//    }    
+
+    // we now check if the two letters have enough capacity to hold our data
+    
+    size_t rxs = embot::hw::icc::ltr::size(_iccRX.ltr);
+    size_t txs = embot::hw::icc::ltr::size(_iccTX.ltr);
+    
+    if(embot::hw::icc::ltr::size(_iccRX.ltr) < sizeof(lrtData))
+    {
+        embot::core::print("embot::app::eth::theICCservice::Impl::initialise(): the RX ltr cannot hold the Config::maxcapacity");
+        return false;
+    }
+    
+    if(embot::hw::icc::ltr::size(_iccTX.ltr) < sizeof(lrtData))
+    {
+        embot::core::print("embot::app::eth::theICCservice::Impl::initialise(): the TX ltr cannot hold the Config::maxcapacity");
+        return false;
+    }
+    
+    _TX_fifo.reserve(_config.txcapacity);
+    _TX_fifo.clear();
+    _TX_mtx = embot::os::rtos::mutex_new();
+    
+    _RX_fifo.reserve(_config.rxcapacity);
+    _RX_fifo.clear();
+    _RX_mtx = embot::os::rtos::mutex_new();
+    
+    
+    itemparser = (nullptr != _config.itemparser) ? (_config.itemparser) : (defaultparser);
+    
+    constexpr embot::core::relTime timeout {1000*embot::core::time1millisec};
+    
+    // RX thread
+    embot::os::EventThread::Config rxCfg { 
+        4096, //_config.thread.stacksize, 
+        _config.rxpriority, // _config.thread.priority, 
+        tRXstartup, this,
+        timeout,
+        tRXonevent,
+        "tRXicc"
+    };
+       
+    tRX = new embot::os::EventThread;          
+    tRX->start(rxCfg, tRXicc);    
+
+    // TX thread
+    embot::os::EventThread::Config txCfg { 
+        4096, //_config.thread.stacksize, 
+        _config.rxpriority, // _config.thread.priority, 
+        tTXstartup, this,
+        timeout,
+        tTXonevent,
+        "tTXicc"
+    };
+       
+        
+    // create the RX thread 
+    tTX = new embot::os::EventThread;          
+    // and start it
+    tTX->start(txCfg, tTXicc);     
     
     _initted = true;
     return true;
 }
 
-bool embot::app::eth::theICCservice::Impl::add(const embot::app::eth::mc::messaging::Location &des, const embot::prot::can::Frame &frame)
+bool embot::app::eth::theICCservice::Impl::set(modeTX txm)
+{
+    if(false == _initted)
+    {
+        return false;
+    }
+    
+    _config.modetx = txm;
+    return true;
+}
+
+bool embot::app::eth::theICCservice::Impl::set(const embot::core::Callback &onr)
+{
+    if(false == _initted)
+    {
+        return false;
+    }  
+    
+    _config.onrx = onr;
+    return true;    
+}
+
+bool embot::app::eth::theICCservice::Impl::set(ItemParser itmp)
+{
+    if(false == _initted)
+    {
+        return false;
+    } 
+    
+    itemparser = (nullptr != itmp) ? (itmp) : (defaultparser);
+    return true;    
+}
+
+
+bool embot::app::eth::theICCservice::Impl::put(const Item &item, embot::core::relTime timeout)
 { 
+    if(false == _initted)
+    {
+        return false;
+    }
+    
     // add in a buffer. do not trigger any tx yet
+    bool r = embot::os::rtos::mutex_take(_TX_mtx, timeout);
+    if(false == r)
+    {
+        return false;
+    }
+    
+    if(_TX_fifo.size() >= _config.txcapacity)
+    {
+        _TX_fifo.erase(_TX_fifo.begin());
+    }
+    _TX_fifo.push_back(item);
+    
+    embot::os::rtos::mutex_release(_TX_mtx);  
+
+    if(modeTX::instant == _config.modetx)
+    {
+        return flush(timeout);
+    }
+    
     return true;  
+}
+
+size_t embot::app::eth::theICCservice::Impl::output() const
+{
+    if(false == _initted)
+    {
+        return 0;
+    }
+    
+    size_t s {0};
+    
+    embot::os::rtos::mutex_take(_TX_mtx, embot::core::reltimeWaitForever);
+    s = _TX_fifo.size();
+    embot::os::rtos::mutex_release(_TX_mtx);
+    
+    return s;
+}
+
+bool embot::app::eth::theICCservice::Impl::flush(const embot::core::Callback &cbk)
+{
+    bool r {false};
+
+    if(false == _initted)
+    {
+        return false;
+    }
+    
+    // we see if we have any to transmit    
+    if(0 == output())
+    {
+        _flushed = true;
+        r = true;
+        cbk.execute();
+        return r;
+    }
+    
+    // but what if ... tyen tx thread has not received any ack yet becaus ethe otehr core is not responding?
+    // think of it 
+    cbkOnTXdone = (true == cbk.isvalid()) ? cbk : cbkDoNothing;
+        
+    tTX->setEvent(evtTX);    
+
+    return _flushed; 
+}
+
+bool embot::app::eth::theICCservice::Impl::flush(embot::core::relTime timeout)
+{
+    bool r {false};
+
+    if(false == _initted)
+    {
+        return false;
+    }
+    
+    // we see if we have any to transmit    
+    if(0 == output())
+    {
+        _flushed = true;
+        r = true;
+        return r;
+    }
+    
+    // what if the tx thread does not received any ack because the other core is stopped or is not responding?
+    // the txThread just calls embot::hw::icc::ltr::post() and terminates its tick. however its 
+    // callback is never called, the semaphore is never released and ... this function just returns 
+    // false after the timeout
+        
+    cbkOnTXdone.clear();
+    tTX->setEvent(evtTX);    
+    r = embot::os::rtos::semaphore_acquire(txsemaphore, timeout); 
+    r = _flushed;    
+    return r; 
+}
+
+bool embot::app::eth::theICCservice::Impl::flushed() const
+{
+    if(false == _initted)
+    {
+        return false;
+    }    
+    
+    return _flushed;
+}
+
+size_t embot::app::eth::theICCservice::Impl::input() const
+{
+    if(false == _initted)
+    {
+        return 0;
+    }
+    
+    size_t s {0};
+    
+    embot::os::rtos::mutex_take(_RX_mtx, embot::core::reltimeWaitForever);
+    s = _RX_fifo.size();
+    embot::os::rtos::mutex_release(_RX_mtx);
+    
+    return s;
+}
+
+bool embot::app::eth::theICCservice::Impl::get(Item &item, size_t &remaining)
+{
+    if(false == _initted)
+    {
+        return false;
+    }    
+    
+    bool r {false};
+    Item rxi {};
+    size_t s {0};
+    
+    embot::os::rtos::mutex_take(_RX_mtx, embot::core::reltimeWaitForever);
+    s = _RX_fifo.size();
+    if(s > 0)
+    {
+        rxi = _RX_fifo.front();
+        _RX_fifo.erase(_RX_fifo.begin());
+        remaining = _RX_fifo.size();
+        r = true;
+    }
+    embot::os::rtos::mutex_release(_RX_mtx);     
+        
+    item = rxi;
+        
+    return r; 
+}
+
+bool embot::app::eth::theICCservice::Impl::parse()
+{
+    if(false == _initted)
+    {
+        return false;
+    }    
+    
+#if 0
+    bool r {false};
+    Item rxi {};
+    size_t s {0};
+    
+    embot::os::rtos::mutex_take(_RX_mtx, embot::core::reltimeWaitForever);
+    s = _RX_fifo.size();
+    for(size_t i=0; i<s; i++)
+    {
+        rxi = _RX_fifo[i];
+        itemparser(rxi);
+    }
+    _RX_fifo.clear();
+    embot::os::rtos::mutex_release(_RX_mtx);     
+        
+    r = true;
+    return r;     
+#else
+    // the above is ok but keeps the rx fifo locked for too long, so the ack towards the other core is delayed. 
+    // the quicker response is if we extract items one by one because we keep the rx queue available during parsing
+    // on the other hand, this solution takes and releases a mutex and operations over svc ... take time
+    
+    bool r {false};
+    Item rxi {};
+    size_t s {0};
+    
+    s = input();
+    
+    
+    for(size_t i=0; i<s; i++)
+    {
+        size_t remaining {0};
+        if(true == get(rxi, remaining))
+        {
+            itemparser(rxi);
+            r = true;
+        }
+    }    
+    
+    return r;
+    
+#endif
 }
 
 
@@ -75,6 +500,125 @@ void embot::app::eth::theICCservice::Impl::todo()
 
 }
 
+
+void embot::app::eth::theICCservice::Impl::tRXstartup(embot::os::Thread *t, void *p)
+{
+    Impl *impl = reinterpret_cast<Impl*>(p);
+    volatile embot::hw::result_t r {embot::hw::resNOK};
+    r = embot::hw::icc::ltr::init(impl->_iccRX.ltr, {embot::hw::icc::DIR::rx});
+    bool b = embot::hw::icc::ltr::subscribe(impl->_iccRX.ltr, {{rxICCcbk, impl}, embot::hw::Subscription::MODE::permanent});
+    r = r;
+    b = b;
+}
+
+
+void embot::app::eth::theICCservice::Impl::tRXonevent(embot::os::Thread *t, os::EventMask eventmask, void *p)
+{
+    if(0 == eventmask)
+    {   // timeout ...       
+        return;
+    }
+    
+    Impl *impl = reinterpret_cast<Impl*>(p);
+    
+    if(true == embot::core::binary::mask::check(eventmask, evtRX))
+    {
+        impl->_iccRX.ltrdata.clear();
+        
+        embot::hw::icc::ltr::read(impl->_iccRX.ltr, impl->_iccRX.dd);
+        
+        // now i use _iccRX.dd, or rather i use _iccRX.ltrdata
+        // take mtx
+        // copy _iccRX.ltrdata
+        // release mtx
+        size_t num = std::min(impl->_iccRX.ltrdata.size, impl->_iccRX.ltrdata.items.size());
+        
+        embot::os::rtos::mutex_take(impl->_RX_mtx, embot::core::reltimeWaitForever);
+        for(size_t i=0; i<num; i++)
+        {
+            Item item = impl->_iccRX.ltrdata.items[i];
+            if(impl->_RX_fifo.size() >= impl->_config.txcapacity)
+            {
+                impl->_RX_fifo.erase(impl->_RX_fifo.begin());
+            }
+            impl->_RX_fifo.push_back(item);
+        }
+        embot::os::rtos::mutex_release(impl->_RX_mtx); 
+        // question: shall we put it out of the mutex? to be ok it should be called
+        // in a protected way using a dedicated mutex that is also used when ...
+        // we change _config.onrx
+        // the same applies for the 
+        impl->_config.onrx.execute();        
+    }
+    
+}
+
+
+
+void embot::app::eth::theICCservice::Impl::tTXstartup(embot::os::Thread *t, void *p)
+{
+    Impl *impl = reinterpret_cast<Impl*>(p);
+    
+    volatile embot::hw::result_t r {embot::hw::resNOK};
+    r = embot::hw::icc::ltr::init(impl->_iccTX.ltr, {embot::hw::icc::DIR::tx});
+    r = r;
+    impl->txsemaphore = embot::os::rtos::semaphore_new(2, 0);
+    impl->cbkOnTXdone.clear();
+    impl->_flushed = impl->_TXacked = false;
+}
+
+
+void embot::app::eth::theICCservice::Impl::tTXonevent(embot::os::Thread *t, os::EventMask eventmask, void *p)
+{
+    if(0 == eventmask)
+    {   // timeout ...       
+        return;
+    }
+    
+    Impl *impl = reinterpret_cast<Impl*>(p);
+    
+    if(true == embot::core::binary::mask::check(eventmask, evtTX))
+    {
+        // note: i cannot flush more items than what the max size of the letter
+        // so, we just ... lose some if we have a txfifo w/ capacity larger than max size of the
+        // letter ... but we checkd about that in the initialise() ....
+               
+        impl->_iccTX.ltrdata.clear();
+        
+        embot::os::rtos::mutex_take(impl->_TX_mtx, embot::core::reltimeWaitForever);
+        
+        // we checked that in the initialise(), so this min() is redundant. important but useless in here
+        size_t num = std::min(impl->_TX_fifo.size(), impl->_iccTX.ltrdata.items.size());
+                
+        impl->_iccTX.ltrdata.size = num;
+        
+        for(size_t i=0; i<num; i++)
+        {
+            impl->_iccTX.ltrdata.items[i] = impl->_TX_fifo[i];          
+        }
+                
+        impl->_TX_fifo.clear();
+        
+        embot::os::rtos::mutex_release(impl->_TX_mtx);  
+#if 0
+        // and now we send over the ICC
+        embot::hw::icc::ltr::post(impl->_iccTX.ltr, impl->_iccTX.dd, embot::core::time1millisec);
+        impl->_TXacked = embot::hw::icc::ltr::acked(impl->_iccTX.ltr, true);
+#else
+        // embot::hw::icc::ltr::post(impl->_iccTX.ltr, impl->_iccTX.dd, embot::core::time1millisec);
+        // impl->txsemaphore
+        
+//        mettere qualcosa tipo:
+//        il flush() chiama il sem.get() after it sends the tx event e si blocca con timeout
+//        il thread chiama la ltr con uan callbacl che unlocka il semaforo. e poi esce.
+//        in questo modo nessun thread esegue
+        impl->_flushed = impl->_TXacked = false;
+        embot::core::Callback cbk {txackcbk, p};
+        embot::hw::icc::ltr::post(impl->_iccTX.ltr, impl->_iccTX.dd, cbk);
+#endif        
+    }
+    
+}
 
 // --------------------------------------------------------------------------------------------------------------------
 // - the class
@@ -93,16 +637,66 @@ embot::app::eth::theICCservice::theICCservice()
 
 embot::app::eth::theICCservice::~theICCservice() { }
 
-bool embot::app::eth::theICCservice::initialise()
+bool embot::app::eth::theICCservice::initialise(const Config &config)
 {
-    return pImpl->initialise();
+    return pImpl->initialise(config);
 }
 
-bool embot::app::eth::theICCservice::add(const embot::app::eth::mc::messaging::Location &des, const embot::prot::can::Frame &frame)
+bool embot::app::eth::theICCservice::set(modeTX txm)
 {
-    return pImpl->add(des, frame); 
+    return pImpl->set(txm);
 }
 
+bool embot::app::eth::theICCservice::set(const embot::core::Callback &onr)
+{
+    return pImpl->set(onr);
+}
+
+bool embot::app::eth::theICCservice::set(ItemParser itmp)
+{
+    return pImpl->set(itmp);
+}
+
+
+bool embot::app::eth::theICCservice::put(const Item &item, embot::core::relTime timeout)
+{
+    return pImpl->put(item, timeout); 
+}
+
+size_t embot::app::eth::theICCservice::output() const
+{
+    return pImpl->output();
+}
+
+bool embot::app::eth::theICCservice::flush(const embot::core::Callback &cbk)
+{
+    return pImpl->flush(cbk);
+}
+
+bool embot::app::eth::theICCservice::flush(embot::core::relTime timeout)
+{
+    return pImpl->flush(timeout);
+}
+
+bool embot::app::eth::theICCservice::flushed() const
+{
+    return pImpl->flushed();    
+}
+
+size_t embot::app::eth::theICCservice::input() const
+{
+    return pImpl->input();
+}
+
+bool embot::app::eth::theICCservice::get(Item &item, size_t &remaining)
+{
+    return pImpl->get(item, remaining); 
+}
+
+bool embot::app::eth::theICCservice::parse()
+{
+    return pImpl->parse();
+}
 
 
 // - end-of-file (leave a blank line after)----------------------------------------------------------------------------
--- emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theICCservice.h
+++ emBODY/eBcode/arch-arm/embot/app/eth/embot_app_eth_theICCservice.h
@@ -13,11 +13,12 @@
 
 #include "embot_core.h"
 #include "embot_app_eth.h"
-
+#include "embot_os_rtos.h"
 
 #include "embot_app_eth_mc_messaging.h"
+#include "embot_hw_icc_ltr.h"
 
-namespace embot { namespace app { namespace eth {
+namespace embot::app::eth {
       
 #if 0
     The singleton `theICCservice` is responsible to ... 
@@ -29,25 +30,82 @@ namespace embot { namespace app { namespace eth {
     {
     public:
         static theICCservice& getInstance();
-                
-
-        bool initialise();
     
-        bool add(const embot::app::eth::mc::messaging::Location &des, const embot::prot::can::Frame &frame);
-       
-        // IFsomething
-//        bool Verify(const Config &config, const OnEndOfOperation &onverify, bool activateafterverify) override;         
-//        bool Activate(const Config &config) override;
-//        bool Deactivate() override;
-//        bool SendReport() override;         
-//        bool StartReading() override;                          
-//        bool IsReadingAvailable() override;       
-//        bool Read(uint8_t jomo, embot::app::eth::encoder::v1::valueInfo &primary, embot::app::eth::encoder::v1::valueInfo &secondary) override;
-//        bool Diagnostics_Tick() override;
-//        embot::app::eth::encoder::v1::Type GetType(const embot::app::eth::encoder::v1::Target &target) override;
-//        bool Scale(const embot::app::eth::encoder::v1::Target &target, const embot::app::eth::encoder::v1::Scaler &scaler) override;
     
-
+        struct Item
+        {
+            embot::app::eth::mc::messaging::Location des {};
+            embot::prot::can::Frame frame {};
+            constexpr Item() = default;
+        };
+        // static_assert(20 == sizeof(Item), "sizeof(Item) is not 20");
+        
+        using ItemParser = bool (*)(const Item &item);
+        
+        enum class modeTX : uint8_t { instant = 0, onflush = 1 };
+         
+        enum Mode : uint8_t { master = 0, slave = 1 };
+        
+        // TOBEDONE
+        // the master calls init(). if the slave init() it stops forever until
+        // the first has finished. when done it sends via icc::LTR a done message.
+        // the first gets the message and marks the handshaken state. 
+        // the public API, apart set(), can be called only when handhsaken is true.         
+        
+        struct Config
+        {
+            static constexpr size_t maxcapacity {48}; 
+            // we can manage 48 Items in each direction by using two icc::LTR of 1024 bytes
+            // the check is done inside initialise()            
+            
+            Mode mode {Mode::master};
+            embot::hw::icc::LTR rxltr {embot::hw::icc::LTR::one};
+            embot::hw::icc::LTR txltr {embot::hw::icc::LTR::two};
+            size_t rxcapacity {32}; 
+            size_t txcapacity {32};
+            modeTX modetx {modeTX::instant};
+            embot::os::Priority rxpriority {embot::os::Priority::system54};
+            embot::os::Priority txpriority {embot::os::Priority::system53};
+            embot::core::Callback onrx {};
+            ItemParser itemparser {nullptr};
+            
+            constexpr Config() = default;
+            
+            constexpr Config(   Mode mo, embot::hw::icc::LTR rl, embot::hw::icc::LTR tl,
+                                size_t r, size_t t, modeTX mt, embot::os::Priority rp, embot::os::Priority tp, const embot::core::Callback &orx, ItemParser ip)
+                            :   mode (mo), rxltr(rl), txltr(tl),
+                                rxcapacity(std::min(r, maxcapacity)), txcapacity(std::min(t, maxcapacity)),
+                                modetx(mt), 
+                                rxpriority(rp), txpriority(tp), 
+                                onrx(orx),
+                                itemparser(ip)
+                            {}
+                                
+            bool isvalid() const { return true; } // for now always valid. but we check vs it inside initialise()
+        };      
+        
+        bool initialise(const Config &config);
+        
+        // pay attention: so far we don protect the following three set() w/ a mutex
+        bool set(modeTX txm);
+        bool set(const embot::core::Callback &onr);
+        bool set(ItemParser itmp);
+    
+        // if modeTX::onflush we accumulate and tx on flush else we tx immediately
+        bool put(const Item &item, embot::core::relTime timeout = embot::core::reltimeWaitForever);
+        size_t output() const;
+        bool flush(const embot::core::Callback &cbk);
+        bool flush(embot::core::relTime timeout = embot::core::reltimeWaitForever);
+        bool flushed() const;
+        
+        size_t input() const;
+        // we get one by one and we parse it w/ something external ... the betters solution
+        // timeout ? not for now
+        bool get(Item &item, size_t &remaining);
+        // BUT theCANservice has a parse all method, so .... 
+        bool parse();
+        
+    
     private:
         theICCservice(); 
         ~theICCservice(); 
@@ -57,8 +115,22 @@ namespace embot { namespace app { namespace eth {
         std::unique_ptr<Impl> pImpl;
     }; 
 
+    
+    constexpr theICCservice::Config iccmastercfg
+    {
+        theICCservice::Mode::master, embot::hw::icc::LTR::one, embot::hw::icc::LTR::two, 32, 32, theICCservice::modeTX::onflush, 
+        embot::os::Priority::system54, embot::os::Priority::system53,
+        {}, nullptr
+    };       
 
-}}} // namespace embot { namespace app { namespace eth
+    constexpr theICCservice::Config iccslavecfg
+    {
+        theICCservice::Mode::slave, embot::hw::icc::LTR::two, embot::hw::icc::LTR::one, 32, 32, theICCservice::modeTX::onflush, 
+        embot::os::Priority::system54, embot::os::Priority::system53,
+        {}, nullptr
+    };     
+
+} // namespace embot::app::eth {
 
 
 #endif  // include-guard
