29b5fe6d48f129312390be8e9952ba2174320bb5
SanLordKevin <kevin.sangalli@iit.it>
2024-10-18 14:31:49 +0200

    updated firmware version and date

--- emBODY/eBcode/arch-arm/board/amc/application/v2/cfg/theApplication_config.h
+++ emBODY/eBcode/arch-arm/board/amc/application/v2/cfg/theApplication_config.h
@@ -38,8 +38,8 @@ namespace embot { namespace app { namespace eth {
         .property =
         {
             Process::eApplication,
-            {2, 8},
-            {2024, Month::Jul, Day::twenty, 14, 00}
+            {2, 9},
+            {2024, Month::Oct, Day::twentyone, 14, 00}
         },
         .OStick = 1000*embot::core::time1microsec,
         .OSstacksizeinit = 10*1024,
