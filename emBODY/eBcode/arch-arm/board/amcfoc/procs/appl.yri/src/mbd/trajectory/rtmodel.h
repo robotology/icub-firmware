8a4a6165ca56e9ccac593c744bed8f51a4c25ecd
marco.accame <marco.accame@iit.it>
2025-04-28 13:02:16 +0200

    added mbd trajectory

---
+++ emBODY/eBcode/arch-arm/board/amcfoc/procs/appl.yri/src/mbd/trajectory/rtmodel.h
@@ -0,0 +1,50 @@
+//
+// Non-Degree Granting Education License -- for use at non-degree
+// granting, nonprofit, education, and research organizations only. Not
+// for commercial or industrial use.
+//
+// File: rtmodel.h
+//
+// Code generated for Simulink model 'Trajectory'.
+//
+// Model version                  : 1.211
+// Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+// C/C++ source code generated on : Thu Apr 24 14:56:11 2025
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex-M
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef rtmodel_h_
+#define rtmodel_h_
+#include "Trajectory.h"
+#define MODEL_CLASSNAME                namespace_s1::Trajectory
+#define MODEL_STEPNAME                 Trajectory_step
+
+//
+//  ROOT_IO_FORMAT: 0 (Individual arguments)
+//  ROOT_IO_FORMAT: 1 (Structure reference)
+//  ROOT_IO_FORMAT: 2 (Part of model data structure)
+
+#define ROOT_IO_FORMAT                 1
+
+// Macros generated for backwards compatibility
+#ifndef rtmGetErrorStatus
+#define rtmGetErrorStatus(rtm)         ((void*) 0)
+#endif
+
+#ifndef rtmSetErrorStatus
+#define rtmSetErrorStatus(rtm, val)    ((void) 0)
+#endif
+
+#ifndef rtmGetStopRequested
+#define rtmGetStopRequested(rtm)       ((void*) 0)
+#endif
+#endif                                 // rtmodel_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
