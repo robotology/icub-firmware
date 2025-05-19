b6b7807546406bbd18d57e28c5da9f4151183147
Mattia Fussi <38140169+mfussi66@users.noreply.github.com>
2025-05-19 15:44:44 +0200

    AMC-FOC: Align codegen files structure to amcbldc (#598)

--- emBODY/eBcode/arch-arm/mbd/utils/copier.py
+++ emBODY/eBcode/arch-arm/mbd/utils/copier.py
@@ -27,12 +27,18 @@ def is_empty(dir):
         return True
 
 def find_subdirectory(main_directory, subdirectory, parent = None):
-    for root, dirs, files in os.walk(main_directory):
-        for dir in dirs:
-            if dir == subdirectory:
-                dir_parent = root.split(os.sep)[-1]
-                if parent is None or dir_parent == parent:
-                    return os.path.join(root, subdirectory)
+
+    if not os.path.isdir(main_directory):
+        raise ValueError(f"Main directory '{main_directory}' does not exist")
+        return ""
+            
+    for root, dirs, _ in os.walk(main_directory):
+        if subdirectory in dirs:
+            dir_parent = os.path.basename(root)
+            
+            if parent is None or dir_parent == parent:
+                return os.path.join(root, subdirectory)
+
     return ""
 
 def check_dictionary_type(dictionary, key, type):
@@ -45,7 +51,6 @@ def parse_instructions(dictionary, source_directory, target_directory):
     check_dictionary_type(dictionary, "subdirectories_to_copy", list)
     subdirectories_to_copy = dictionary["subdirectories_to_copy"]
 
-    # loop over the subdirectories
     for subdirectory in subdirectories_to_copy:
         parent = subdirectory["source_directory_parent"] if "source_directory_parent" in subdirectory else None
         source_subdir = find_subdirectory(source_directory, subdirectory["source_directory"], parent)
@@ -54,33 +59,35 @@ def parse_instructions(dictionary, source_directory, target_directory):
         if(source_subdir == "" or target_subdir == ""):
             continue
 
-        copy_files(source_subdir, target_subdir, subdirectory["files"], True)
+        if "exclude" in subdirectory:
+            copy_files(source_subdir, target_subdir, subdirectory["files"], subdirectory["exclude"])
+        else:
+            copy_files(source_subdir, target_subdir, subdirectory["files"])
 
-def copy_files(source_dir, target_dir, file_selectors, overwrite):
+def copy_files(source_dir, target_dir, file_selectors, exclude_list=None):
     if not (os.path.isdir(source_dir) and os.path.isdir(target_dir)):
-        raise Exception(f"""Invalid path given (Source: {source_dir}, Target: {target_dir}).
-        If target is empty, consider creating the empty directory in the desired destination before running the copier.""")
+        raise ValueError(f"Invalid path given (Source: {source_dir}, Target: {target_dir})")
+    
     print(f"{source_dir} ➡️ {target_dir}")
-    files_to_copy = []
-    for file_selector in file_selectors:
-        selected_files = glob.glob(os.path.join(source_dir, file_selector))
-        # print(f"Selector {file_selector} matched with the following files: {selected_files}")
-        files_to_copy = files_to_copy + selected_files
-    files_to_copy = set(files_to_copy)
     print(SECTION)
+    
+    files_to_copy = set()
+    exclude_list = set(exclude_list or [])
+    
+    for selector in file_selectors:
+        selected_files = glob.glob(os.path.join(source_dir, selector))
+        files_to_copy.update(file for file in selected_files if file not in exclude_list)
+    
     for source_file in files_to_copy:
         file_basename = os.path.basename(source_file)
         target_file = os.path.join(target_dir, file_basename)
-        if overwrite or (not os.path.exists(target_file)):
-            shutil.copy(source_file, target_file)
-            # print(f"Copying  {file_basename} from {source_dir} to {target_dir}")
-        else:
-            print(f"Skipping {file_basename} from {source_dir} since overwriting is not allowed")
+        shutil.copy(source_file, target_file)
 
 def main():
     parser = argparse.ArgumentParser(prog='The Copier',
                                     description="The copier has the purpose to copy the code generated from Simulink to the proper board folder in icub-firmware ",
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument('args', nargs='*')
     parser.add_argument('-s', '--source', help="Absolute paths to the codegen directory within icub-firmware-models")
     parser.add_argument('-d', '--destination', help="Absolute path to the model-based-design directory within icub-firmware (for a specific board)")
     parser.add_argument('-j', '--json_path', help="Absolute path to the directory containing the directories.json file")
