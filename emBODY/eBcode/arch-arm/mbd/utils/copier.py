2eb224cfbd695c741108ea2d64f4ee5732e19b21
Mattia Fussi <mattia.fussi@iit.it>
2025-05-19 11:02:39 +0200

    Update copier and directories.json

--- emBODY/eBcode/arch-arm/mbd/utils/copier.py
+++ emBODY/eBcode/arch-arm/mbd/utils/copier.py
@@ -54,39 +54,42 @@ def parse_instructions(dictionary, source_directory, target_directory):
         if(source_subdir == "" or target_subdir == ""):
             continue
 
-        copy_files(source_subdir, target_subdir, subdirectory["files"], True)
+        copy_files(source_subdir, target_subdir, subdirectory["files"], subdirectory["exclude"])
 
-def copy_files(source_dir, target_dir, file_selectors, overwrite):
+def copy_files(source_dir, target_dir, file_selectors, exclude_list=None):
     if not (os.path.isdir(source_dir) and os.path.isdir(target_dir)):
-        raise Exception(f"""Invalid path given (Source: {source_dir}, Target: {target_dir}).
-        If target is empty, consider creating the empty directory in the desired destination before running the copier.""")
+        raise ValueError(f"Invalid path given (Source: {source_dir}, Target: {target_dir})")
+    
     print(f"{source_dir} ➡️ {target_dir}")
-    files_to_copy = []
-    for file_selector in file_selectors:
-        selected_files = glob.glob(os.path.join(source_dir, file_selector))
-        # print(f"Selector {file_selector} matched with the following files: {selected_files}")
-        files_to_copy = files_to_copy + selected_files
-    files_to_copy = set(files_to_copy)
     print(SECTION)
+    
+    files_to_copy = set()
+    exclude_list = set(exclude_list or [])
+    
+    for selector in file_selectors:
+        selected_files = glob.glob(os.path.join(source_dir, selector))
+        files_to_copy.update(file for file in selected_files if file not in exclude_list)
+    
     for source_file in files_to_copy:
         file_basename = os.path.basename(source_file)
         target_file = os.path.join(target_dir, file_basename)
-        if overwrite or (not os.path.exists(target_file)):
-            shutil.copy(source_file, target_file)
-            # print(f"Copying  {file_basename} from {source_dir} to {target_dir}")
-        else:
-            print(f"Skipping {file_basename} from {source_dir} since overwriting is not allowed")
+        shutil.copy(source_file, target_file)
 
 def main():
     parser = argparse.ArgumentParser(prog='The Copier',
                                     description="The copier has the purpose to copy the code generated from Simulink to the proper board folder in icub-firmware ",
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('args', nargs='*')
     parser.add_argument('-s', '--source', help="Absolute paths to the codegen directory within icub-firmware-models")
     parser.add_argument('-d', '--destination', help="Absolute path to the model-based-design directory within icub-firmware (for a specific board)")
     parser.add_argument('-j', '--json_path', help="Absolute path to the directory containing the directories.json file")
 
     args = parser.parse_args()
     config = vars(args)
+
+    if not args.args:
+        parser.print_help()
+        return
 
     path_to_json = os.path.join(config['json_path'], "directories.json")
     path_to_src = config['source']
