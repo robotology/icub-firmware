3ce5e951cb33b20e90d0f35bb7d292807699a488
marcoaccame <marco.accame@iit.it>
2025-10-24 11:31:04 +0200

    fixed projects

---
+++ emBODY/eBcode/arch-arm/board/amcmj1/bsp/shared/eEmemorymap.h
@@ -0,0 +1,23 @@
+
+/*
+ * Copyright (C) 2025 iCub Tech - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame
+ * email:   marco.accame@iit.it
+*/
+
+
+// - include guard ----------------------------------------------------------------------------------------------------
+#ifndef __EEMEMORYMAP_H_
+#define __EEMEMORYMAP_H_
+
+
+// in here we impose the macros required by the shared services
+#include "eEmemorymap_sharedservices_dualcore.h"
+
+
+#endif  // include-guard
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
+
