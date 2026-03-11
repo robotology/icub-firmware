f56ae37e37f81cb1a59292bc632f0aa7ae6d3c80
marcoaccame <marco.accame@iit.it>
2026-03-11 08:46:42 +0100

    increased the version of the eloader

--- emBODY/eBcode/arch-arm/board/amc/environment/eloader/src/info.cpp
+++ emBODY/eBcode/arch-arm/board/amc/environment/eloader/src/info.cpp
@@ -30,16 +30,16 @@ constexpr eEmoduleExtendedInfo_t s_loader_info_extended __attribute__((section(E
                 .signature  = ee_procLoader,
                 .version    = 
                 { 
-                    .major = 3, 
-                    .minor = 3
+                    .major = 4, 
+                    .minor = 0
                 },  
                 .builddate  = 
                 {
-                    .year  = 2025,
-                    .month = 10,
-                    .day   = 02,
+                    .year  = 2026,
+                    .month = 3,
+                    .day   = 3,
                     .hour  = 11,
-                    .min   = 11
+                    .min   = 34
                 }
             },
             .rom        = 
