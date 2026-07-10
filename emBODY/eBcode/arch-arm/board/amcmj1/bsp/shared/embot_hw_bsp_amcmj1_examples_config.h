46a12278c95f0b80424f3904e0d4e4170925be74
Kevin Sangalli <kevin.sangalli@iit.it>
2026-07-10 09:41:44 +0200

    amcmj1: refactory config.h files for different projects (#692)

---
+++ emBODY/eBcode/arch-arm/board/amcmj1/bsp/shared/embot_hw_bsp_amcmj1_examples_config.h
@@ -0,0 +1,74 @@
+
+/*
+ * Copyright (C) 2026 MESH - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame, Kevin Sangalli
+ * email:   marco.accame@iit.it
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef __EMBOT_HW_BSP_AMCMJ1_EXAMPLES_YRI_CONFIG_H_
+#define __EMBOT_HW_BSP_AMCMJ1_EXAMPLES_YRI_CONFIG_H_
+
+#include "embot_core.h"
+#include "embot_hw_types.h"
+#include "embot_hw_bsp.h"
+
+#include "stm32hal.h"
+
+
+#if defined(STM32HAL_BOARD_AMCMJ1_2CM4) || defined(STM32HAL_BOARD_AMCMJ1_1CM7)
+
+    #warning this is the bsp config of STM32HAL_BOARD_AMCMJ1_EXAMPLES_YRI, which is not a real bsp. 
+    #warning It is not meant to be used in production. 
+    #warning It is meant to be used only for testing and development purposes.
+
+    
+    #define EMBOT_ENABLE_hw_sys_emulateRAND
+    #define EMBOT_ENABLE_hw_bsp_specialize
+             
+    // not minimal section
+    
+    // shared
+    #define EMBOT_ENABLE_hw_mtx
+    #define EMBOT_ENABLE_hw_icc_sig
+    #define EMBOT_ENABLE_hw_icc_mem
+    #define EMBOT_ENABLE_hw_icc_ltr
+
+    // portable  
+    // cm7: spi, eeprom
+
+
+    #define EMBOT_ENABLE_hw_timer
+
+
+// so far can, if not commented out, is assigned to the slave core
+    #if defined(EMBOT_CORE_slave)
+        #define EMBOT_ENABLE_hw_can    
+    #else
+        #define EMBOT_ENABLE_hw_eth
+        #define EMBOT_ENABLE_hw_eeprom
+        #define EMBOT_ENABLE_hw_spi
+        #define EMBOT_ENABLE_hw_spi_two
+        #define EMBOT_ENABLE_hw_spi_three
+    
+    #endif // EMBOT_CORE_slave 
+    #define EMBOT_ENABLE_hw_can
+    // so far can is assigned to the slave core
+    #if defined(EMBOT_ENABLE_hw_can)
+        #define EMBOT_ENABLE_hw_can_one
+    #endif  
+#endif
+
+
+#else
+    #error this is the bsp config of STM32HAL_BOARD_AMCMJ1_EXAMPLES_YRI ...
+#endif
+
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
---
+++ emBODY/eBcode/arch-arm/board/amcmj1/bsp/shared/embot_hw_bsp_amcmj1_updater_maintainer_config.h
@@ -0,0 +1,69 @@
+
+/*
+ * Copyright (C) 2026 MESH - Istituto Italiano di Tecnologia
+ * Author:  Marco Accame, Kevin Sangalli
+ * email:   marco.accame@iit.it
+*/
+
+// - include guard ----------------------------------------------------------------------------------------------------
+
+#ifndef __EMBOT_HW_BSP_AMCMJ1_UPDATER_MAINTAINER_CONFIG_H_
+#define __EMBOT_HW_BSP_AMCMJ1_UPDATER_MAINTAINER_CONFIG_H_
+
+#include "embot_core.h"
+#include "embot_hw_types.h"
+#include "embot_hw_bsp.h"
+
+#include "stm32hal.h"
+
+
+#if defined(STM32HAL_BOARD_AMCMJ1_2CM4) || defined(STM32HAL_BOARD_AMCMJ1_1CM7)
+
+
+    #define EMBOT_ENABLE_hw_bsp_specialize
+    #define EMBOT_ENABLE_hw_sys_emulateRAND
+     
+
+    // shared
+    #define EMBOT_ENABLE_hw_mtx
+    #define EMBOT_ENABLE_hw_icc_sig
+    #define EMBOT_ENABLE_hw_icc_mem
+    #define EMBOT_ENABLE_hw_icc_ltr
+
+
+// so far, spi and eeprom are assigned to the master core, so:
+#if defined(EMBOT_CORE_master)
+
+
+    #define EMBOT_ENABLE_hw_spi
+    #if defined(EMBOT_ENABLE_hw_spi)
+        #define EMBOT_ENABLE_hw_spi_two
+        #define EMBOT_ENABLE_hw_spi_three
+    #endif 
+    
+    #define EMBOT_ENABLE_hw_eeprom
+
+    #if defined(STM32HAL_CORE_CM4) 
+    //#warning ... if the cm4 is also master then you need EMBOT_REDEFINE_hw_bsp_DRIVER_uniqueid + EMBOT_REDEFINE_hw_bsp_DRIVER_setuniqueid
+        #define EMBOT_REDEFINE_hw_bsp_DRIVER_uniqueid 
+        #define EMBOT_REDEFINE_hw_bsp_DRIVER_setuniqueid
+    #endif // STM32HAL_CORE_CM4
+    
+    // and also eth is assigned to master
+    #define EMBOT_ENABLE_hw_eth
+    
+#endif // EMBOT_CORE_master
+
+
+
+#else
+    #error this is the bsp config of __EMBOT_HW_BSP_AMCMJ1_UPDATER_MAINTAINER ...
+#endif
+
+
+#endif  // include-guard
+
+
+// - end-of-file (leave a blank line after)----------------------------------------------------------------------------
+
+
